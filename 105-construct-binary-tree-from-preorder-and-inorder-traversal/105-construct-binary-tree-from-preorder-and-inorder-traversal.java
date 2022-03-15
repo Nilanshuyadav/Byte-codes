7a431062d264f4ac7cb6fbde179f4833bba63cea
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-03-15 17:13:50 +0530

    Time: 1 ms (99.78%), Space: 41.9 MB (76.11%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,28 +15,29 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-     
-        Map<Integer,Integer> map = new HashMap();
         int n = preorder.length;
+        
+        Map<Integer,Integer> map = new HashMap();
         
         for(int i=0;i<n;i++) map.put(inorder[i],i);
         
-        return helper(preorder,0,n-1,inorder,0,n-1,map);
-        
+        return buildtree(preorder,0,n-1,inorder,0,n-1,map);
     }
     
-    private TreeNode helper(int[] preorder,int startpreorder,int endpreorder,int[] inorder,int startinorder,int endinorder, Map<Integer,Integer> map){
+    private TreeNode buildtree(int[] preorder,int prestart,int preend,int[] inorder,int instart,int inend,Map<Integer,Integer> map){
         
-        if(startpreorder>endpreorder || startinorder>endinorder) return null;
+        if(prestart>preend) return null;
         
-        TreeNode root = new TreeNode(preorder[startpreorder]);
+        TreeNode root = new TreeNode(preorder[prestart]);
+        
         int inroot = map.get(root.val);
-        int numsleft = inroot - startinorder;
+        int numsleft = inroot - instart;
         
-            root.left = helper(preorder,startpreorder+1,startpreorder+numsleft,inorder,startinorder,inroot-1,map);
-        root.right = helper(preorder,startpreorder+numsleft+1,endpreorder,inorder,inroot+1,endinorder,map);
+        root.left = buildtree(preorder,prestart+1,prestart+numsleft,inorder,instart,inroot-1,map);
+        
+        
+        root.right = buildtree(preorder,prestart+numsleft+1,preend,inorder,inroot+1,inend,map);
         
         return root;
-        
     }
 }
