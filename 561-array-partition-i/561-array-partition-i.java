3947697db43233c7fc49e4936a1acdd825200fd2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-03-15 09:58:13 +0530

    Time: 15 ms (67.40%), Space: 53.7 MB (50.09%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum = 0,n = nums.length;
+        
+        for(int i=0;i<n;i=i+2){
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+}
