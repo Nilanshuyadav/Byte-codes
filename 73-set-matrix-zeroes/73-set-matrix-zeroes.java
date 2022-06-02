82e18289507bebdfad33553806a2a63ec050aa1c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-06-02 09:37:32 +0530

    Time: 3 ms (22.18%), Space: 54.7 MB (5.91%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,49 +3,50 @@ class Solution {
         int r = matrix.length;
         int c = matrix[0].length;
         
-        List<Pair> al = new ArrayList<Pair>();
+        List<Pair> lst = new ArrayList<Pair>();
         
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++)
-                if(matrix[i][j] == 0) al.add(new Pair<Integer,Integer>(i,j));
+            for(int j=0;j<c;j++){
+                if(matrix[i][j] == 0){
+                    lst.add(new Pair<Integer,Integer>(i,j));
+                }
+            }
         }
         
-        int n = al.size();
+        int n = lst.size();
         
         for(int i=0;i<n;i++){
-            Pair<Integer,Integer> p = al.get(i);
-            setzero(p.getKey(),p.getValue(),matrix,c,r);
-        }    
+            Pair<Integer,Integer> p = lst.get(i);
+            setzero(p.getKey(),p.getValue(),matrix,r,c);
+        }
     }
     
-    
-    
-    private void setzero(int i,int j,int[][] arr,int c,int r){
-        int ti = i;
-        int tj = j;
+    private void setzero(int i,int j,int[][] matri, int r, int c){
+        int curri = i;
+        int currj = j;
         
-        while(tj>=0){
-            arr[i][tj] = 0;
-            tj--;
+        while(curri >=0){
+            matri[curri][j] = 0;
+            curri--;
         }
         
-        tj = j;
+        curri = i;
         
-        while(tj<c){
-            arr[i][tj] = 0;
-            tj++;
+        while(curri<r){
+            matri[curri][j] = 0;
+            curri++;
         }
         
-        while(ti>=0){
-            arr[ti][j] = 0;
-            ti--;
+        while(currj>=0){
+            matri[i][currj] = 0;
+            currj--;
         }
         
-        ti = i;
+        currj = j;
         
-        while(ti<r){
-            arr[ti][j] = 0;
-            ti++;
+        while(currj<c){
+            matri[i][currj] = 0;
+            currj++;
         }
     }
 }
