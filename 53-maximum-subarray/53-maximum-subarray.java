8f21f2e61d5fc33a55920f53247c7cfc9523e74a
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-06-11 19:38:43 +0530

    Time: 2 ms (59.53%), Space: 74.4 MB (31.13%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -4,13 +4,17 @@ class Solution {
         int sum = 0;
         int max = Integer.MIN_VALUE;
         
+        
         for(int i=0;i<n;i++){
             sum += nums[i];
             
-            if(sum > max) max = sum;
-            
-            if(sum<=0)
+             max = Math.max(max,sum);
+            if(sum < 0){
+                max = Math.max(sum,max);
                 sum = 0;
+            }
+            
+           
         }
         
         return max;
