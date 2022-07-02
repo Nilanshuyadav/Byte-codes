99ae52bba912f8bcc8c75670f3f61269625dc13b
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-07-02 16:56:51 +0530

    Time: 4 ms (44.07%), Space: 41.5 MB (60.08%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -1,48 +1,25 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1;i<=n;i++){
-            sb.append(i);
-        }
+        int fact = 1;
+        ArrayList<Integer> al = new ArrayList<>();
         
-        while(k-->1){
-            sb = nextPermutation(sb,n);
+        for(int i = 1;i<n;i++){
+            fact *= i;
+            al.add(i);
+        }
+        al.add(n);
+        
+        k = k-1;
+        StringBuilder sb = new StringBuilder("");
+        
+        while(true){
+            sb.append(al.get(k/fact));
+            al.remove(k/fact);
+            if(al.size() == 0) break;
+            k = k%fact;
+            fact = fact/al.size();
         }
         
         return sb.toString();
-    }
-    
-    public StringBuilder nextPermutation(StringBuilder s, int n){
-        int i = n-1;
-        if(n==1) return s;
-        
-        while(i > 0 && s.charAt(i)<s.charAt(i-1)) i--;
-        
-        int ind1 = --i;
-        
-        i = n-1;
-        while(ind1>=0 && s.charAt(i)<s.charAt(ind1))i--;
-        
-        char ch;
-        
-        if(ind1 >=0 ){
-            ch = s.charAt(ind1);
-            s.setCharAt(ind1,s.charAt(i));
-            s.setCharAt(i,ch);
-        }    
-        
-        ind1++;
-        while(ind1<n-1){
-            ch = s.charAt(ind1);
-            s.setCharAt(ind1,s.charAt(n-1));
-            s.setCharAt(n-1,ch);
-            
-            ind1++;
-            n--;
-        }
-        
-        return s;
-        
-        
     }
 }
