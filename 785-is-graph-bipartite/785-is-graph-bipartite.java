91cb1267aa651fb079d874c571ba8c2b62499692
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-07-27 15:04:16 +0530

    Time: 2 ms (58.23%), Space: 43.1 MB (88.97%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,37 +1,45 @@
 class Solution {
-    int[] color;
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         
-        color = new int[n];
+        int[] color = new int[n];
         Arrays.fill(color,-1);
         
-        for(int i =0;i<n;i++)
+        Queue<Pair> q = new LinkedList<>();
+        
+        for(int i =0;i<n;i++){
+            
             if(color[i] == -1){
                 color[i] = 0;
-                if(!function(i,-1,graph))
-                    return false;
-            }    
-        
-        return true;
-    }
-    
-    public boolean function(int ind, int parent, int[][] graph){
-        int ind_color = color[ind];
-        System.out.println(ind);
-        
-        for(int i: graph[ind]){
-            if(i == parent) continue;
-            
-            if(color[i] != -1){
-                if(color[i] == ind_color) return false;
-                else continue;
+                q.add(new Pair(i,-1));
+                
+                while(!q.isEmpty()){
+                    Pair temp = q.remove();
+                    
+                    for(int j : graph[temp.val])
+                    {
+                        if(j == temp.parent) continue;
+                        if(color[j] != -1){
+                            if(color[j] == color[temp.val])return false;
+                            else continue;
+                        }
+                        color[j] = color[temp.val] == 0? 1:0;
+                        q.add(new Pair(j,temp.val));
+                    }
+                }
             }
-            
-            color[i] = ind_color==0?1:0;
-            if(!function(i,ind,graph)) return false;
         }
         
         return true;
+    }
+}
+
+class Pair{
+    int val;
+    int parent;
+    
+    public Pair(int val,int parent){
+        this.val = val;
+        this.parent = parent;
     }
 }
