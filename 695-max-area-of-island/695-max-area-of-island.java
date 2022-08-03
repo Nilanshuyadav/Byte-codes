38e2891a9533e40f9ea679ad33dfac6bbbaa8eb2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-03 23:49:12 +0530

    Time: 4 ms (55.43%), Space: 47.1 MB (64.71%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,41 +1,53 @@
 class Solution {
-    boolean[][] visited;
-    
     public int maxAreaOfIsland(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        int max = 0;
         
-        int row = grid.length;
-        int col = grid[0].length;
+        for(int i =0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                if(grid[i][j] == 1){
+                    max = Math.max(max,function(grid,i,j));
+                }    
+            }
+        }
         
-        int max = Integer.MIN_VALUE;
-        
-        visited = new boolean[row][col];
-        
-        for(int drow = 0;drow<row;drow++)
-            for(int dcol = 0;dcol<col;dcol++)
-                if(grid[drow][dcol] == 1 && visited[drow][dcol] == false){
-                    int area = helper(drow,dcol,grid,row,col);
-                    max = area > max? area:max;
-                }
-        
-        return max==Integer.MIN_VALUE? 0:max;
+        return max;
     }
     
-    public int helper(int row, int col, int[][] grid, int row_length, int col_length){
-        visited[row][col] = true;
+    public int function(int[][] grid, int row, int col){
+        int duplicate_row = row, duplicate_col = col;
+        grid[row][col] = 0;
+        int sum=1;
         
-        int sum = 1;
+        while(--row >= 0){
+            if(grid[row][col] == 1)
+                sum += function(grid,row,col);
+            else break;
+        }
         
-        if(row+1 < row_length && grid[row+1][col] == 1 && visited[row+1][col] == false)
-            sum += helper(row+1,col,grid,row_length,col_length);
+        row = duplicate_row;
         
-        if(row-1 >= 0 && grid[row-1][col] == 1 && visited[row-1][col] == false)
-            sum += helper(row-1,col,grid,row_length,col_length);
+        while(++row < grid.length){
+            if(grid[row][col] == 1)
+                sum += function(grid,row,col);
+            else break;
+        }
         
-        if(col+1 < col_length && grid[row][col+1] == 1 && visited[row][col+1] == false)
-            sum += helper(row,col+1,grid,row_length,col_length);
+        row = duplicate_row;
         
-        if(col-1 >= 0 && grid[row][col-1] == 1 && visited[row][col-1] == false)
-            sum += helper(row,col-1,grid,row_length,col_length);
+        while(--col >= 0){
+            if(grid[row][col] == 1)
+                sum += function(grid,row,col);
+            else break;
+        }
+        
+        col = duplicate_col;
+        
+        while(++col < grid[0].length){
+            if(grid[row][col] == 1)
+                sum += function(grid,row,col);
+            else break;
+        }
         
         return sum;
     }
