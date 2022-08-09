e5aaf75a79da91bb48c6b635e13c33d7beb84884
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-09 21:56:16 +0530

    Time: 20 ms (59.94%), Space: 73.3 MB (36.20%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -10,26 +10,26 @@ class Solution {
         
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++)
-                if(mat[i][j] == 0)
-                    q.add(new Pair(i,j,0));
+                if(mat[i][j] == 0){
+                    ans[i][j] = 0;
+                    q.add(new Pair(i,j));
+                }
         }
         
         int[] r = {-1,1,0,0}, c = {0,0,-1,1};
         
         while(!q.isEmpty()){
             Pair temp = q.remove();
-            
-            if(ans[temp.r][temp.c] > temp.dis){
-                ans[temp.r][temp.c] = temp.dis;
                 
                 for(int i=0;i<4;i++){
                     int tr = temp.r + r[i];
                     int tc = temp.c + c[i];
                     
-                    if(tr >= 0 && tr<row && tc >=0 && tc<col && ans[tr][tc] == Integer.MAX_VALUE)
-                        q.add(new Pair(tr,tc,temp.dis+1));
+                    if(tr >= 0 && tr<row && tc >=0 && tc<col &&                          ans[tr][tc] == Integer.MAX_VALUE){
+                        ans[tr][tc] = 1 + ans[temp.r][temp.c];
+                        q.add(new Pair(tr,tc));
+                    }
                 }
-            }
         }
         
         return ans;
@@ -39,11 +39,9 @@ class Solution {
 class Pair{
     int r;
     int c;
-    int dis;
     
-    public Pair(int r,int c, int dis){
+    public Pair(int r,int c){
         this.r = r;
         this.c = c;
-        this.dis = dis;
     }
 }
