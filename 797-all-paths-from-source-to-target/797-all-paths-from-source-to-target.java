6e6de05f4be9e081cd949b9ada8f8208818a2e12
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-10 01:43:59 +0530

    Time: 3 ms (88.84%), Space: 55.7 MB (11.61%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<List<Integer>> ans;
+    
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        ans = new ArrayList<>();
+        
+        function(graph, 0,new ArrayList<Integer>());
+        
+        return ans;
+    }
+    
+    public void function(int[][] graph, int ind, List<Integer> al){
+        al.add(ind);
+        
+        if(ind == graph.length-1){
+            ans.add(new ArrayList<>(al));
+            al.remove(al.size()-1);
+            return;
+        }
+        
+        for(int i : graph[ind]){
+            function(graph,i,al);
+        }
+        
+        al.remove(al.size()-1);
+    } 
+}
