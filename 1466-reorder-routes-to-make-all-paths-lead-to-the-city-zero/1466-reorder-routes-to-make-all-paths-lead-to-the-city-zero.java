e0d220e073a758e29f9f0a1db6a806b6f0007ff5
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-14 15:40:20 +0530

    Time: 257 ms (25.18%), Space: 123 MB (50.05%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,13 +1,9 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        // Set<Pair> set = new HashSet<>();
-        Map<Integer,List<Integer>> ad = new HashMap<>();
-        
-        for(int i=0;i<n;i++)
-            ad.put(i,new ArrayList<>());
+        Set<String> set = new HashSet<>();
         
         for(int i[] : connections)
-            ad.get(i[0]).add(i[1]);
+            set.add(new StringBuilder().append(i[0]).append(",").append(i[1]).toString());
         
         List<List<Integer>> adj = new ArrayList<>();
         boolean[] vis = new boolean[n];
@@ -22,7 +18,6 @@ class Solution {
         
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        //Pair temp_pair;
         int cnt = 0;
         
         while(!q.isEmpty()){
@@ -30,23 +25,27 @@ class Solution {
             vis[temp] = true;
             for(int i : adj.get(temp)){
                 if(vis[i] != false) continue;
-                //temp_pair = new Pair(temp,i);
-                if(ad.get(temp).contains(i))
+                // final int temp_from = temp;
+                // final int temp_to = i;
+                String temp_s = new StringBuilder().append(temp).append(",").append(i).toString();
+                if(set.contains(temp_s))
                     cnt++;
                 q.add(i);
             }
         }
+        
+        
         return cnt;
     }
     
-    public boolean contains2(List<int[]> set, int[] temp_pair){
-        boolean temp = false;
+//     public boolean contains2(List<int[]> set, int[] temp_pair){
+//         boolean temp = false;
         
-        for(int i[] : set)
-            temp  = temp || ((i[0]==temp_pair[0]) && i[1]==temp_pair[1]);
+//         for(int i[] : set)
+//             temp  = temp || ((i[0]==temp_pair[0]) && i[1]==temp_pair[1]);
         
-        return temp;
-    }
+//         return temp;
+//     }
     
 }
 
