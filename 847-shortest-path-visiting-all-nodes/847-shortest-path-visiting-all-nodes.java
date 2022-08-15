6ec29dbb5f3a12b7e3d658bf442571a450b14a1c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-15 15:53:26 +0530

    Time: 8 ms (99.87%), Space: 42.3 MB (96.18%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
@@ -1,48 +1,46 @@
 class Solution {
-    class Pair {
-        int i;
-        int path;
-        public Pair(int i, int path) {
-            this.i = i;
-            this.path = path;
-        }
-    }
     public int shortestPathLength(int[][] graph) {
-        /*
-        For each node currentNode, steps as key, visited as value
-        boolean[currentNode][steps]
-        */
         int n = graph.length;
+        boolean[][] visited = new boolean[n][1<<n];
         
-        // 111....1, 1<< n - 1
-        int allVisited = (1 << n) - 1;
+        int allVisited = (1<<n) -1;
         
-        boolean[][] visited = new boolean[n][1 << n];
+        if(n==1) return 0;
+        
         Queue<Pair> q = new LinkedList<>();
-        if (1 << 0 == allVisited) return 0;
-        for (int i = 0; i < n; i++) {
-            
-            visited[i][1 << i] = true;   
-            q.offer(new Pair(i, 1 << i));
+        
+        for(int i=0;i<n;i++){
+            visited[i][1<<i] = true;
+            q.add(new Pair(i,1<<i));
         }
+        
         int step = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Pair p = q.poll();
-                int[] edges = graph[p.i];
-         
-                for(int t: edges) {
-                    int path = p.path | (1 << t);
-                    if (path == allVisited) return step + 1;
-                    if (!visited[t][path]) {
-                        visited[t][path] = true;
-                        q.offer(new Pair(t, path)); 
-                    }                                    
+            for(int i=0;i<size;i++){
+                Pair p = q.remove();
+                for(int edges : graph[p.ind]){
+                    int temp = p.path | (1<<edges);
+                    
+                    if(temp == allVisited) return ++step;
+                    if(!visited[edges][temp]){
+                        visited[edges][temp] = true;
+                        q.add(new Pair(edges,temp));
+                    }
                 }
             }
             step++;
         }
+        
         return step;
+    }
+}
+
+class Pair{
+    int ind;
+    int path;
+    public Pair(int ind,int path){
+        this.ind = ind;
+        this.path = path;
     }
 }
