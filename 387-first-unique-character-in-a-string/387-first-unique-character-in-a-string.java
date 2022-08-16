62514b6e94caa5ca29d2f27b80c23ac0440ad510
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-17 05:02:38 +0530

    Time: 39 ms (50.91%), Space: 48.9 MB (58.11%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,33 +1,24 @@
 class Solution {
     public int firstUniqChar(String s) {
+        Set<Character> set1 = new HashSet<>(), set2 = new HashSet<>();
         
-//         Set<Character> set1 = new HashSet<>();
-//         Set<Character> set2 = new HashSet<>();
-        
-//         int n = s.length();
-        
-//         for(int i=0;i<n;i++){
-//             if(!set2.contains(s.charAt(i)) && !set1.add(s.charAt(i))){
-//                 set1.remove(s.charAt(i));
-//                 set2.add(s.charAt(i));
-//             }
-//         }
-        
-//         for(int i=0;i<n;i++){
-//             if(set1.contains(s.charAt(i))){
-//                 return s.indexOf(s.charAt(i));
-//             }
-//         }
-        
-//         return -1;
-        
-        int[] freq = new int[26];
         int n = s.length();
-        for(int i=0;i<n;i++) freq[s.charAt(i)-'a']++;
+        for(int i =0;i<n;i++){
+            char ch = s.charAt(i);
+            if(!set2.contains(ch) && set1.add(ch)){}
+            else{
+                set1.remove(ch);
+                set2.add(ch);
+            }
+        }
+        
+        if(set1.size() == 0) return -1;
         
         for(int i=0;i<n;i++){
-            if(freq[s.charAt(i)-'a'] == 1) return i;
+            if(set1.contains(s.charAt(i)))
+                return i;
         }
+        
         return -1;
     }
 }
