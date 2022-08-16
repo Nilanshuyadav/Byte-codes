dee75193f46e7fa0db41abcd4e4aa3a0cbe0a71b
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-16 10:45:48 +0530

    Time: 3 ms (97.79%), Space: 57.5 MB (43.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -6,23 +6,23 @@ class Solution {
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j] == '1'){
-                    function(grid,i,j,row,col);
+                    function(grid,i,j);
                     cnt++;
                 }
             }
         }
         return cnt;
     }
-    public void function(char[][] grid, int sr, int sc, int row, int col){
+    public void function(char[][] grid, int sr, int sc){
         grid[sr][sc] = '0';
         
         if(sr-1>=0 && grid[sr-1][sc] == '1')
-            function(grid,sr-1,sc,row,col);
-        if(sr+1<row && grid[sr+1][sc] == '1')
-            function(grid,sr+1,sc,row,col);
+            function(grid,sr-1,sc);
+        if(sr+1<grid.length && grid[sr+1][sc] == '1')
+            function(grid,sr+1,sc);
         if(sc-1>=0 && grid[sr][sc-1] == '1')
-            function(grid,sr,sc-1,row,col);
-        if(sc+1<col && grid[sr][sc+1]=='1')
-            function(grid,sr,sc+1,row,col);
+            function(grid,sr,sc-1);
+        if(sc+1<grid[0].length && grid[sr][sc+1]=='1')
+            function(grid,sr,sc+1);
     }
 }
