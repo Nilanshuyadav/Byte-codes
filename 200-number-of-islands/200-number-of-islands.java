36ade322946ad930b6ae58df0c0f439f5354831a
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-16 10:40:22 +0530

    Time: 7 ms (32.60%), Space: 56.4 MB (75.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,54 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int row = grid.length, col = grid[0].length;
-        int count = 0;
-        for(int i =0;i<row;i++){
-            for(int j = 0;j<col;j++){
+        int row = grid.length,col = grid[0].length;
+        int cnt = 0;
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j] == '1'){
-                    function(grid,i,j);
-                    count++;
+                    function(grid,i,j,row,col);
+                    cnt++;
                 }
             }
         }
-        
-        return count;
+        return cnt;
     }
-    
-    public void function(char[][] grid, int row, int col){
+    public void function(char[][] grid, int sr, int sc, int row, int col){
+        grid[sr][sc] = '0';
         
-        grid[row][col] = '0';
+        int[] r={-1,1,0,0}, c={0,0,-1,1};
         
-        int duplicate_row = row, duplicate_col = col;
-        int row_length = grid.length,col_length = grid[0].length;
-        
-        while(--row >= 0){
-            if(grid[row][col] == '1')
-                function(grid,row,col);
-            else break;
-        }
-        
-        row = duplicate_row;
-        
-        while(++row < row_length){
-            if(grid[row][col] == '1')
-                function(grid,row,col);
-            else break;
-        }
-        
-        row = duplicate_row;
-        
-        while(--col >=0){
-            if(grid[row][col] == '1')
-                function(grid,row,col);
-            else break;
-        }
-        
-        col = duplicate_col;
-        
-        while(++col < col_length){
-            if(grid[row][col] == '1')
-                function(grid,row,col);
-            else break;
+        for(int i=0;i<4;i++){
+            int ro = sr+r[i];
+            int co = sc+c[i];
+            
+            if(ro>=0 && ro<row && co>=0 && co<col && grid[ro][co] == '1')
+                function(grid,ro,co,row,col);
         }
     }
 }
