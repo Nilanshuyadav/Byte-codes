baa7df6ddbef438290f9d9dc46132c89a77c91fc
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 02:39:50 +0530

    Time: 2 ms (93.29%), Space: 67.9 MB (50.66%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,25 +1,15 @@
 class Solution {
-    Boolean[] dp;
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        dp = new Boolean[n];
+    public boolean canJump(int[] nums) 
+    {
+        int r=0;
         
-        
-        return function(nums,0,nums.length);
-    }
-    
-    public boolean function(int[] nums, int ind,int n){
-        if(ind == n-1) return dp[ind] = true;
-        if(dp[ind] != null) {return dp[ind];}
-        
-        boolean temp = false;
-        
-        for(int i=1;i<=nums[ind];i++){
-            if(ind+i >= n || i==0) break;
-            temp = temp || function(nums,ind+i,n);
-        }    
-        
-        dp[ind] = temp;
-        return temp;
+        for(int i=0; i<nums.length;i++)
+        {
+            if(i > r)
+                return false;
+            
+            r = Math.max(r , i + nums[i]);
+        }
+        return true;
     }
 }
