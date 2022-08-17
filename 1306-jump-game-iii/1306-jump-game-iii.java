c4e91994b027ddd11e333c319a97056fa5c952de
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 04:13:19 +0530

    Time: 14 ms (26.65%), Space: 59.9 MB (70.06%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,26 +1,21 @@
 class Solution {
-    Boolean[] V;
-    boolean[] vis;
     public boolean canReach(int[] arr, int start) {
         int n = arr.length;
-        V = new Boolean[n];
-        vis = new boolean[n];
-        return function(arr,start,n);
-    }
-    
-    public boolean function(int[] arr, int start, int n){
-        if(arr[start] == 0) return true;
-        if(V[start] != null) return V[start];
-        if(vis[start] == true) return false;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        
+        q.add(start);
         vis[start] = true;
         
-        boolean temp1 = false,temp2 = false;
+        while(!q.isEmpty()){
+            int temp = q.remove();
+            if(arr[temp] == 0) return true;
+            int prev = temp-arr[temp],next = temp+arr[temp];
+            
+            if(prev >= 0 && !vis[prev]){vis[prev] = true; q.add(prev);}
+            if(next < n && !vis[next]){vis[next] = true; q.add(next);}
+        }
         
-        if(start+arr[start] < n) temp1 = function(arr,start+arr[start],n);
-        
-        if(start - arr[start] >= 0) temp2 = function(arr,start-arr[start],n);
-        
-        
-        return V[start] = (temp1||temp2);
+        return false;
     }
 }
