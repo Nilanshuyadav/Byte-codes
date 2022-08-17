eb0a1bdeacd32860d72b46b4592ee206549c65f7
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-17 08:49:17 +0530

    Time: 17 ms (35.38%), Space: 55.5 MB (22.71%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,107 +1,61 @@
-class Solution { 
-    int[] r = {-1,1,0,0}, c = {0,0,-1,1};
-    
+class Solution {
     public int shortestBridge(int[][] grid) {
         int n = grid.length;
         
-        
+        Queue<Pair> q1 = new LinkedList<>(), q2 = new LinkedList<>();
         boolean[][] vis = new boolean[n][n];
-        
-        Queue<Pair> q = new LinkedList<>();
-        
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 1){
-                    
-                    function(grid,i,j,n,vis,q);
-                    i = n; j = n;
+                    q1.add(new Pair(i,j));
+                    i=n;
+                    j=n;
                 }
             }
-            
         }
         
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(grid[i][j] == 1){
-        //             vis[i][j] = true;
-        //             q.add(new int[]{i,j});
-        //         }
-        //     }
-        // }
+        while(!q1.isEmpty()){
+            Pair temp = q1.remove();
+            q2.add(temp);
+            
+            int r = temp.row,c = temp.col;
+            grid[r][c] = 0;
+            vis[r][c] = true;
+            
+            if(r-1 >=0 && !vis[r-1][c] && grid[r-1][c] == 1){vis[r-1][c] = true; q1.add(new Pair(r-1,c));}
+            if(r+1 < n && !vis[r+1][c] && grid[r+1][c] == 1){vis[r+1][c] = true; q1.add(new Pair(r+1,c));}
+            if(c-1 >=0 && !vis[r][c-1] && grid[r][c-1] == 1){vis[r][c-1] = true; q1.add(new Pair(r,c-1));}
+            if(c+1 < n && !vis[r][c+1] && grid[r][c+1] == 1){vis[r][c+1] = true; q1.add(new Pair(r,c+1));}
+        }
         
-//         while(!q.isEmpty()){
-//             Pair temp = q.remove();
-            
-//             int row = temp.row;
-//             int col = temp.col;
-            
-//             q2.add(new Pair(row,col));
-            
-//             vis[row][col] = true;
-//             grid[row][col] = 0;
-            
-//             if(row-1 >=0 && vis[row-1][col]!=true && grid[row-1][col] == 1)
-//                 q.add(new Pair(row-1,col));
-//             if(row+1 <n && vis[row+1][col]!=true && grid[row+1][col] == 1)
-//                 q.add(new Pair(row+1,col));
-//             if(col-1 >=0 && vis[row][col-1]!=true && grid[row][col-1] == 1)
-//                 q.add(new Pair(row,col-1));
-//             if(col+1 <n && vis[row][col+1]!=true && grid[row][col+1] == 1)
-//                 q.add(new Pair(row,col+1));
-//         }
+        int[] r = {-1,1,0,0}, c = {0,0,-1,1}; int step = 0;
         
-        
-        int dis = 0;
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            while(size-- > 0){
+        while(!q2.isEmpty()){
+            int size = q2.size();
+            for(int i=0;i<size;i++){
+                Pair temp = q2.remove();
                 
-                Pair temp = q.remove();
-                
-                for(int i=0;i<4;i++){
-                    int tr = temp.row + r[i], tc = temp.col +
-                        c[i];
+                for(int j=0;j<4;j++){
+                    int tr = temp.row+r[j], tc = temp.col+c[j];
                     
-                    if(tr >= 0 && tc >= 0
-                      && tr < n && tc < n
-                      && vis[tr][tc] != true){
-                        if(grid[tr][tc] == 1)
-                            return dis;
+                    if(tr>=0 && tc>=0 && tr<n && tc<n && !vis[tr][tc]){
+                        if(grid[tr][tc] == 1) return step;
                         vis[tr][tc] = true;
-                        q.add(new Pair(tr,tc));
+                        q2.add(new Pair(tr,tc));
                     }
                 }
             }
-            dis++;
+            step++;
         }
-        
-        return dis;
-    }
-    
-    public void function(int[][] grid, int row,int col,int n,boolean vis[][],Queue<Pair> q){
-
-        grid[row][col] = 0;
-        q.add(new Pair(row,col));
-        vis[row][col] = true;
-        for(int i=0;i<4;i++){
-            int tr = row+r[i], tc = col+c[i];
-            
-            if(tr >= 0 && tc >= 0
-              && tr < n && tc < n
-              && grid[tr][tc] == 1)
-                function(grid,tr,tc,n,vis,q);
-        }
+        return -1;
     }
 }
 
 class Pair{
     int row;
     int col;
-    
-    public Pair(int row,int col){
+    public Pair(int row, int col){
         this.row = row;
         this.col = col;
     }
