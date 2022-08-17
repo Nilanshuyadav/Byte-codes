46c8d3c4f9e467c776c286cc3d6f2ef431417ab6
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 04:25:58 +0530

    Time: 3 ms (99.24%), Space: 54.3 MB (88.39%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,38 +1,40 @@
 class Solution {
-    Boolean[] V;
+    
     boolean[] vis;
     public boolean canReach(int[] arr, int start) {
         int n = arr.length;
-        V = new Boolean[n];
+        
         vis = new boolean[n];
         return function(arr,start,n);
     }
     
     public boolean function(int[] arr, int start, int n){
-//         if(arr[start] == 0) return true;
-//         if(V[start] != null) return V[start];
-//         if(vis[start] == true) return false;
-//         vis[start] = true;
-        
-//         boolean temp1 = false,temp2 = false;
-        
-//         if(start+arr[start] < n) temp1 = function(arr,start+arr[start],n);
-        
-//         if(start - arr[start] >= 0) temp2 = function(arr,start-arr[start],n);
-        
-        
-//         return V[start] = (temp1||temp2);
-        
-        if(vis[start]) return false;
         if(arr[start] == 0) return true;
+        
+        if(vis[start] == true) return false;
         vis[start] = true;
-        int prev = start-arr[start],next = start+arr[start];
         
-        boolean temp1 = false, temp2 = false;
+        boolean temp1 = false,temp2 = false;
         
-        if(prev >= 0) temp1 = function(arr,prev,n);
-        if(next < n) temp2 = function(arr,next,n);
+        if(start+arr[start] < n) temp1 = function(arr,start+arr[start],n);
         
-        return temp1||temp2;
+        if(start - arr[start] >= 0) temp2 = function(arr,start-arr[start],n);
+        
+        
+        return (temp1||temp2);
+        
+        /********************************************************************/
+        
+//         if(vis[start]) return false;
+//         if(arr[start] == 0) return true;
+//         vis[start] = true;
+//         int prev = start-arr[start],next = start+arr[start];
+        
+//         boolean temp1 = false, temp2 = false;
+        
+//         if(prev >= 0) temp1 = function(arr,prev,n);
+//         if(next < n) temp2 = function(arr,next,n);
+        
+//         return temp1||temp2;
     }
 }
