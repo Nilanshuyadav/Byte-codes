5347cc103b7b9843f5620a77033745d8ec5675b3
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-17 15:29:56 +0530

    Time: 3 ms (84.63%), Space: 47.2 MB (58.83%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,27 +1,30 @@
 class Solution {
+    int ans;
     public int maxAreaOfIsland(int[][] grid) {
         int row = grid.length,col = grid[0].length;
-        int max,ans = 0;
+        int max=0;
+        ans = 0;
         for(int  i=0;i<row;i++){
             for(int j =0 ;j<col;j++){
                 if(grid[i][j] == 1){
-                    max = function(grid,i,j);
-                    if(max > ans)ans = max;
+                    max = Math.max(max,function(grid,i,j));
+                    ans = 0;
                 } 
             }
         }
-        return ans;
+        return max;
     }
     
     public int function(int[][] grid,int row,int col){
-        int sum = 1;
+        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0) return 0;
+        
+        ans++;
         grid[row][col] = 0;
+        function(grid,row-1,col);
+        function(grid,row+1,col);
+        function(grid,row,col-1);
+        function(grid,row,col+1);
         
-        if(row-1 >=0 && grid[row-1][col] == 1) sum += function(grid,row-1,col);
-        if(row+1 <grid.length && grid[row+1][col] == 1) sum += function(grid,row+1,col);
-        if(col-1 >=0 && grid[row][col-1] == 1) sum += function(grid,row,col-1);
-        if(col+1 < grid[0].length && grid[row][col+1] == 1) sum += function(grid,row,col+1);
-        
-        return sum;
+        return ans;
     }
 }
