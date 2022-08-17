f4e2b05fa1f1f984d219138157a475a6c2aefce2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 03:41:04 +0530

    Time: 47 ms (39.98%), Space: 49.7 MB (32.46%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,27 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] vis = new boolean[n];
-        if(n==1) return 0;
-        q.add(0);
-        int step = 1;
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-                int temp = q.remove();
-                
-                for(int i =0;i<=nums[temp];i++){
-                    if(temp+i >= n) break;
-                    if(temp+i == n-1) return step;
-                    if(!vis[temp+i]){
-                        vis[temp+i] = true;
-                        q.add(temp+i);
-                    }
-                }
+        int[] dp = new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        if(n<=1) return 0;
+        for(int i=0;i<n;i++){
+            for(int k = i+1;k<=i+nums[i] && k<n ; k++){
+                dp[k] = Math.min(dp[k],dp[i]+1);
             }
-            step++;
         }
-        return 0;
+        
+        return dp[n-1];
     }
 }
