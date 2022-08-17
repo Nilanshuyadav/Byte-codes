ec60df5be2980d686acd265ac204b9e9a32de2d2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-17 15:20:51 +0530

    Time: 3 ms (84.63%), Space: 47.8 MB (31.02%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,53 +1,26 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int row = grid.length, col = grid[0].length;
-        int max = 0;
-        
-        for(int i =0;i<row;i++){
-            for(int j = 0;j<col;j++){
+        int row = grid.length,col = grid[0].length;
+        int max,ans = 0;
+        for(int  i=0;i<row;i++){
+            for(int j =0 ;j<col;j++){
                 if(grid[i][j] == 1){
-                    max = Math.max(max,function(grid,i,j));
-                }    
+                    max = function(grid,i,j);
+                    if(max > ans)ans = max;
+                } 
             }
         }
-        
-        return max;
+        return ans;
     }
     
-    public int function(int[][] grid, int row, int col){
-        int duplicate_row = row, duplicate_col = col;
+    public int function(int[][] grid,int row,int col){
+        int sum = 1;
         grid[row][col] = 0;
-        int sum=1;
         
-        while(--row >= 0){
-            if(grid[row][col] == 1)
-                sum += function(grid,row,col);
-            else break;
-        }
-        
-        row = duplicate_row;
-        
-        while(++row < grid.length){
-            if(grid[row][col] == 1)
-                sum += function(grid,row,col);
-            else break;
-        }
-        
-        row = duplicate_row;
-        
-        while(--col >= 0){
-            if(grid[row][col] == 1)
-                sum += function(grid,row,col);
-            else break;
-        }
-        
-        col = duplicate_col;
-        
-        while(++col < grid[0].length){
-            if(grid[row][col] == 1)
-                sum += function(grid,row,col);
-            else break;
-        }
+        if(row-1 >=0 && grid[row-1][col] == 1) sum += function(grid,row-1,col);
+        if(row+1 <grid.length && grid[row+1][col] == 1) sum += function(grid,row+1,col);
+        if(col-1 >=0 && grid[row][col-1] == 1) sum += function(grid,row,col-1);
+        if(col+1 < grid[0].length && grid[row][col+1] == 1) sum += function(grid,row,col+1);
         
         return sum;
     }
