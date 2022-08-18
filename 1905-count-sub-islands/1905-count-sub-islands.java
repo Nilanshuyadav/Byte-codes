897d4d506d9671b883364eec8565be23a1092131
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 16:10:01 +0530

    Time: 28 ms (77.26%), Space: 126 MB (66.14%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,39 +1,27 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int row = grid2.length, col = grid2[0].length;
-        int count = 0;
+        int cnt = 0,row = grid2.length,col = grid2[0].length;
         
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid2[i][j] == 1 && function(grid2,i,j,grid1)){
-                    count++;
+            for(int j =0;j<col;j++){
+                if(grid2[i][j] == 1 && function(grid1,grid2,i,j)){
+                    cnt++;
                 }
             }
         }
-        
-        return count;
+        return cnt;
     }
     
-    public boolean function(int[][] grid2, int row, int col,int[][] grid1){
+    public boolean function(int[][] grid1, int[][] grid2, int row, int col){
         boolean temp = true;
+        if(grid2[row][col] != grid1[row][col])temp = false;
         
         grid2[row][col] = 0;
         
-        if(row-1 >= 0 && grid2[row-1][col] == 1)
-            temp = function(grid2,row-1,col,grid1) && temp;
-        
-        if(row+1 < grid2.length && grid2[row+1][col] == 1)
-            temp = function(grid2,row+1,col,grid1) && temp;
-        
-        if(col-1 >= 0 && grid2[row][col-1] == 1)
-            temp = function(grid2,row,col-1,grid1) && temp;
-        
-        if(col+1<grid2[0].length && grid2[row][col+1] ==1)
-            temp = function(grid2,row,col+1,grid1) && temp;
-        
-        if(1 != grid1[row][col]){
-            return false;
-        }
+        if(row-1>=0 && grid2[row-1][col]==1) temp = function(grid1,grid2,row-1,col) && temp;
+        if(row+1<grid1.length && grid2[row+1][col]==1) temp = function(grid1,grid2,row+1,col) && temp;
+        if(col-1>=0 && grid2[row][col-1]==1) temp = function(grid1,grid2,row,col-1) && temp;
+        if(col+1<grid1[0].length && grid2[row][col+1]==1) temp = function(grid1,grid2,row,col+1) && temp;
         
         return temp;
     }
