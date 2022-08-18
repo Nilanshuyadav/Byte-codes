4e0f9413929e23551da93e949a4d45d1e3f97602
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-18 15:52:09 +0530

    Time: 8 ms (53.03%), Space: 65.6 MB (51.26%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,63 +1,37 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int row = grid.length, col = grid[0].length;
+        int row = grid.length,col = grid[0].length;
         
         for(int i=0;i<row;i++){
             if(grid[i][0] == 1) function(grid,i,0);
             if(grid[i][col-1] == 1) function(grid,i,col-1);
         }
         
-        for(int i = 0;i<col;i++){
+        for(int i=0;i<col;i++){
             if(grid[0][i] == 1) function(grid,0,i);
             if(grid[row-1][i] == 1) function(grid,row-1,i);
         }
         
-        int sum = 0;
+        int ans = 0;
         
-        for(int i =0;i<row;i++){
+        for(int i=0;i<row;i++){
             for(int j =0;j<col;j++){
-                if(grid[i][j] == 1)
-                    sum += function2(grid,i,j);
+                if(grid[i][j] == 1){
+                    ans += function(grid,i,j);
+                }
             }
         }
-        
-        return sum;
+        return ans;
     }
     
-    public void function(int[][] grid, int row, int col){
-        
+    public int function(int[][] grid,int row,int col){
+        int area = 1;
         grid[row][col] = 0;
+        if(row-1>=0 && grid[row-1][col] == 1)area += function(grid,row-1,col);
+        if(row+1<grid.length && grid[row+1][col] == 1) area += function(grid,row+1,col);
+        if(col-1>=0 && grid[row][col-1] == 1)area += function(grid,row,col-1);
+        if(col+1<grid[0].length && grid[row][col+1] == 1)area += function(grid,row,col+1);
         
-        if(row-1>=0 && grid[row-1][col] == 1)
-            function(grid,row-1,col);
-        
-        if(row+1<grid.length && grid[row+1][col] == 1)
-            function(grid,row+1,col);
-        
-        if(col-1>=0 && grid[row][col-1] == 1)
-            function(grid,row,col-1);
-        
-        if(col+1<grid[0].length && grid[row][col+1] == 1)
-            function(grid,row,col+1);
-    }
-    
-    public int function2(int[][] grid, int row, int col){
-        int sum = 1;
-        
-        grid[row][col] = 0;
-        
-        if(row-1>=0 && grid[row-1][col] == 1)
-            sum += function2(grid,row-1,col);
-        
-        if(row+1<grid.length && grid[row+1][col] == 1)
-            sum += function2(grid,row+1,col);
-        
-        if(col-1>=0 && grid[row][col-1] == 1)
-            sum += function2(grid,row,col-1);
-        
-        if(col+1<grid[0].length && grid[row][col+1] == 1)
-            sum += function2(grid,row,col+1);
-        
-        return sum;
+        return area;
     }
 }
