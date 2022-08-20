2aba1c1588720fc0a6fd3631472f169593a12212
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-21 04:22:10 +0530

    Time: 16 ms (36.19%), Space: 54.3 MB (81.14%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,109 +1,89 @@
 class Solution {
-    Pair[][] grid;
-    
     public List<List<Integer>> pacificAtlantic(int[][] height) {
-        int row = height.length, col = height[0].length;
+        int row = height.length,col = height[0].length;
+        int[][] arr = new int[row][col];
+        for(int i[] : arr)
+            Arrays.fill(i,-1);
+        
         List<List<Integer>> ans = new ArrayList<>();
         
-        grid = new Pair[row][col];
+        int[] r = {-1,0,1,0}, c={0,1,0,-1};
+        
+        Queue<Pair> q = new LinkedList<>();
         
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j] != null && grid[i][j].pacific == true && grid[i][j].atlantic == true)
-                    ans.add(new ArrayList(Arrays.asList(i,j)));
-                else{
-                    if(function(height,i,j))
-                        ans.add(new ArrayList(Arrays.asList(i,j)));
-                }
-            }
+            q.add(new Pair(i,0));            
+            arr[i][0] = 1;
+        }
+        
+        for(int i=0;i<col;i++){
+             q.add(new Pair(0,i));
+             arr[0][i] = 1;
+        }
+        
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
             
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || arr[new_row][new_col] != -1 || height[temp.row][temp.col]>height[new_row][new_col]) continue;
+                
+                arr[new_row][new_col] = 1;
+                q.add(new Pair(new_row,new_col));
+            }
+        }
+
+        for(int i=0;i<row;i++){
+            if(arr[i][col-1] == 1){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                temp.add(col-1);
+                ans.add(temp);
+            }
+            q.add(new Pair(i,col-1));
+            arr[i][col-1] = 2;
+            
+        }
+        
+        for(int i=0;i<col;i++){
+            if(arr[row-1][i] == 1){
+                List<Integer> al = new ArrayList<>();
+                al.add(row-1);
+                al.add(i);
+                ans.add(al);
+            }            
+             q.add(new Pair(row-1,i));
+             arr[row-1][i] =  2;
+        }
+        
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
+            
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col||arr[new_row][new_col]==2 || height[temp.row][temp.col]>height[new_row][new_col]) continue;
+                
+                 if(arr[new_row][new_col] == 1) ans.add(new ArrayList<>(){{add(new_row); add(new_col);}});
+                
+                
+                arr[new_row][new_col] = 2;
+                q.add(new Pair(new_row,new_col));
+            }
         }
         
         return ans;
     }
-    
-    public boolean function(int[][] height,int row,int col){
-        return pacific(height,row,col) && atlantic(height,row,col);
-    }
-    
-    public boolean pacific(int[][] height, int row, int col){
-        if(row == 0 || col == 0) return true;
-        
-        boolean pacific = false;
-        int present_value = height[row][col];
-        height[row][col] = -1;
-        
-        if(row-1 >=0 && height[row-1][col] != -1 && height[row-1][col] <= present_value)
-            pacific = pacific || pacific(height,row-1,col);
-        
-        if(row+1 <height.length && height[row+1][col] != -1 && height[row+1][col] <= present_value)
-            pacific = pacific || pacific(height,row+1,col);
-        
-        if(col-1 >=0 && height[row][col-1] != -1 && height[row][col-1] <= present_value)
-            pacific = pacific || pacific(height,row,col-1);
-        
-        if(col+1 <height[0].length && height[row][col+1] != -1 && height[row][col+1] <= present_value)
-            pacific = pacific || pacific(height,row,col+1);
-        
-        height[row][col] = present_value;
-        
-        if(grid[row][col] == null){
-            grid[row][col] = new Pair(true,pacific,false);
-        }
-        else{
-            grid[row][col].visited = true;
-            grid[row][col].pacific = pacific;
-        }
-        
-//         grid[row][col].visited = true;
-//         grid[row][col].pacific = pacific;
-        
-        return pacific;
-    }
-    
-    
-    
-    public boolean atlantic(int[][] height, int row, int col){
-        if(row == height.length-1 || col == height[0].length-1) return true;
-        
-        boolean atlantic = false;
-        int present_value = height[row][col];
-        height[row][col] = -1;
-        
-        if(row-1 >=0 && height[row-1][col] != -1 && height[row-1][col] <= present_value)
-            atlantic = atlantic || atlantic(height,row-1,col);
-        
-        if(row+1 <height.length && height[row+1][col] != -1 && height[row+1][col] <= present_value)
-            atlantic = atlantic || atlantic(height,row+1,col);
-        
-        if(col-1 >=0 && height[row][col-1] != -1 && height[row][col-1] <= present_value)
-            atlantic = atlantic || atlantic(height,row,col-1);
-        
-        if(col+1 <height[0].length && height[row][col+1] != -1 && height[row][col+1] <= present_value)
-            atlantic = atlantic || atlantic(height,row,col+1);
-        
-        height[row][col] = present_value;
-        
-        if(grid[row][col] == null){
-            grid[row][col] = new Pair(true,false,atlantic);
-        }
-        else{
-            grid[row][col].visited = true;
-            grid[row][col].atlantic = atlantic;
-        }
-        
-        return atlantic;
-    }
 }
 
-
-
 class Pair{
-    boolean visited = false,atlantic = false,pacific = false;
-    
-    public Pair(boolean visited, boolean pacific,boolean atlantic){
-        this.visited = visited;
-        this.pacific = pacific;
-        this.atlantic = atlantic;
+    int row;
+    int col;
+    public Pair(int row,int col){
+        this.row = row;
+        this.col = col;
     }
 }
