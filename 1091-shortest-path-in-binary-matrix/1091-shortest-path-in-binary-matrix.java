e9f9401ddaa2e5eb373687da448b86bdfcd9dcdc
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-21 01:13:18 +0530

    Time: 31 ms (48.85%), Space: 59.4 MB (65.59%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,42 +1,47 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
         int n = grid.length;
+        boolean[][] vis = new boolean[n][n];
         
-        if(n == 1 && grid[0][0] == 0) return 1;
+        int[] r = {-1,-1,0,1,1,1,0,-1} , c = {0,1,1,1,0,-1,-1,-1};
         
+        if(n==1 && grid[0][0] == 0) return 1;
+        
+        Queue<Pair> q = new LinkedList<>();
         if(grid[0][0] == 0)
             q.add(new Pair(0,0));
-        
-        int k = 1;
-        
-        int[] r = new int[]{-1,-1,0,1,1,1,0,-1}, c = new int[]{0,1,1,1,0,-1,-1,-1};
+        int cnt = 1;
         
         while(!q.isEmpty()){
             int size = q.size();
             
-            k++;
-            
-            while(size-- > 0){
-                Pair<Integer,Integer> temp = q.remove();
+            for(int j=0;j<size;j++){
+                Pair temp = q.remove();
                 
                 for(int i=0;i<8;i++){
-                    int row = temp.getKey() + r[i];
-                    int col = temp.getValue() + c[i];
+                    int new_row = temp.row + r[i];
+                    int new_col = temp.col + c[i];
                     
-                    if(row == n-1 && col == n-1 && grid[row][col] == 0) return k;
+                    if(new_row<0 || new_col<0 || new_row>=n || new_col>=n || vis[new_row][new_col]==true || grid[new_row][new_col]==1) continue;
                     
-                    if(row >= 0 && row < n
-                      && col >= 0 && col < n
-                      && grid[row][col] == 0){
-                        q.add(new Pair(row,col));
-                        grid[row][col] = 1;
-                    }    
+                    if(new_row == n-1 && new_col == n-1) return cnt+1;
+                    
+                    q.add(new Pair(new_row,new_col));
+                    vis[new_row][new_col] = true;
                 }
-                
             }
+            cnt++;
         }
         
         return -1;
+    }
+}
+
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
