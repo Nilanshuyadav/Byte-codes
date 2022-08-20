7619fb6e8419b21b1ec2f0ee1631384d385ae6ab
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-21 02:01:16 +0530

    Time: 19 ms (62.29%), Space: 73.2 MB (36.04%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,47 +1,46 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int row = mat.length,col = mat[0].length;
+        int row = mat.length, col = mat[0].length;
         int ans[][] = new int[row][col];
         
-        for(int i[] : ans)
+        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
+        
+        for(int[] i : ans)
             Arrays.fill(i,Integer.MAX_VALUE);
         
         Queue<Pair> q = new LinkedList<>();
         
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++)
+            for(int j=0;j<col;j++){
                 if(mat[i][j] == 0){
                     ans[i][j] = 0;
                     q.add(new Pair(i,j));
-                }
+                }    
+            }
         }
-        
-        int[] r = {-1,1,0,0}, c = {0,0,-1,1};
         
         while(!q.isEmpty()){
             Pair temp = q.remove();
+            
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
                 
-                for(int i=0;i<4;i++){
-                    int tr = temp.r + r[i];
-                    int tc = temp.c + c[i];
-                    
-                    if(tr >= 0 && tr<row && tc >=0 && tc<col &&                          ans[tr][tc] == Integer.MAX_VALUE){
-                        ans[tr][tc] = 1 + ans[temp.r][temp.c];
-                        q.add(new Pair(tr,tc));
-                    }
-                }
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || ans[new_row][new_col] != Integer.MAX_VALUE) continue;
+                
+                ans[new_row][new_col] = 1+ans[temp.row][temp.col];
+                q.add(new Pair(new_row,new_col));
+            }
         }
-        
         return ans;
     }
 }
 
 class Pair{
-    int r;
-    int c;
-    
-    public Pair(int r,int c){
-        this.r = r;
-        this.c = c;
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
