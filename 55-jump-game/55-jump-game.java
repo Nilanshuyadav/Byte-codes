457ab5bab48ab5321ca2871dbacde5c9618664aa
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-21 23:34:46 +0530

    Time: 3 ms (67.51%), Space: 66.8 MB (81.31%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,33 +1,12 @@
 class Solution {
-    public boolean canJump(int[] nums) 
-    {
-        int n = nums.length;
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] vis = new boolean[n];
-        int tep = -1;
-        for(int i=0;i<n;i++)
-            if(nums[i] == 0){
-                tep = 0;
-                break;
-            }
+    public boolean canJump(int[] nums) {
+        int max = 0,n = nums.length;
+        for(int i=0;i<n;i++){
+            if(i>max) return false; 
+            max = Math.max(max,i+nums[i]);
+           // if(max>=n) return true;
+        }    
         
-        if(tep == -1) return true;
-            
-        
-        q.add(0);
-        
-        while(!q.isEmpty()){
-            int temp = q.remove();
-            
-            for(int i=0;i<=nums[temp];i++){
-                if(temp+i>=n) break;
-                if(temp+i == n-1) return true;
-                if(!vis[temp+i]){
-                    vis[temp+i] = true;
-                    q.add(temp+i);
-                }
-            }
-        }
-        return false;
+        return true;
     }
 }
