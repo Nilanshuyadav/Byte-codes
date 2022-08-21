b721260e5b243f26005fcc9a1ed81af1ca2f5ef0
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-22 01:53:41 +0530

    Time: 6 ms (96.50%), Space: 71.6 MB (43.94%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,42 +1,29 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
-        int cnt = 1,row = maze.length,col = maze[0].length;
+        int row = maze.length, col = maze[0].length, level = -1;
+        int[] r ={1,0,-1,0}, c = {0,1,0,-1};
         Queue<int[]> q = new LinkedList<>();
-        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
-        boolean[][] vis = new boolean[row][col];
-        vis[entrance[0]][entrance[1]] = true;
-        
         q.add(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
         
         while(!q.isEmpty()){
             int size = q.size();
-            
+            level++;
             for(int j=0;j<size;j++){
                 int[] temp = q.remove();
-
+                
                 for(int i=0;i<4;i++){
                     int new_row = temp[0]+r[i];
                     int new_col = temp[1]+c[i];
                     
-                    if(new_row<0 || new_col<0 || new_row>=row || new_col>=col){
-                        if(temp[0] == entrance[0] && temp[1]==entrance[1]) continue;
-                        else return cnt-1;
-                    }
-
-                    if(maze[new_row][new_col] == '+') continue;
-                    
-                    if(new_row>=0 && new_col>=0 && new_row<row && new_col<col && !vis[new_row][new_col]){
-                        vis[new_row][new_col] = true;
+                    if((new_row<0 || new_col<0 || new_row>=row || new_col>=col) && level!=0) return level;
+                    if((new_row>=0 && new_col>=0 && new_row<row && new_col<col) && maze[new_row][new_col] == '.'){
                         q.add(new int[]{new_row,new_col});
+                        maze[new_row][new_col] = '+';
                     }
-                    
-
-
                 }
             }
-            cnt++;
         }
-        
         return -1;
     }
 }
