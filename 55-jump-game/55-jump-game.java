d091fcafd6cb8fc7e2ae4364ddffc3680197e532
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-21 23:35:15 +0530

    Time: 3 ms (67.51%), Space: 66.8 MB (82.03%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<n;i++){
             if(i>max) return false; 
             max = Math.max(max,i+nums[i]);
-           // if(max>=n) return true;
+            if(max>=n) return true;
         }    
         
         return true;
