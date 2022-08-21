dc71a22d2cd8640a3b5a6621132c5ba90756b3c9
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-22 02:47:20 +0530

    Time: 10 ms (82.40%), Space: 54.2 MB (60.62%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,39 +1,41 @@
 class Solution {
-    Queue<int[]> q;
     int[] r = {-1,0,1,0}, c = {0,1,0,-1};
     public int shortestBridge(int[][] grid) {
-        q = new LinkedList<>();
-        int n = grid.length;
+       int n = grid.length;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 1){
-                    q.add(new int[]{i,j});
                     function(grid,i,j);
-                    i=n;
-                    j=n;
-                }    
-                    
+                    i = n;
+                    j = n;
+                }
             }
         }
         
-        int cnt =0 ;
+        Queue<int[]> q = new LinkedList<>();
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1){grid[i][j] = -1; q.add(new int[]{i,j});}
+            }
+        
+        int cnt = -1;
         while(!q.isEmpty()){
-            cnt++;
             int size = q.size();
-            
-            for(int j=0;j<size;j++){
+            cnt++;
+            for(int i=0;i<size;i++){
                 int[] temp = q.remove();
-                for(int i=0;i<4;i++){
-                    int new_row = temp[0]+r[i];
-                    int new_col = temp[1]+c[i];
+                for(int j=0;j<4;j++){
+                    int nr = temp[0]+r[j];
+                    int nc = temp[1]+c[j];
                     
-                    if(new_row<0 || new_row>=n || new_col<0 || new_col>=n) continue;
-                    if(grid[new_row][new_col] == 1) return cnt-1;
-                    if(grid[new_row][new_col] == 0) {
-                        q.add(new int[]{new_row,new_col});
-                        grid[new_row][new_col] = 2;
-                    }                                 
+                    if(nr<0 || nc<0 || nr>=n || nc>=n) continue;
+                    if(grid[nr][nc] == 2) return cnt;
+                    if(grid[nr][nc] == 0){
+                        grid[nr][nc] = -1;
+                        q.add(new int[]{nr,nc});
+                    }
                 }
             }
         }
@@ -41,18 +43,14 @@ class Solution {
     }
     
     public void function(int[][] grid, int row, int col){
-        grid[row][col] = 0;
+        grid[row][col] = 2;
         int n = grid.length;
-        
         for(int i=0;i<4;i++){
-            int new_row = row+r[i];
-            int new_col = col+c[i];
+            int nr = row+r[i];
+            int nc = col+c[i];
             
-            if(new_row<0 || new_col<0 || new_row>=n || new_col>=n) continue;
-            if(grid[new_row][new_col] != 0){
-                q.add(new int[]{new_row,new_col});
-                function(grid,new_row,new_col);
-            }
+            if(nr<0 || nc<0 || nr>=n || nc>=n) continue;
+            if(grid[nr][nc] == 1) function(grid,nr,nc);
         }
     }
 }
