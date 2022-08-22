ae6883fae4c65d5769f7852ce06911af55d41ab8
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-22 05:50:59 +0530

    Time: 5 ms (53.32%), Space: 55 MB (67.09%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,27 +1,36 @@
 class Solution {
     List<List<Integer>> ans;
-    
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int n = graph.length;
         ans = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<>());
         
-        function(graph, 0,new ArrayList<Integer>());
+        for(int j=0;j<n;j++)
+            for(int i : graph[j])
+                adj.get(j).add(i);
+        
+        List<Integer> al = new ArrayList<>();
+        al.add(0);
+        function(adj,0,n,al);
         
         return ans;
     }
     
-    public void function(int[][] graph, int ind, List<Integer> al){
-        al.add(ind);
-        
-        if(ind == graph.length-1){
-            ans.add(new ArrayList<>(al));
-            al.remove(al.size()-1);
-            return;
+    public void function(List<List<Integer>> adj, int ind,int n,List<Integer> al){
+        for(int i : adj.get(ind)){
+            if(i == n-1){
+                al.add(i);
+                ans.add(new ArrayList<>(al));
+                al.remove(al.size()-1);
+                continue;
+            }
+            else{
+                al.add(i);
+                function(adj,i,n,al);
+                al.remove(al.size()-1);
+            }
         }
-        
-        for(int i : graph[ind]){
-            function(graph,i,al);
-        }
-        
-        al.remove(al.size()-1);
-    } 
+    }
 }
