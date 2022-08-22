b1ffac43ca425d1e51b84f8ef3cdeb23fbe8eeea
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-22 06:19:16 +0530

    Time: 2 ms (65.34%), Space: 41.4 MB (50.03%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1) return true;
+        while(n%4 == 0 && n!=0){
+            n = n/4;
+            if(n==1) return true;
+        }
+        return false;
+    }
+}
