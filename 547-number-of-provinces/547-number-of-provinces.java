db42d07afe1119148898f488714747dff01a6bde
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-24 01:28:35 +0530

    Time: 4 ms (35.02%), Space: 53.4 MB (82.36%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,35 +1,37 @@
 class Solution {
+    boolean vis[];
     public int findCircleNum(int[][] isConnected) {
-        Queue<Integer> q = new LinkedList<>();
-        List<List<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        boolean[] vis = new boolean[n+1];
+        vis= new boolean[n+1];
+        List<List<Integer>> adj = new ArrayList<>();
         
-        for(int i =0;i<=n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j) continue;
-                if(isConnected[i][j] == 1) adj.get(i+1).add(j+1);
+                if(isConnected[i][j] == 1)
+                    adj.get(i+1).add(j+1);
             }
         }
         
-        int cnt = 0;
+        int ans = 0;
         for(int i=1;i<=n;i++){
             if(!vis[i]){
-                cnt++;
                 vis[i] = true;
-                q.add(i);
-                while(!q.isEmpty()){
-                    int temp = q.remove();
-                    for(int j : adj.get(temp))
-                        if(!vis[j]){
-                            vis[j] = true;
-                            q.add(j);
-                        }
-                }
+                ans++;
+                function(adj,i);
             }
-        }
-        return cnt;
+        }    
+        
+        return ans;
+    }
+    
+    public void function(List<List<Integer>> adj, int ind){
+        for(int i : adj.get(ind))
+            if(!vis[i]){
+                vis[i] = true;
+                function(adj,i);
+            }
     }
 }
