785e91e293a1eddba810f7963468d026b3c72c59
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-24 01:49:49 +0530

    Time: 22 ms (58.92%), Space: 80.6 MB (13.16%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,36 +1,32 @@
 class Solution {
+    boolean[] vis;
     public int makeConnected(int n, int[][] connections) {
         int wire = connections.length;
-        
         if(wire < n-1) return -1;
         
-        boolean[] vis = new boolean[n];
+        vis = new boolean[n];
         List<List<Integer>> adj = new ArrayList<>();
-        
         for(int i=0;i<n;i++)adj.add(new ArrayList<>());
         for(int i[] : connections){
             adj.get(i[0]).add(i[1]);
             adj.get(i[1]).add(i[0]);
         }
         
-        Queue<Integer> q = new LinkedList<>();
-        
         int ans = -1;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
             if(!vis[i]){
                 ans++;
-                vis[i] = true;
-                q.add(i);
-                while(!q.isEmpty()){
-                    int temp = q.remove();
-                    for(int ind : adj.get(temp))
-                        if(!vis[ind]){
-                            vis[ind] = true;
-                            q.add(ind);
-                        }
-                }
+                function(adj,i);
             }
-        }
+        
         return ans;
+    }
+    
+    public void function(List<List<Integer>> adj , int ind){
+        for(int i : adj.get(ind))
+            if(!vis[i]){
+                vis[i] = true;
+                function(adj,i);
+            }
     }
 }
