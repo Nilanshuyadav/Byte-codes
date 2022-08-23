6db9a002c0b947f7585257dc9721f12eef3b5304
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-24 01:44:43 +0530

    Time: 31 ms (39.31%), Space: 77.6 MB (21.15%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,36 +1,36 @@
 class Solution {
-    boolean[] vis;
     public int makeConnected(int n, int[][] connections) {
         int wire = connections.length;
-        if(wire<n-1) return -1;
         
+        if(wire < n-1) return -1;
+        
+        boolean[] vis = new boolean[n];
         List<List<Integer>> adj = new ArrayList<>();
-        vis = new boolean[n];
         
-        for(int i=0;i<n;i++)
-            adj.add(new ArrayList<>());
+        for(int i=0;i<n;i++)adj.add(new ArrayList<>());
+        for(int i[] : connections){
+            adj.get(i[0]).add(i[1]);
+            adj.get(i[1]).add(i[0]);
+        }
         
-        for(int i=0;i<wire;i++){
-            adj.get(connections[i][0]).add(connections[i][1]);
-            adj.get(connections[i][1]).add(connections[i][0]);
-        }    
+        Queue<Integer> q = new LinkedList<>();
         
-       
-        int cnt = 0;
-        
+        int ans = -1;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                cnt++;
-                function(adj,i);
+                ans++;
+                vis[i] = true;
+                q.add(i);
+                while(!q.isEmpty()){
+                    int temp = q.remove();
+                    for(int ind : adj.get(temp))
+                        if(!vis[ind]){
+                            vis[ind] = true;
+                            q.add(ind);
+                        }
+                }
             }
         }
-        return cnt-1;
-    }
-    
-    public void function(List<List<Integer>> adj, int ind){
-        vis[ind] = true;
-        for(int i : adj.get(ind))
-            if(!vis[i])
-                function(adj,i);
+        return ans;
     }
 }
