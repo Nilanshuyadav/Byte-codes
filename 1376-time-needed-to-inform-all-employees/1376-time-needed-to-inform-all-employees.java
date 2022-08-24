7a7c284a77b99bb2db11e560b91fc13d2838611e
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-25 02:07:35 +0530

    Time: 107 ms (78.31%), Space: 121.4 MB (70.23%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,48 +1,22 @@
 class Solution {
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        Queue<Integer> q = new LinkedList<>();        
+    public int numOfMinutes(int n, int headID, int[] manager, int[] it) {
         List<List<Integer>> adj = new ArrayList<>();
         
-        for(int i=0;i<n;i++)
-            adj.add(new ArrayList<>());
-        
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
         for(int i=0;i<n;i++){
-            if(manager[i] == -1) {continue;}
+            if(i == headID) continue;
             adj.get(manager[i]).add(i);
         }
         
-//         int val = -1,cnt = 0,max = Integer.MIN_VALUE;
-        
-//         while(!q.isEmpty()){
-//             int size = q.size();
-//             max = Integer.MIN_VALUE;
-            
-//             while(size-->0){
-//                 int temp = q.remove();
-//                 if(informTime[temp] > max){
-//                     max = informTime[temp];
-//                     val = temp;
-//                 }
-//                 for(int i : adj.get(val))
-//                     q.add(i);
-//             }
-//             cnt += max;
-//         }
-        
-//         return cnt;
-        
-        
-        return function(headID,adj,informTime);
-        
+        return function(headID,it,adj);
     }
     
-    
-    public int function(int ind,List<List<Integer>> adj, int[] it){
+    public int function(int ind,int[] it, List<List<Integer>>adj){
+        int max = 0;
         
-        int max = 0;        
-        for(int i : adj.get(ind))
-            max = Math.max(max, it[ind] + function(i,adj,it));
-        
-        return max;
+        for(int i : adj.get(ind)){
+            max = Math.max(max,function(i,it,adj));
+        }
+        return it[ind]+max;
     }
 }
