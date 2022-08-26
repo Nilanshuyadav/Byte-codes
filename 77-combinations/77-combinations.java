79f7dc4dddbba463b4d4576b627994b11ac60e9c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-27 01:43:09 +0530

    Time: 37 ms (49.58%), Space: 55.5 MB (25.52%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> ans;
+    public List<List<Integer>> combine(int n, int k) {
+        ans = new ArrayList<>();
+        function(1,n,k,new ArrayList<Integer>());
+        return ans;
+    }
+    
+    public void function(int ind, int n, int k, List<Integer> al){
+        if(al.size() == k){
+            ans.add(new ArrayList(al));
+            return;
+        }
+        
+        for(int i=ind;i<=n;i++){
+            al.add(i);
+            function(i+1,n,k,al);
+            al.remove(al.size()-1);
+        }
+    }
+}
