2ecf9be824333bec8a2595e1bb5f8a37b355e203
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-28 16:11:55 +0530

    Time: 8 ms (73.84%), Space: 47.6 MB (69.20%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,51 +1,48 @@
 class Solution {
-    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
-        List<List<int[]>> adj = new ArrayList<>();
-        
+    public int[] shortestAlternatingPaths(int n, int[][] r, int[][] b) {
+        List<List<Integer>> redEdges = new ArrayList<>();
+        List<List<Integer>> blueEdges = new ArrayList<>();
+        boolean[][] vis = new boolean[2][n];
         int[] ans = new int[n];
         Arrays.fill(ans,-1);
+        ans[0] = 0;
         
-        for(int i=0;i<n;i++)
-            adj.add(new ArrayList<>());
+        for(int i=0;i<n;i++){
+            redEdges.add(new ArrayList<>());
+            blueEdges.add(new ArrayList<>());
+        }
         
-        for(int i[] : redEdges)
-            adj.get(i[0]).add(new int[]{1,i[1]});
-        
-        for(int i[] : blueEdges)
-            adj.get(i[0]).add(new int[]{2,i[1]});
+        for(int i[] : r)
+            redEdges.get(i[0]).add(i[1]);
+        for(int i[] : b)
+            blueEdges.get(i[0]).add(i[1]);
         
         Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(0,0));
         
-        q.add(new Pair(0,-1,0));
-        
-        Set<String> set = new HashSet<>();
-        
+        int cnt = 0;
         while(!q.isEmpty()){
             int size = q.size();
-            
-            while(size-- > 0){
+            cnt++;
+            for(int i=0;i<size;i++){
                 Pair temp = q.remove();
-                
-                int currval = temp.val;
-                int currlength = temp.length;
-                int currcolor = temp.color;
-                
-                if(ans[currval] == -1) ans[currval] = currlength+1;
-                
-                String st = new StringBuilder()
-                    .append(currval)
-                    .append(",")
-                    .append(currcolor).toString();
-                
-                if(!set.contains(st)){
-                    for(int[] i : adj.get(currval)){
-                        int val = i[1];
-                        int col = i[0];
-                        
-                        if(col != currcolor) q.add(new Pair(val,currlength+1,col));
-                    }
+                if(temp.color != 1){
+                    for(int j : redEdges.get(temp.ind))
+                        if(!vis[0][j]){
+                            vis[0][j] = true;
+                            q.add(new Pair(j,1));
+                            if(ans[j] == -1) ans[j] = cnt;
+                        }
                 }
-                set.add(st);
+                
+                if(temp.color != 2){
+                    for(int j : blueEdges.get(temp.ind))
+                        if(!vis[1][j]){
+                            vis[1][j] = true;
+                            q.add(new Pair(j,2));
+                            if(ans[j] == -1) ans[j] = cnt;
+                        }
+                }
             }
         }
         return ans;
@@ -53,13 +50,10 @@ class Solution {
 }
 
 class Pair{
-    int val;
-    int length;
+    int ind;
     int color;
-    
-    public Pair(int val, int length, int color){
-        this.val = val;
-        this.length = length;
+    public Pair(int ind,int color){
+        this.ind = ind;
         this.color = color;
     }
 }
