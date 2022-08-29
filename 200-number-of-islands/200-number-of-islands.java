be4401a6ecfbd2fb5332b203aeb8b5f31cc8d6b7
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-08-30 01:15:12 +0530

    Time: 9 ms (23.76%), Space: 56.8 MB (70.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,28 +1,41 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int row = grid.length,col = grid[0].length;
-        int cnt = 0;
+        int row = grid.length, col = grid[0].length;
+        //boolean[][] vis = new boolean[row][col];
+        int ans = 0;
+        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
         
-        for(int i=0;i<row;i++){
+        Queue<Pair> q = new LinkedList<>();
+        
+        for(int i=0;i<row;i++)
             for(int j=0;j<col;j++){
                 if(grid[i][j] == '1'){
-                    function(grid,i,j);
-                    cnt++;
+                    q.add(new Pair(i,j));
+                    ans++;
+                    
+                    while(!q.isEmpty()){
+                        Pair temp = q.remove();
+                        for(int k=0;k<4;k++){
+                            int new_row = temp.row+r[k];
+                            int new_col = temp.col+c[k];
+                            
+                            if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || grid[new_row][new_col]=='0') continue;
+                            
+                            grid[new_row][new_col] = '0';
+                            q.add(new Pair(new_row,new_col));
+                        }
+                    }
                 }
             }
-        }
-        return cnt;
+        return ans;
     }
-    public void function(char[][] grid, int sr, int sc){
-        grid[sr][sc] = '0';
-        
-        if(sr-1>=0 && grid[sr-1][sc] == '1')
-            function(grid,sr-1,sc);
-        if(sr+1<grid.length && grid[sr+1][sc] == '1')
-            function(grid,sr+1,sc);
-        if(sc-1>=0 && grid[sr][sc-1] == '1')
-            function(grid,sr,sc-1);
-        if(sc+1<grid[0].length && grid[sr][sc+1]=='1')
-            function(grid,sr,sc+1);
+}
+
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
