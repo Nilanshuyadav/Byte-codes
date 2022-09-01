312c0e956f6cea362a51002cb583d5c6575c25c9
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-02 04:56:18 +0530

    Time: 2 ms (31.38%), Space: 48.1 MB (40.30%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,40 +1,25 @@
 class Solution {
+    int present_value;
+    int[] r = {-1,0,1,0}, c={0,1,0,-1};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int row = image.length, col = image[0].length;
-        int[] r = {-1,0,1,0}, c={0,1,0,-1};
         
         if(image[sr][sc] == color) return image;
         
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
+        present_value = image[sr][sc];
+        dfs(image, sr,sc,color);
         
-        boolean[][] vis = new boolean[row][col];
-        vis[sr][sc] = true;
-        
-        while(!q.isEmpty()){
-            Pair temp = q.remove();
-            int present_value = image[temp.row][temp.col];
-            image[temp.row][temp.col] = color;
-            
-            for(int i=0;i<4;i++){
-                int new_row = temp.row+r[i];
-                int new_col = temp.col+c[i];
-                
-                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || image[new_row][new_col]!=present_value || vis[new_row][new_col]) continue;
-                vis[new_row][new_col] = true;
-                q.add(new Pair(new_row,new_col));
-            }
-        }
         return image;
     }
-}
-
-class Pair{
-    int row;
-    int col;
     
-    public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
+    public void dfs(int[][] image, int row, int col, int color){
+        image[row][col] = color;
+        for(int i=0;i<4;i++){
+            int new_row = row+r[i];
+            int new_col = col+c[i];
+            
+            if(new_row<0 || new_col<0 || new_row>=image.length || new_col>=image[0].length || image[new_row][new_col]!=present_value) continue;
+            dfs(image,new_row,new_col,color);
+        }
     }
 }
