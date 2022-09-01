42dee1c3279954be88c1bd0a2d4ff0182066bfd1
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-02 04:47:47 +0530

    Time: 4 ms (6.67%), Space: 48.3 MB (22.43%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,22 +1,40 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int row = image.length,col = image[0].length;
-        if(image[sr][sc] == color) return image;
-        function(image,sr,sc,color,row,col,image[sr][sc]);
+        int row = image.length, col = image[0].length;
+        int[] r = {-1,0,1,0}, c={0,1,0,-1};
         
+        if(image[sr][sc] == color) return image;
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr,sc));
+        
+        boolean[][] vis = new boolean[row][col];
+        vis[sr][sc] = true;
+        
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
+            int present_value = image[temp.row][temp.col];
+            image[temp.row][temp.col] = color;
+            
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || image[new_row][new_col]!=present_value || vis[new_row][new_col]) continue;
+                vis[new_row][new_col] = true;
+                q.add(new Pair(new_row,new_col));
+            }
+        }
         return image;
     }
+}
+
+class Pair{
+    int row;
+    int col;
     
-    public void function(int[][] image, int sr, int sc, int color, int row, int col,int present_value){
-        image[sr][sc] = color;
-        
-        if(sr+1<row && image[sr+1][sc]==present_value)
-            function(image,sr+1,sc,color,row,col,present_value);
-        if(sr-1>=0 && image[sr-1][sc]==present_value)
-            function(image,sr-1,sc,color,row,col,present_value);
-        if(sc-1>=0 && image[sr][sc-1]==present_value)
-            function(image,sr,sc-1,color,row,col,present_value);
-        if(sc+1<col && image[sr][sc+1]==present_value)
-            function(image,sr,sc+1,color,row,col,present_value);
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
