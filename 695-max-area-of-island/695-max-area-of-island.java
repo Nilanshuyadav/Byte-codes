c35b72fd7344c6ce2ca3cc8ca3496e8c45f250c3
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-02 10:39:25 +0530

    Time: 4 ms (54.73%), Space: 47.1 MB (70.98%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,30 +1,32 @@
 class Solution {
-    int ans;
+    int[] r = {-1,0,1,0},c={0,1,0,-1};
     public int maxAreaOfIsland(int[][] grid) {
         int row = grid.length,col = grid[0].length;
-        int max=0;
-        ans = 0;
-        for(int  i=0;i<row;i++){
-            for(int j =0 ;j<col;j++){
-                if(grid[i][j] == 1){
-                    max = Math.max(max,function(grid,i,j));
-                    ans = 0;
-                } 
+        int max = 0;
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == 1)
+                    max = Math.max(max,dfs(grid,i,j));
             }
         }
+
         return max;
     }
     
-    public int function(int[][] grid,int row,int col){
-        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0) return 0;
-        
-        ans++;
+    public int dfs(int[][] grid, int row, int col){
         grid[row][col] = 0;
-        function(grid,row-1,col);
-        function(grid,row+1,col);
-        function(grid,row,col-1);
-        function(grid,row,col+1);
+        int sum = 0;
         
-        return ans;
+        for(int i=0;i<4;i++){
+            int new_row = row+r[i];
+            int new_col = col+c[i];
+            
+            if(new_row<0 || new_col<0 || new_row>=grid.length || new_col>=grid[0].length || grid[new_row][new_col]!=1) continue;
+            
+            sum += dfs(grid,new_row,new_col);
+        }
+        
+        return sum+1;
     }
 }
