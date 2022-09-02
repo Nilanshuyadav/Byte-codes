0c1f5aa30c5ae30979743227e5a1b66c212baba0
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-02 11:04:23 +0530

    Time: 7 ms (10.87%), Space: 47.1 MB (38.27%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,33 +1,38 @@
 class Solution {
+    int[] r = {-1,0,1,0}, c = {0,1,0,-1};
     public int closedIsland(int[][] grid) {
-        int row = grid.length, col = grid[0].length,cnt = 0;
+        int row = grid.length, col = grid[0].length;
         
-        for(int i=0;i<row;i++){ 
-            if(grid[i][0] == 0) function(grid,i,0); 
-            if(grid[i][col-1] == 0) function(grid,i,col-1);
-        }
         for(int i=0;i<col;i++){
-            if(grid[0][i] == 0) function(grid,0,i);
-            if(grid[row-1][i] == 0) function(grid,row-1,i);
-        }    
+            if(grid[0][i]==0) dfs(grid,0,i);
+            if(grid[row-1][i]==0) dfs(grid,row-1,i);
+        }
         
-        for(int i=1;i<row-1;i++){
-            for(int j=1;j<col-1;j++){
+        for(int i=0;i<row;i++){
+            if(grid[i][0]==0) dfs(grid,i,0);
+            if(grid[i][col-1]==0)dfs(grid,i,col-1);
+        }
+        
+        int cnt = 0;
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
                 if(grid[i][j] == 0){
                     cnt++;
-                    function(grid,i,j);
+                    dfs(grid,i,j);
                 }
-            }
-        }
+        
         return cnt;
     }
     
-    public void function(int[][] grid,int row, int col){
-        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==1) return;
+    public void dfs(int[][] grid, int row, int col){
         grid[row][col] = 1;
-        function(grid,row-1,col);
-        function(grid,row+1,col);
-        function(grid,row,col-1);
-        function(grid,row,col+1);
+        for(int i=0;i<4;i++){
+            int new_row = row+r[i];
+            int new_col = col+c[i];
+            
+            if(new_row<0 || new_col<0 || new_row>=grid.length || new_col>=grid[0].length || grid[new_row][new_col]!=0) continue;
+            
+            dfs(grid,new_row,new_col);
+        }
     }
 }
