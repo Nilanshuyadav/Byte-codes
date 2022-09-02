f2a446b54f4b0e4ebfe0d9fed194dea915567982
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-02 10:51:02 +0530

    Time: 6 ms (23.30%), Space: 49 MB (11.96%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,32 +1,45 @@
 class Solution {
-    int[] r = {-1,0,1,0},c={0,1,0,-1};
     public int maxAreaOfIsland(int[][] grid) {
-        int row = grid.length,col = grid[0].length;
+        int row = grid.length, col = grid[0].length;
         int max = 0;
+        int[] r = {-1,0,1,0}, c={0,1,0,-1};
+        
+        Queue<Pair> q = new LinkedList<>();
         
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j] == 1)
-                    max = Math.max(max,dfs(grid,i,j));
+                if(grid[i][j]==1){
+                    q.add(new Pair(i,j));
+                    grid[i][j] = 0;
+                    int cnt = 0;
+                    while(!q.isEmpty()){
+                        int size = q.size();
+                        cnt += size;
+                        while(size-->0){
+                            Pair temp = q.remove();
+                            for(int k=0;k<4;k++){
+                                int new_row = temp.row+r[k];
+                                int new_col = temp.col+c[k];
+                                
+                                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || grid[new_row][new_col]!=1) continue;
+                                grid[new_row][new_col] = 0;
+                                q.add(new Pair(new_row,new_col));
+                            }
+                        }
+                    }
+                    max = Math.max(max,cnt);
+                }
             }
         }
-
         return max;
     }
-    
-    public int dfs(int[][] grid, int row, int col){
-        grid[row][col] = 0;
-        int sum = 0;
-        
-        for(int i=0;i<4;i++){
-            int new_row = row+r[i];
-            int new_col = col+c[i];
-            
-            if(new_row<0 || new_col<0 || new_row>=grid.length || new_col>=grid[0].length || grid[new_row][new_col]!=1) continue;
-            
-            sum += dfs(grid,new_row,new_col);
-        }
-        
-        return sum+1;
+}
+
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
