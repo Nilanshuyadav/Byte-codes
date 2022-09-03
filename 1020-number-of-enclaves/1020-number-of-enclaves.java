de36a5cbeff4fa0aa4b788a05f693af64fbd42ea
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-03 15:27:39 +0530

    Time: 11 ms (32.30%), Space: 75.7 MB (5.24%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,50 +1,36 @@
 class Solution {
+    int[] r = {-1,0,1,0}, c = {0,1,0,-1};
     public int numEnclaves(int[][] grid) {
-        int row = grid.length,col = grid[0].length;
-        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
-        
-        Queue<Pair> q = new LinkedList<>();
+        int row = grid.length, col = grid[0].length;
         
         for(int i=0;i<row;i++){
-            if(grid[i][0]==1){grid[i][0]=0; q.add(new Pair(i,0));}
-            if(grid[i][col-1]==1){grid[i][col-1]=0; q.add(new Pair(i,col-1));}
-        }  
-        
-        for(int i=0;i<col;i++){
-            if(grid[0][i]==1){grid[0][i]=0; q.add(new Pair(0,i));}
-            if(grid[row-1][i]==1){grid[row-1][i]=0; q.add(new Pair(row-1,i));}
+            if(grid[i][0] == 1){grid[i][0]=0; dfs(grid,i,0);}
+            if(grid[i][col-1]==1){grid[i][col-1]=0; dfs(grid,i,col-1);}
         }
         
-        while(!q.isEmpty()){
-            Pair temp = q.remove();
-            
-            for(int i=0;i<4;i++){
-                int new_row = temp.row+r[i];
-                int new_col = temp.col+c[i];
-                
-                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || grid[new_row][new_col]==0) continue;
-                
-                grid[new_row][new_col] = 0;
-                q.add(new Pair(new_row,new_col));
-            }
+        for(int i=0;i<col;i++){
+            if(grid[0][i]==1){grid[0][i]=0; dfs(grid,0,i);}
+            if(grid[row-1][i]==1){grid[row-1][i]=0; dfs(grid,row-1,i);}
         }
         
         int ans = 0;
         
         for(int i=0;i<row;i++)
             for(int j=0;j<col;j++)
-                if(grid[i][j]==1)
-                    ans++;
+                if(grid[i][j] == 1) ans++;
         
         return ans;
     }
-}
-
-class Pair{
-    int row;
-    int col;
-    public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
+    
+    public void dfs(int[][] grid, int row, int col){
+        for(int i=0;i<4;i++){
+            int new_row = row+r[i];
+            int new_col = col+c[i];
+            
+            if(new_row<0 || new_col<0 || new_row>=grid.length || new_col>=grid[0].length || grid[new_row][new_col]==0) continue;
+            
+            grid[new_row][new_col]=0;
+            dfs(grid,new_row,new_col);
+        }
     }
 }
