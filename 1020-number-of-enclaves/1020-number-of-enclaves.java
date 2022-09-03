d53acdb0fca630be40782cddece7c7e6955bf9f1
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-03 15:12:09 +0530

    Time: 14 ms (20.53%), Space: 74.3 MB (5.24%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,37 +1,50 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
         int row = grid.length,col = grid[0].length;
+        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
+        
+        Queue<Pair> q = new LinkedList<>();
         
         for(int i=0;i<row;i++){
-            if(grid[i][0] == 1) function(grid,i,0);
-            if(grid[i][col-1] == 1) function(grid,i,col-1);
-        }
+            if(grid[i][0]==1){grid[i][0]=0; q.add(new Pair(i,0));}
+            if(grid[i][col-1]==1){grid[i][col-1]=0; q.add(new Pair(i,col-1));}
+        }  
         
         for(int i=0;i<col;i++){
-            if(grid[0][i] == 1) function(grid,0,i);
-            if(grid[row-1][i] == 1) function(grid,row-1,i);
+            if(grid[0][i]==1){grid[0][i]=0; q.add(new Pair(0,i));}
+            if(grid[row-1][i]==1){grid[row-1][i]=0; q.add(new Pair(row-1,i));}
+        }
+        
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
+            
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || grid[new_row][new_col]==0) continue;
+                
+                grid[new_row][new_col] = 0;
+                q.add(new Pair(new_row,new_col));
+            }
         }
         
         int ans = 0;
         
-        for(int i=0;i<row;i++){
-            for(int j =0;j<col;j++){
-                if(grid[i][j] == 1){
-                    ans += function(grid,i,j);
-                }
-            }
-        }
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                if(grid[i][j]==1)
+                    ans++;
+        
         return ans;
     }
-    
-    public int function(int[][] grid,int row,int col){
-        int area = 1;
-        grid[row][col] = 0;
-        if(row-1>=0 && grid[row-1][col] == 1)area += function(grid,row-1,col);
-        if(row+1<grid.length && grid[row+1][col] == 1) area += function(grid,row+1,col);
-        if(col-1>=0 && grid[row][col-1] == 1)area += function(grid,row,col-1);
-        if(col+1<grid[0].length && grid[row][col+1] == 1)area += function(grid,row,col+1);
-        
-        return area;
+}
+
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
