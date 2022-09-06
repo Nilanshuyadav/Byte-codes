61a57978eed7023b600a902cee921a7c1d553480
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-06 17:13:31 +0530

    Time: 29 ms (50.20%), Space: 65.5 MB (27.79%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,43 +1,43 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        int n = grid.length;
-        
+        int row = grid.length, col = grid[0].length;
         int[] r = {-1,0,1,0}, c = {0,1,0,-1};
+        int[][] dis = new int[row][col];
+        for(int i[] : dis)
+            Arrays.fill(i,-1);
         
-        int[][] ans = new int[n][n];
-        for(int i[] : ans)
-            Arrays.fill(i,Integer.MAX_VALUE);
+        Queue<Pair> q = new LinkedList<>();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 1) {ans[i][j] = 0; q.add(new Pair(i,j));}
-            }
-        }
-        
-        if(q.isEmpty() || q.size() == n*n) return -1;
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                if(grid[i][j] == 1){
+                    dis[i][j] = 0;
+                    q.add(new Pair(i,j));
+                }    
+        int size1 = q.size();
+        if(size1==(row*col) || size1==0) return -1;
         
         while(!q.isEmpty()){
-            
-            Pair temp = q.remove();
-
-            for(int i=0;i<4;i++){
-                int new_row = temp.row+r[i];
-                int new_col = temp.col+c[i];
-
-                if(new_row<0 || new_col<0 || new_row>=n || new_col>=n || ans[new_row][new_col] != Integer.MAX_VALUE) continue;
-
-                ans[new_row][new_col] = 1+ans[temp.row][temp.col];
-                q.add(new Pair(new_row,new_col));
+            int size = q.size();
+            while(size-->0){
+                Pair temp = q.remove();
+                for(int i=0;i<4;i++){
+                    int new_row = temp.row+r[i];
+                    int new_col = temp.col+c[i];
+                    
+                    if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || dis[new_row][new_col]!=-1) continue;
+                    
+                    dis[new_row][new_col] = dis[temp.row][temp.col]+1;
+                    q.add(new Pair(new_row,new_col));
+                }
             }
-            
         }
         
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
-                max = Math.max(max,ans[i][j]);
-        }
+        int max = 0;
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                max = Math.max(max,dis[i][j]);
+        
         return max;
     }
 }
