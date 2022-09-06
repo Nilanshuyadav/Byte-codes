5a56bf946065b7469ff45c5207533b042b592116
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-06 17:40:04 +0530

    Time: 14 ms (34.27%), Space: 54.8 MB (52.72%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,50 +1,74 @@
 class Solution {
-    int[] r = {-1,0,1,0},c = {0,1,0,-1};
-    public List<List<Integer>> pacificAtlantic(int[][] height) {
-        int row = height.length,col = height[0].length;      
-        boolean[][] alt = new boolean[row][col];
-        boolean[][] pac = new boolean[row][col];
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int row = heights.length, col = heights[0].length;
+        boolean[][] pac = new boolean[row][col], atl = new boolean[row][col];
+        int[] r = {-1,0,1,0}, c ={0,1,0,-1};
+        Queue<Pair> q = new LinkedList<>();
         
         List<List<Integer>> ans = new ArrayList<>();
         
-        for(int i=0;i<row;i++){
+        for(int i=0;i<col;i++){
+            pac[0][i] = true;
+            q.add(new Pair(0,i));
+        }
+        for(int i=1;i<row;i++){
             pac[i][0] = true;
-            dfs(pac,i,0,height);
-            alt[i][col-1] = true;
-            dfs(alt,i,col-1,height);
+            q.add(new Pair(i,0));
+        }
+        
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || heights[new_row][new_col]<heights[temp.row][temp.col] || pac[new_row][new_col]) continue;
+                
+                pac[new_row][new_col] = true;
+                q.add(new Pair(new_row,new_col));
+            }
         }
         
         for(int i=0;i<col;i++){
-            pac[0][i] = true;
-            dfs(pac,0,i,height);
-            alt[row-1][i] = true;
-            dfs(alt,row-1,i,height);
+            atl[row-1][i] = true;
+            q.add(new Pair(row-1,i));
+        }
+        for(int i=0;i<row;i++){
+            atl[i][col-1] = true;
+            q.add(new Pair(i,col-1));
         }
         
-        for(int i=0;i<row;i++){
+        while(!q.isEmpty()){
+            Pair temp = q.remove();
+            for(int i=0;i<4;i++){
+                int new_row = temp.row+r[i];
+                int new_col = temp.col+c[i];
+                
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || heights[new_row][new_col]<heights[temp.row][temp.col] || atl[new_row][new_col]) continue;
+                
+                atl[new_row][new_col] = true;
+                q.add(new Pair(new_row, new_col));
+            }
+        }
+        
+        for(int i=0;i<row;i++)
             for(int j=0;j<col;j++){
-                if(pac[i][j] && alt[i][j]){
+                if(pac[i][j] && atl[i][j]){
                     List<Integer> al = new ArrayList<>();
                     al.add(i);
                     al.add(j);
                     ans.add(al);
                 }
             }
-        }
-        
         return ans;
-    }
-    
-    public void dfs(boolean[][] arr, int row, int col, int[][] height){
-        
-        for(int i=0;i<4;i++){
-            int new_row = row+r[i];
-            int new_col = col+c[i];
-            
-            if(new_row<0 || new_col<0 || new_row>=height.length || new_col>=height[0].length || arr[new_row][new_col] || height[row][col]>height[new_row][new_col ]) continue;
-            arr[new_row][new_col] = true;
-            dfs(arr,new_row,new_col,height);
-        }
     }
 }
 
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
