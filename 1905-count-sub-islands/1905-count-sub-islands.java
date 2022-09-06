1c01362138b75ade4dde44254f138dd56ea735d1
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-06 16:45:53 +0530

    Time: 33 ms (57.81%), Space: 125.4 MB (69.00%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,28 +1,35 @@
 class Solution {
+    int[] r = {-1,0,1,0} , c = {0,1,0,-1};
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int cnt = 0,row = grid2.length,col = grid2[0].length;
+        int row = grid2.length, col = grid2[0].length,cnt = 0;
         
         for(int i=0;i<row;i++){
-            for(int j =0;j<col;j++){
-                if(grid2[i][j] == 1 && function(grid1,grid2,i,j)){
-                    cnt++;
+            for(int j=0;j<col;j++){
+                if(grid2[i][j] == 1){
+                    if(dfs(grid1,grid2,i,j))
+                        cnt++;
                 }
             }
         }
+        
         return cnt;
     }
     
-    public boolean function(int[][] grid1, int[][] grid2, int row, int col){
-        boolean temp = true;
-        if(grid2[row][col] != grid1[row][col])temp = false;
+    public boolean dfs(int[][] grid1, int[][] grid2, int row, int col){
+        boolean bool = true;
+        if(grid1[row][col] != grid2[row][col]) bool = false;
         
         grid2[row][col] = 0;
         
-        if(row-1>=0 && grid2[row-1][col]==1) temp = function(grid1,grid2,row-1,col) && temp;
-        if(row+1<grid1.length && grid2[row+1][col]==1) temp = function(grid1,grid2,row+1,col) && temp;
-        if(col-1>=0 && grid2[row][col-1]==1) temp = function(grid1,grid2,row,col-1) && temp;
-        if(col+1<grid1[0].length && grid2[row][col+1]==1) temp = function(grid1,grid2,row,col+1) && temp;
+        for(int i=0;i<4;i++){
+            int new_row = row+r[i];
+            int new_col = col+c[i];
+            
+            if(new_row<0 || new_col<0 || new_row>=grid2.length || new_col>=grid2[0].length || grid2[new_row][new_col]==0) continue;
+            
+            bool = dfs(grid1,grid2,new_row,new_col) && bool;
+        }
         
-        return temp;
+        return bool;
     }
 }
