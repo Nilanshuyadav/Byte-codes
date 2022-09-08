49f6823f6d6ec01ed731620f83e03ae1289e290d
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-08 15:13:01 +0530

    Time: 22 ms (24.50%), Space: 54.7 MB (35.95%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,56 +1,60 @@
 class Solution {
+    Queue<Pair> q;
     int[] r = {-1,0,1,0}, c = {0,1,0,-1};
     public int shortestBridge(int[][] grid) {
-       int n = grid.length;
+        int row = grid.length, col = grid[0].length;
+        q = new LinkedList<>();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 1){
-                    function(grid,i,j);
-                    i = n;
-                    j = n;
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                if(grid[i][j]==1){
+                    grid[i][j]=2;
+                    q.add(new Pair(i,j));
+                    dfs(grid,i,j);
+                    j=col;
+                    i=row;
                 }
-            }
-        }
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 1){grid[i][j] = -1; q.add(new int[]{i,j});}
-            }
-        
+    
         int cnt = -1;
+        
         while(!q.isEmpty()){
             int size = q.size();
             cnt++;
-            for(int i=0;i<size;i++){
-                int[] temp = q.remove();
-                for(int j=0;j<4;j++){
-                    int nr = temp[0]+r[j];
-                    int nc = temp[1]+c[j];
+            while(size-->0){
+                Pair temp = q.remove();
+                for(int i=0;i<4;i++){
+                    int new_row = temp.row+r[i];
+                    int new_col = temp.col+c[i];
                     
-                    if(nr<0 || nc<0 || nr>=n || nc>=n) continue;
-                    if(grid[nr][nc] == 2) return cnt;
-                    if(grid[nr][nc] == 0){
-                        grid[nr][nc] = -1;
-                        q.add(new int[]{nr,nc});
-                    }
+                    if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || grid[new_row][new_col]==2) continue;
+                    
+                    if(grid[new_row][new_col]==1) return cnt;
+                    grid[new_row][new_col] = 2;
+                    q.add(new Pair(new_row, new_col));
                 }
             }
         }
-        return -1;
+        return 0;
     }
     
-    public void function(int[][] grid, int row, int col){
-        grid[row][col] = 2;
-        int n = grid.length;
+    public void dfs(int[][] grid, int row, int col){
         for(int i=0;i<4;i++){
-            int nr = row+r[i];
-            int nc = col+c[i];
+            int new_row = row+r[i];
+            int new_col = col+c[i];
             
-            if(nr<0 || nc<0 || nr>=n || nc>=n) continue;
-            if(grid[nr][nc] == 1) function(grid,nr,nc);
+            if(new_row<0 || new_col<0 || new_row>=grid.length || new_col>=grid[0].length || grid[new_row][new_col]!=1)continue;
+            grid[new_row][new_col] = 2;
+            q.add(new Pair(new_row,new_col));
+            dfs(grid,new_row,new_col);
         }
+    }
+}
+
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
