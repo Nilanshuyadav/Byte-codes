b231c8e69958f4695bad75f5258f2329aec1a3e5
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-08 14:53:08 +0530

    Time: 36 ms (17.25%), Space: 73.2 MB (37.10%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int row = mat.length, col = mat[0].length;
-        int ans[][] = new int[row][col];
-        
-        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
-        
-        for(int[] i : ans)
-            Arrays.fill(i,Integer.MAX_VALUE);
+        int[] r = {-1,0,1,0}, c={0,1,0,-1};
+        int[][] dis = new int[row][col];
+        for(int[] i : dis) Arrays.fill(i,Integer.MAX_VALUE);
         
         Queue<Pair> q = new LinkedList<>();
-        
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
                 if(mat[i][j] == 0){
-                    ans[i][j] = 0;
+                    dis[i][j] = 0;
                     q.add(new Pair(i,j));
-                }    
-            }
-        }
+                }
         
         while(!q.isEmpty()){
             Pair temp = q.remove();
@@ -26,13 +20,16 @@ class Solution {
                 int new_row = temp.row+r[i];
                 int new_col = temp.col+c[i];
                 
-                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || ans[new_row][new_col] != Integer.MAX_VALUE) continue;
+                if(new_row<0 || new_col<0 || new_row>=row || new_col>=col || mat[new_row][new_col]==0) continue;
                 
-                ans[new_row][new_col] = 1+ans[temp.row][temp.col];
-                q.add(new Pair(new_row,new_col));
+                if(dis[new_row][new_col]>dis[temp.row][temp.col]+1){
+                    dis[new_row][new_col] = dis[temp.row][temp.col]+1;
+                    q.add(new Pair(new_row,new_col));
+                }
             }
         }
-        return ans;
+        
+        return dis;
     }
 }
 
