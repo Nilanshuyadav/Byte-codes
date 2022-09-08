8cb6cb5b26a95ff881a1b132fe9bb500361361e2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-08 15:34:31 +0530

    Time: 7 ms (93.90%), Space: 73.2 MB (11.04%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,26 +1,30 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
-        int row = maze.length, col = maze[0].length, level = -1;
-        int[] r ={1,0,-1,0}, c = {0,1,0,-1};
+        int[] r = {-1,0,1,0}, c = {0,1,0,-1};
+        int row = maze.length, col = maze[0].length;
         Queue<int[]> q = new LinkedList<>();
         q.add(entrance);
         maze[entrance[0]][entrance[1]] = '+';
         
+        int cnt = -1;
         while(!q.isEmpty()){
             int size = q.size();
-            level++;
-            for(int j=0;j<size;j++){
+            cnt++;
+            while(size-->0){
                 int[] temp = q.remove();
                 
                 for(int i=0;i<4;i++){
                     int new_row = temp[0]+r[i];
                     int new_col = temp[1]+c[i];
                     
-                    if((new_row<0 || new_col<0 || new_row>=row || new_col>=col) && level!=0) return level;
-                    if((new_row>=0 && new_col>=0 && new_row<row && new_col<col) && maze[new_row][new_col] == '.'){
-                        q.add(new int[]{new_row,new_col});
-                        maze[new_row][new_col] = '+';
+                    if(new_row<0 || new_col<0 || new_row>=row || new_col>=col){
+                        if(cnt>0) return cnt;
+                        else continue;
                     }
+                    
+                    if(maze[new_row][new_col]!='.') continue;
+                    maze[new_row][new_col]='+';
+                    q.add(new int[]{new_row,new_col});
                 }
             }
         }
