0e93469ff46b0f99459803ed28a07273c19efdaf
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-15 23:32:30 +0530

    Time: 1394 ms (10.41%), Space: 145.1 MB (5.09%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int ladderLength(String start, String end, List<String> wordList) {
+    public int ladderLength(String begin, String end, List<String> wordList) {
         Set<String> set = new HashSet<>(wordList);
         
-        Queue<String> q = new LinkedList<>();
-        q.add(start);
+        Queue<StringBuilder> q = new LinkedList<>();
+        q.add(new StringBuilder(begin));
         
         int cnt = 0;
         
         while(!q.isEmpty()){
             int size = q.size();
             cnt++;
-            for(int i=0;i<size;i++){
+            while(size-->0){
                 StringBuilder st = new StringBuilder(q.remove());
                 if(end.equals(st.toString())) return cnt;
-                for(int j=0;j<st.length();j++)
-                    for(int k = 0;k<26;k++){
-                        StringBuilder sb = new StringBuilder(st.substring(0,j)+(char)(k+'a')+st.substring(j+1));
-                        if(set.contains(sb.toString())){
-                            set.remove(sb.toString());
-                            q.add(sb.toString());
-                        }
-                    }
+                
+                for(int i=0;i<st.length();i++){
+                    for(int j=0;j<26;j++){
+                        StringBuilder s = new StringBuilder(st.substring(0,i) + (char)(j+'a') + st.substring(i+1));
+                        if(set.contains(s.toString())){set.remove(s.toString()); q.add(s); }
+                    }    
+                }
             }
         }
         
