890455d961700d60a006476e1bac1bec3fec2e00
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-16 19:27:30 +0530

    Time: 1050 ms (15.05%), Space: 144.8 MB (5.03%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,41 +1,32 @@
 class Solution {
     public int ladderLength(String begin, String end, List<String> wordList) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(begin,1));
+        Queue<String> q = new LinkedList<>();
+        q.add(begin);
         
         Set<String> set = new HashSet<>(wordList);
         
+        int cnt = 0;
         while(!q.isEmpty()){
             int size = q.size();
+            cnt++;
             
             while(size-->0){
-                Pair temp = q.remove();
+                StringBuilder sb = new StringBuilder(q.remove());
                 
-                StringBuilder sb = new StringBuilder(temp.st);
-                
-                for(int i=0;i<sb.length();i++){
-                    for(int j=0;j<26;j++){
-                        StringBuilder s = new StringBuilder(sb.substring(0,i) + (char)(j+'a') + sb.substring(i+1));
-                        if(set.contains(s.toString())){
-                            set.remove(s.toString());
-                            q.add(new Pair(s.toString(),temp.level+1));
+                for(int curr_ind = 0;curr_ind<sb.length();curr_ind++)
+                    for(int char_ind=0; char_ind<26; char_ind++){
+                        StringBuilder modified_string = new StringBuilder(sb.substring(0,curr_ind) + (char)(char_ind+'a') + sb.substring(curr_ind+1));
+                        
+                        if(set.contains(modified_string.toString())){
+                            set.remove(modified_string.toString());
+                            q.add(modified_string.toString());
                             
-                            if(end.equals(s.toString())) return temp.level+1;
+                            if(end.equals(modified_string.toString())) return ++cnt;
                         }
                     }
-                }
             }
         }
         
         return 0;
-    }
-}
-
-class Pair{
-    String st;
-    int level;
-    public Pair(String st,int level){
-        this.st = st;
-        this.level = level;
     }
 }
