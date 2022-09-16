b692c737b3a458e7d19049b2058dae6d71f2d2f3
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-16 19:16:44 +0530

    Time: 1121 ms (14.03%), Space: 145 MB (5.03%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,28 +1,41 @@
 class Solution {
     public int ladderLength(String begin, String end, List<String> wordList) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(begin,1));
+        
         Set<String> set = new HashSet<>(wordList);
-        
-        Queue<StringBuilder> q = new LinkedList<>();
-        q.add(new StringBuilder(begin));
-        
-        int cnt = 0;
         
         while(!q.isEmpty()){
             int size = q.size();
-            cnt++;
+            
             while(size-->0){
-                StringBuilder st = new StringBuilder(q.remove());
-                if(end.equals(st.toString())) return cnt;
+                Pair temp = q.remove();
                 
-                for(int i=0;i<st.length();i++){
+                StringBuilder sb = new StringBuilder(temp.st);
+                
+                for(int i=0;i<sb.length();i++){
                     for(int j=0;j<26;j++){
-                        StringBuilder s = new StringBuilder(st.substring(0,i) + (char)(j+'a') + st.substring(i+1));
-                        if(set.contains(s.toString())){set.remove(s.toString()); q.add(s); }
-                    }    
+                        StringBuilder s = new StringBuilder(sb.substring(0,i) + (char)(j+'a') + sb.substring(i+1));
+                        if(set.contains(s.toString())){
+                            set.remove(s.toString());
+                            q.add(new Pair(s.toString(),temp.level+1));
+                            
+                            if(end.equals(s.toString())) return temp.level+1;
+                        }
+                    }
                 }
             }
         }
         
         return 0;
+    }
+}
+
+class Pair{
+    String st;
+    int level;
+    public Pair(String st,int level){
+        this.st = st;
+        this.level = level;
     }
 }
