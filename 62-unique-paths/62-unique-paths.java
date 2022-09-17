3f74c136d8ab5e3406c2a9da7be7e334c1a66adc
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-18 00:53:48 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (98.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i[] : dp) Arrays.fill(i,-1);
         
-        return dfs(m-1,n-1,dp);
-    }
-    
-    public int dfs(int row, int col, int[][] dp){
-        if(row<0 || col<0) return 0;
-        if(row==0 && col==0) return 1;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(row==0 && col==0){dp[0][0] = 1; continue;}
+                if(col!=0) dp[row][col] = dp[row][col-1];
+                if(row!=0) dp[row][col] += dp[row-1][col];
+            }
+        }
         
-        if(dp[row][col] != -1) return dp[row][col];
-        
-        return dp[row][col] = dfs(row-1,col,dp)+dfs(row,col-1,dp);
+        return dp[m-1][n-1];
     }
 }
