fe8338462fda548412d69019934f110290d217ee
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-20 09:38:17 +0530

    Time: 85 ms (49.15%), Space: 78.2 MB (47.17%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -2,15 +2,14 @@ class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length;
         
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         int max = 0;
-        for(int ind_nums1 = 0; ind_nums1<n;ind_nums1++){
-            for(int ind_nums2=0;ind_nums2<m;ind_nums2++){
-                if(nums1[ind_nums1] != nums2[ind_nums2]) continue;
+        for(int ind_nums1 = 1; ind_nums1<=n;ind_nums1++){
+            for(int ind_nums2=1;ind_nums2<=m;ind_nums2++){
+                if(nums1[ind_nums1-1] != nums2[ind_nums2-1]) continue;
                 
-                dp[ind_nums1][ind_nums2] = 1;
+                dp[ind_nums1][ind_nums2] = 1 + dp[ind_nums1-1][ind_nums2-1];
                 
-                if(ind_nums1!=0 && ind_nums2!=0) dp[ind_nums1][ind_nums2] += dp[ind_nums1-1][ind_nums2-1];
                 max = Math.max(max,dp[ind_nums1][ind_nums2]);
             }
         }
