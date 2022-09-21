ebed0184bce2090dc6b6a9e705d454a72c45d3f5
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-21 23:35:27 +0530

    Time: 10 ms (23.47%), Space: 68.5 MB (78.46%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -3,26 +3,25 @@ class Solution {
         int[] ans = new int[queries.length];
         int curr=0, sum=0;
         
-        Set<Integer> set = new HashSet<>();
-        
         for(int ind=0; ind<nums.length; ind++){
             if(nums[ind]%2==0){
                 sum += nums[ind];
-                set.add(ind);
             }
         }
         
         for(int[] ind : queries){
-            if(set.contains(ind[1])){
-                sum -= nums[ind[1]];
-                set.remove(ind[1]);
-            }
-            
-            nums[ind[1]] = nums[ind[1]]+ind[0];
-            
-            if(nums[ind[1]]%2==0){
+            if((nums[ind[1]]%2==0 && ind[0]%2==0) || (nums[ind[1]]%2!=0 && ind[0]%2!=0)){
+                if(nums[ind[1]]%2==0) sum -= nums[ind[1]];
+                nums[ind[1]] = nums[ind[1]]+ind[0];
+                
                 sum += nums[ind[1]];
-                set.add(ind[1]);
+            }
+            else if(nums[ind[1]]%2==0 && ind[0]%2!=0){
+                sum -= nums[ind[1]];
+                nums[ind[1]] = nums[ind[1]]+ind[0];
+            }
+            else{
+                nums[ind[1]] = nums[ind[1]]+ind[0];
             }
             
             ans[curr++] = sum;
