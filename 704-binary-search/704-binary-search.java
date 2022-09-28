d565e88b588db7ad5a053d8896b8117225bfec32
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-28 18:56:37 +0530

    Time: 0 ms (100.00%), Space: 54.2 MB (52.61%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,15 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return dfs(nums,target,0,nums.length-1);
+        return dfs(nums,0,nums.length-1,target);
     }
     
-    public int dfs(int[] nums, int target, int low, int high){
+    public int dfs(int[] nums,int low, int high, int target){
         if(low>high) return -1;
         
-        int mid = (low+high)/2;
+        int mid = low+(high-low)/2;
         
-        if(nums[mid] == target) return mid;
-        else if(nums[mid]<target) return dfs(nums,target,mid+1,high);
-        else return dfs(nums,target,low,mid-1);
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]<target) return dfs(nums,mid+1, high, target);
+        else return dfs(nums,low, mid-1,target);
     }
 }
