654ec18852e3c36d9d411786d2b4ee2d22b45282
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-09-29 19:29:05 +0530

    Time: 10 ms (56.83%), Space: 62.4 MB (61.00%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -3,12 +3,14 @@ class Solution {
         int min_ind=0, min=Integer.MAX_VALUE, n = arr.length;
         
         for(int ind=0; ind<n; ind++){
-            int temp = Math.abs(arr[ind]-x);
+            int temp = arr[ind]-x;
             
-            if(temp<min){
-                min = temp;
+            if(Math.abs(temp)<min){
+                min = Math.abs(temp);
                 min_ind = ind;
             }
+            
+            arr[ind] = temp;
         }    
         
         List<Integer> ans = new ArrayList<>();
@@ -16,14 +18,14 @@ class Solution {
         int low = min_ind-1, high = min_ind+1;
         
         while(low>=0 && high<n && --k>0)
-            if(Math.abs(arr[high]-x)<Math.abs(arr[low]-x)) high++;
+            if(Math.abs(arr[high])<Math.abs(arr[low])) high++;
             else low--;
         
         while(low>=0 && --k>0) low--;
         while(high<n && --k>0) high++;
         
         for(int ind=low+1; ind<high; ind++)
-            ans.add(arr[ind]);
+            ans.add(arr[ind]+x);
         
         return ans;
     }
