ae0d2f0ab562fdaf85d66719415b6c34c12dae14
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-10-31 02:58:56 +0530

    Added solution - LeetHub

---
+++ Detect Cycle using DSU - GFG/detect-cycle-using-dsu.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            int ans = obj.detectCycle(V, adj);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to detect cycle using DSU in an undirected graph.
+    public int detectCycle(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        boolean[] vis = new boolean[V];
+        int[]  parent = new int[V];
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        
+        for(int ind=0; ind<V; ind++){
+            if(!vis[ind]){
+                q.add(ind);
+                vis[ind] = true;
+                parent[ind] = -1;
+                
+                while(!q.isEmpty()){
+                    int temp = q.remove();
+                    
+                    for(int inx : adj.get(temp)){
+                        if(inx == parent[temp]) continue;
+                        if(vis[inx]) return 1;
+                        
+                        vis[inx] = true;
+                        q.add(inx);
+                    }
+                }    
+            }
+        }
+        
+        return 0;
+    }
+}
