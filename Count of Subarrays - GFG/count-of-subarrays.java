a954e897c28bd423eea3ec5b32557bf7257cafbd
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-11-17 13:46:25 +0530

    Added solution - LeetHub

---
+++ Count of Subarrays - GFG/count-of-subarrays.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+            str = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            long ans = new Solution().countSubarray(arr, n, k);
+
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    long countSubarray(int arr[], int n, int k) {
+
+        long ans=0;
+        int last=-1;
+        
+        for(int ind=0; ind<n; ind++){
+            if(arr[ind] > k){
+                ans += ((last+1)*(ind-last-1));
+                ans += ind+1;
+                last = ind;
+            }
+        }
+        
+        ans += ((last+1)*(n-last-1));
+        
+        return ans;
+    }
+}
