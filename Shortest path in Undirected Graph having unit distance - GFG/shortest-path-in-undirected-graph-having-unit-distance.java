45148a44d67b4fe87a49ef790fcc4f7ab932cbce
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-11-19 15:38:33 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        // Code here
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for(int ind=0; ind<n; ind++)
+            adj.add(new ArrayList<>());
+        
+        for(int ind[] : edges){
+            adj.get(ind[0]).add(ind[1]);
+            adj.get(ind[1]).add(ind[0]);
+        }
+        
+        int ans[] = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        
+        ans[src] = 0;
+        Queue<int[]> q = new LinkedList<>();
+        
+        q.add(new int[]{src,0});
+        
+        while(!q.isEmpty()){
+            int[] temp = q.remove();
+            
+            int ind = temp[0];
+            int wt = temp[1];
+            
+            for(int inx : adj.get(ind)){
+                if(wt+1 <  ans[inx]){
+                    ans[inx] = wt+1;
+                    q.add(new int[]{inx, wt+1});
+                }
+            }
+        }
+        
+        for(int ind=0; ind<n; ind++){
+            if(ans[ind] == Integer.MAX_VALUE)
+                ans[ind] = -1;
+        }
+        
+        return ans;
+    }
+}
