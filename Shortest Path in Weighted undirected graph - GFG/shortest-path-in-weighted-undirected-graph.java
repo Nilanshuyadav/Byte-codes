834bbab909222a2883915c2b7d0e2850d7d35c92
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-11-20 19:02:15 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
@@ -33,11 +33,16 @@ import java.io.*;
 
 class Solution {
     public static List<Integer> shortestPath(int n, int m, int edges[][]) {
+        // code here
+        int[] parent = new int[n+1], arr = new int[n+1];
         PriorityQueue<Pair> pq = new PriorityQueue<>(new my_comparator());
         
         List<List<int[]>> adj = new ArrayList<>();
+        
         for(int ind=0; ind<=n; ind++){
             adj.add(new ArrayList<>());
+            parent[ind] = ind;
+            arr[ind] = Integer.MAX_VALUE;
         }
         
         for(int ind[] : edges){
@@ -45,44 +50,42 @@ class Solution {
             adj.get(ind[1]).add(new int[]{ind[0], ind[2]});
         }
         
-        List<Integer> al = new ArrayList<>();
-        al.add(1);
         
-        pq.add(new Pair(al, 1, 0));
-        List<Integer> ans = new ArrayList<>();
-        boolean found = false;
-        
-        int[] arr = new int[n+1];
-        Arrays.fill(arr,Integer.MAX_VALUE);
+        parent[1] = 1;
         arr[1] = 0;
+        
+        pq.add(new Pair(1,0));
         
         while(!pq.isEmpty()){
             Pair temp = pq.remove();
             
-            if(temp.node == n){
-                found = true;
-                ans = temp.al;
-                break;
-            }
-            
             int node = temp.node;
-            List<Integer> lst = temp.al;
+            int wt = temp.wt;
             
             for(int[] inx : adj.get(node)){
-                if(temp.wt+inx[1] < arr[inx[0]]){
-                    lst.add(inx[0]);
-                    pq.add(new Pair(new ArrayList<>(lst), inx[0], temp.wt+inx[1]));
-                    lst.remove(lst.size()-1); 
-                    arr[inx[0]] = temp.wt+inx[1];
+                if(wt+inx[1] < arr[inx[0]]){
+                    parent[inx[0]] = node;
+                    arr[inx[0]] = wt+inx[1];
+                    
+                    pq.add(new Pair(inx[0], arr[inx[0]]));
                 }
             }
         }
         
-        if(found)
-            return ans;
+        if(parent[n] == n){
+            return new ArrayList<>(Arrays.asList(-1));
+        }
         
-        ans.add(-1);    
-        return ans;    
+        List<Integer> ans = new ArrayList<>();
+        ans.add(n);
+        int temp = n;
+        
+        while(temp != 1){
+            ans.add(0,parent[temp]);
+            temp = parent[temp];
+        }
+        
+        return ans;
     }
 }
 
@@ -93,11 +96,9 @@ class my_comparator implements Comparator<Pair>{
 }
 
 class Pair{
-    List<Integer> al;
     int node;
     int wt;
-    public Pair(List<Integer> al, int node, int wt){
-        this.al = al;
+    public Pair(int node, int wt){
         this.node = node;
         this.wt = wt;
     }
