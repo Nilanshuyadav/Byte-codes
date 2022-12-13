165b4c09e2013ac381740d57963396af41df2775
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-12-13 06:32:57 +0530

    Time: 4 ms (77.75%), Space: 56 MB (83.14%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -3,7 +3,45 @@ class Solution {
         int n = stones.length;
         boolean[] vis = new boolean[n];
         
-        return fordward(0, stones, n, vis);
+        int ind=0, temp = Integer.MIN_VALUE;
+        
+        while(ind<n){
+            if(ind+2<n && !vis[ind+2]){
+                vis[ind+2] = true;
+                temp = Math.max(temp, stones[ind+2] - stones[ind]);
+                ind += 2;
+            }
+
+            else if(ind+1<n && !vis[ind+1]){
+                vis[ind+1] = true;
+                temp = Math.max(temp, stones[ind+1]-stones[ind]);
+                ind += 1;
+            }
+            
+            if(ind == n-1)
+                break;
+        }
+        
+        ind = n-1;
+        
+        while(0<=ind){
+            if(0<=ind-2 && !vis[ind-2]){
+                vis[ind-2] = true;
+                temp = Math.max(temp, stones[ind]-stones[ind-2]);
+                ind -= 2;
+            }
+
+            else if(0<=ind-1 && !vis[ind-1]){
+                vis[ind-1] = true;
+                temp = Math.max(temp, stones[ind]-stones[ind-1]);
+                ind -= 1;
+            }
+            
+            if(ind == 0)
+                break;
+        }
+        
+        return temp;
     }
     
     public int fordward(int ind, int[] stones, int n, boolean[] vis){
