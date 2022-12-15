b15f544bf87e43efc5822ef5938e0f3371a4dda9
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-12-15 15:49:38 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (92.34%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,27 +1,5 @@
 class Solution {
-    boolean alice_wins;
     public boolean stoneGame(int[] piles) {
-        alice_wins = false;
-        int n = piles.length;
-        
-        Boolean[][][] dp = new Boolean[2][n][n];
-        
-        return dfs(1, 0, n-1, piles, 0, 0, dp);
-    }
-    
-    public boolean dfs(int turn, int s, int e, int[] piles, int a_sum, int b_sum, Boolean[][][] dp){
-        if(e<s){
-            return a_sum>b_sum;
-        }
-        
-        if(dp[turn][s][e] != null)
-            return dp[turn][s][e];
-        
-        if(turn==1){
-            return dp[turn][s][e] = dfs(0, s+1, e, piles, a_sum+piles[s], b_sum, dp) || dfs(0, s, e-1, piles, a_sum+piles[e], b_sum, dp);
-        }
-        else{
-            return dp[turn][s][e] = dfs(1, s+1, e, piles, a_sum, b_sum+piles[s], dp) || dfs(1, s, e-1, piles, a_sum, b_sum+piles[e], dp);
-        }
+        return true;
     }
 }
