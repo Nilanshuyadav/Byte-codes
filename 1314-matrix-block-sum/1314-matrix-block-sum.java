480fbda946436ca68f581b3a63179c7958bcb6ea
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-12-21 22:29:20 +0530

    Time: 11 ms (62.09%), Space: 49.6 MB (7.30%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int row = mat.length, col = mat[0].length;
+        int row = mat.length, col=mat[0].length;
         
         int[][] dp = new int[row][col];
         int sum = 0;
@@ -12,6 +12,10 @@ class Solution {
                 sum += mat[r][c];
                 
                 dp[r][c] = sum;
+                
+                if(r>0){
+                    dp[r][c] += dp[r-1][c];
+                }
             }
         }
         
@@ -19,16 +23,24 @@ class Solution {
         
         for(int r=0; r<row; r++){
             for(int c=0; c<col; c++){
-                int row_from = Math.max(0,r-k), row_to = Math.min(row-1,r+k);
-                int col_from = Math.max(0,c-k), col_to = Math.min(col-1,c+k);
+                int row_from = Math.max(0, r-k), row_to = Math.min(row-1, r+k);
+                int col_from = Math.max(0, c-k), col_to = Math.min(col-1, c+k);
                 
-                sum = 0;
+                int total = dp[row_to][col_to];
                 
-                for(int r_ind=row_from; r_ind<=row_to; r_ind++){
-                    sum += dp[r_ind][col_to]-dp[r_ind][col_from]+mat[r_ind][col_from];
+                if(row_from>0){
+                    total -= dp[row_from-1][col_to];
                 }
                 
-                ans[r][c] = sum;
+                if(col_from>0){
+                    total -= dp[row_to][col_from-1];
+                }
+                
+                if(row_from>0 && col_from>0){
+                    total += dp[row_from-1][col_from-1];
+                }
+                
+                ans[r][c] = total;
             }
         }
         
