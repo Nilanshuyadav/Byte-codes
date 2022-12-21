e13c210f9c3467c01ca004840bd41a9626677726
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2022-12-21 22:07:48 +0530

    Time: 18 ms (54.26%), Space: 49.5 MB (16.00%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -2,18 +2,30 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int row = mat.length, col = mat[0].length;
         
+        int[][] dp = new int[row][col];
+        int sum = 0;
+        
+        for(int r=0; r<row; r++){
+            sum = 0;
+            
+            for(int c=0; c<col; c++){
+                sum += mat[r][c];
+                
+                dp[r][c] = sum;
+            }
+        }
+        
         int[][] ans = new int[row][col];
         
         for(int r=0; r<row; r++){
             for(int c=0; c<col; c++){
-                int row_from = Math.max(0, r-k), row_to = Math.min(row-1, r+k);
-                int col_from = Math.max(0, c-k), col_to = Math.min(col-1, c+k);
+                int row_from = Math.max(0,r-k), row_to = Math.min(row-1,r+k);
+                int col_from = Math.max(0,c-k), col_to = Math.min(col-1,c+k);
                 
-                int sum=0;
+                sum = 0;
                 
-                for(int r_from = row_from; r_from<=row_to; r_from++){
-                    for(int c_from = col_from; c_from<=col_to; c_from++)
-                        sum += mat[r_from][c_from];
+                for(int r_ind=row_from; r_ind<=row_to; r_ind++){
+                    sum += dp[r_ind][col_to]-dp[r_ind][col_from]+mat[r_ind][col_from];
                 }
                 
                 ans[r][c] = sum;
