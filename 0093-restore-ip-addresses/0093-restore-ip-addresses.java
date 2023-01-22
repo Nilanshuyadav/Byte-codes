7c3c1204a245aa4b785cbb82c5245810ee828a1f
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-01-22 20:03:28 +0530

    Time: 5 ms (62.11%), Space: 44 MB (8.77%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,36 +1,41 @@
-class Solution{       
-    private List<String> ans;
-    private int l;
+class Solution {
     public List<String> restoreIpAddresses(String s) {
-       ans = new ArrayList<>();
-       l = s.length();
-       f(s, 0, "", 0);
-       return ans;
-   }
-   
-   private boolean dfs(String ip){
-       if(ip.length() > 3 || ip.length() == 0) return false;
-       if(ip.length() > 1 && ip.charAt(0) == '0') return false;
-       if(ip.length() > 0 && Integer.parseInt(ip) > 255) return false;
-       return true; 
-   }
-
-   private void f(String s, int index, String ip, int dot){
-       if(dot == 3){
-           if(dfs(s.substring(index))) {
-               ip += s.substring(index);
-               ans.add(ip);
-           }
-           return;
-       }
-       
-       for(int i = index; i < l; i++){
-           if(dfs(s.substring(index, i +1))){
-               int k = s.substring(index, i+1).length();
-               ip += s.substring(index, i+1) + ".";
-               f(s, i+1, ip, dot+1);
-               ip = ip.substring(0, ip.length() - k -1);
-           }
-       }
-   }
+        List<String> ans = new ArrayList<>();
+        
+        solve(0, s.length(), 4, s, "", ans);
+        
+        return ans;
+    }
+    
+    public void solve(int ind, int n, int k, String s, String temp, List<String> ans){
+        if(k==0){
+            if(ind==n)
+                ans.add(temp.substring(0, temp.length()-1));
+            return;
+        }
+        
+        StringBuilder sb = new StringBuilder(temp);
+        
+        for(int i=ind; i<n; i++){
+            sb.append(s.charAt(i));
+            
+            if(isValid(s, ind, i)){
+                sb.append('.');
+                solve(i+1, n, k-1, s, sb.toString(), ans);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+    
+    public boolean isValid(String s, int start, int end){
+        if(start!=end && s.charAt(start)=='0')
+            return false;
+        
+        long number = Long.valueOf(s.substring(start, end+1));
+        
+        if(number<0 || number>255)
+            return false;
+        
+        return true;
+    }
 }
