7c021cd2bbc76388659011bced2e791ce61a60f5
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-02-02 22:02:07 +0530

    Time: 1 ms (65.29%), Space: 40.9 MB (81.13%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -5,27 +5,26 @@ class Solution {
         if(n==1)
             return true;
         
-        Map<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[26];
         
         for(int ind=0; ind<26; ind++){
-            map.put(order.charAt(ind), ind);
+            arr[order.charAt(ind)-'a'] = ind;
         }
-        map.put(' ', -1);
         
         for(int ind=1; ind<n; ind++){
-            if(pre_greater(new StringBuilder(words[ind-1]), new StringBuilder(words[ind]), map))
+            if(pre_greater(new StringBuilder(words[ind-1]), new StringBuilder(words[ind]), arr))
                 return false;
         }
         
         return true;
     }
     
-    public boolean pre_greater(StringBuilder pre, StringBuilder curr, Map<Character, Integer> map){
+    public boolean pre_greater(StringBuilder pre, StringBuilder curr, int[] arr){
         int pre_len = pre.length(), curr_len = curr.length();
         
         for(int ind=0; ind<Math.min(pre_len, curr_len); ind++){
             if(pre.charAt(ind)!=curr.charAt(ind)){
-                if(map.get(pre.charAt(ind)) > map.get(curr.charAt(ind)))
+                if(arr[pre.charAt(ind)-'a'] > arr[curr.charAt(ind)-'a'])
                     return true;
                 else
                     return false;
