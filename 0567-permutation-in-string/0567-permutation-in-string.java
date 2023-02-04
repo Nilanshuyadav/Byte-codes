74939241b19f169ebce98ee2904e70c3624aed3e
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-02-04 19:39:18 +0530

    Time: 4 ms (92.11%), Space: 41.7 MB (98.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,44 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq = new int[26];
-        int n1=  s1.length(), n2 = s2.length();
-
-        if(n2<n1) return false;
-        for(int ind=0; ind<n1; ind++) freq[s1.charAt(ind)-'a']++;
-
-        int[] toCheck = new int[26];
-
-        for(int ind=0; ind<=n2; ind++){
-            if(ind<n1){
-                toCheck[s2.charAt(ind)-'a']++;
+        int len1 = s1.length(), len2 = s2.length();
+        
+        if(len1>len2)
+            return false;
+        
+        int[] fre = new int[26];
+        
+        for(int ind=0; ind<len1; ind++){
+            fre[s1.charAt(ind)-'a']++;
+        }
+        
+        int fre2[] = new int[26];
+        
+        for(int ind=0; ind<len2; ind++){
+            if(ind<len1-1){
+                fre2[s2.charAt(ind)-'a']++;
+            }
+            else if(ind==len1-1){
+                fre2[s2.charAt(ind)-'a']++;
+                
+                if(compare(fre, fre2))
+                    return true;
             }
             else{
-                int inx=0;
-                for(inx=0; inx<26; inx++) if(freq[inx] != toCheck[inx]) break;
-
-                if(inx==26) return true;
-
-                if(ind != n2){
-                    toCheck[s2.charAt(ind)-'a']++;
-                    toCheck[s2.charAt(ind-n1)-'a']--;
-                }
+                fre2[s2.charAt(ind)-'a']++;
+                fre2[s2.charAt(ind-len1)-'a']--;
+                
+                if(compare(fre, fre2))
+                    return true;
             }
         }
-
+        
         return false;
+    }
+    
+    public boolean compare(int[] fre1, int[] fre2){
+        for(int ind=0; ind<26; ind++)
+            if(fre1[ind] != fre2[ind])
+                return false;
+        return true;
     }
 }
