f9db6059c39aef9f3f15260c6c6cf7e5835094fc
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-02-16 19:33:05 +0530

    Time: 33 ms (38.09%), Space: 52.1 MB (68.33%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.java
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minCapability(int[] nums, int k) {
         int n = nums.length;
-        int l=0, h=n-1, m;
-        
-        int[] temp = new int[n];
+        int l=1, max=0, h=max, m;
         
         for(int ind=0; ind<n; ind++)
-            temp[ind] = nums[ind];
+            max = Math.max(max, nums[ind]);
         
-        Arrays.sort(temp);
+        h = max;
         
         while(l<h){
             m = l + (h-l)/2;
             
-            if(isPossible(temp[m], nums, k)) h=m;
+            if(isPossible(m, nums, k)) h=m;
             else l=m+1;
         }
         
-        return temp[l];
+        return l;
     }
     
     public boolean isPossible(int inx, int[] nums, int k){
