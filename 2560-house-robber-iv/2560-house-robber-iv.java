dcc787b31e3d572b95cc56d883a7a5e5f6206575
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-02-16 19:00:40 +0530

    Time: 34 ms (36.23%), Space: 51.9 MB (84.31%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int n = nums.length;
+        int l=1, h=1000000000, m;
+        
+        while(l<h){
+            m = l + (h-l)/2;
+            
+            if(isPossible(m, nums, k)) h=m;
+            else l=m+1;
+        }
+        
+        return l;
+    }
+    
+    public boolean isPossible(int inx, int[] nums, int k){
+        int start=0, last_picked=-2, cnt=0;
+        
+        for(int ind=0; ind<nums.length; ind++){
+            if(nums[ind]>inx) continue;
+            if(last_picked==ind-1) continue;
+            
+            cnt++;
+            last_picked = ind;
+        }
+        
+        return cnt>=k;
+    }
+}
