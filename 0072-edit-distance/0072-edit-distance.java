97a920283331bbf0d74e4b35bc0200ec4ebbd516
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-02-27 01:39:50 +0530

    Time: 6 ms (67.58%), Space: 42.7 MB (25.80%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,36 +1,26 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()][word2.length()];
+        int n = word1.length(), m = word2.length();
         
-        for(int ind[] : dp)
-            Arrays.fill(ind, -1);
+        int[][] dp = new int[n+1][m+1];
         
-        return solve(word1.length()-1, word2.length()-1, word1, word2, dp);
-    }
-    
-    public int solve(int i, int j, String s1, String s2, int[][] dp){
-        if(i==-1 || j==-1){
-            if(i==-1)
-                return j+1;
-            else
-                return i+1;
+        for(int i=0; i<=n; i++) dp[i][0] = i;
+        for(int j=0; j<=m; j++) dp[0][j] = j;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    
+                    dp[i][j] = 1 + Math.min(dp[i][j-1],
+                                       Math.min(dp[i-1][j],
+                                               dp[i-1][j-1]));
+                }
+            }
         }
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        char ch_i = s1.charAt(i), ch_j = s2.charAt(j);
-        int insert = (int)1e8, delete=(int)1e8, replace=(int)1e8;
-        
-        if(ch_i == ch_j){
-            return dp[i][j] = solve(i-1, j-1, s1, s2, dp);
-        }
-        else{
-            insert = 1 + solve(i, j-1, s1, s2, dp);
-            delete = 1 + solve(i-1, j, s1, s2, dp);
-            replace = 1 + solve(i-1, j-1, s1, s2, dp);
-        }
-        
-        return dp[i][j] = Math.min(insert, Math.min(delete, replace));
+        return dp[n][m];
     }
 }
