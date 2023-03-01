ae2338ae25cac4354846b7473aec977957ba89e3
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-03-02 02:55:11 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        int xor=0;
+        
+        for(int ind : nums)
+            xor ^= ind;
+            
+        int mask = xor&(-xor);
+        
+        int one=0, zero=0;
+        for(int ind : nums){
+            if((ind&mask) > 0)
+                one ^= ind;
+            else
+                zero ^= ind;
+        }
+        
+        return new int[]{Math.min(one, zero), Math.max(one, zero)};
+    }
+}
