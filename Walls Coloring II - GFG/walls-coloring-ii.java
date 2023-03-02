b10e51e78fabdb7fa3785e7bfb52a82bd1b70066
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-03-02 18:10:20 +0530

    Added solution - LeetHub

--- Walls Coloring II - GFG/walls-coloring-ii.java
+++ Walls Coloring II - GFG/walls-coloring-ii.java
@@ -34,6 +34,7 @@ class GFG{
 class Solution{
 	int minCost(int [][] costs) {
 		int n = costs.length, k = costs[0].length;
+		int[] min1=new int[]{-1,Integer.MAX_VALUE}, min2=new int[]{-1, Integer.MAX_VALUE};
 		
 		if(k==1){
 		    if(n==0)    return 0;
@@ -48,33 +49,79 @@ class Solution{
 		
 		for(int ind=0; ind<k; ind++){
 		    dp[0][ind] = costs[0][ind];
-		    pq.add(new int[]{ind, costs[0][ind]});
-		}    
 		    
-		int temp[] = {-1, -1}, min;
-		boolean isTaken = false;
-		for(int ind=1; ind<n; ind++){
-		    for(int i=0; i<k; i++){
-		        if(pq.peek()[0] == i){
-		            temp = pq.remove();
-		            isTaken = true;
-		        }
+		    if(costs[0][ind] <= min1[1]){
+		        min2[0] = min1[0];
+		        min2[1] = min1[1];
 		        
-		        min = pq.peek()[1];
-		        
-		        if(isTaken){
-		            pq.add(temp);
-		            isTaken = false;
-		        }
-		        
-		        dp[ind][i] = min + costs[ind][i];
-		        
-		        al.add(new int[]{i, dp[ind][i]});
+		        min1[0] = ind;
+		        min1[1] = costs[0][ind];
+		    }
+		    else if(costs[0][ind] < min2[1]){
+		        min2[0] = ind;
+		        min2[1] = costs[0][ind];
 		    }
 		    
-		    pq.clear();
-		    for(int inx[] : al) pq.add(inx);
-		    al.clear();
+		  //  pq.add(new int[]{ind, costs[0][ind]});
+		}    
+		    
+		int temp[] = {-1, -1}, min, temp_min1[]={-1, Integer.MAX_VALUE}, temp_min2[]={-1, Integer.MAX_VALUE};
+		boolean isTaken = false;
+		for(int ind=1; ind<n; ind++){
+		    
+		    temp_min1[1] = Integer.MAX_VALUE;
+		    temp_min2[1] = Integer.MAX_VALUE;
+		    
+		    for(int i=0; i<k; i++){
+		        if(min1[0] == i)
+		            min = min2[1];
+		        else
+		            min = min1[1];
+		            
+		            
+		        dp[ind][i] = min + costs[ind][i];        
+		        
+		        
+		        
+		        if(dp[ind][i] <= temp_min1[1]){
+    		        temp_min2[0] = temp_min1[0];
+    		        temp_min2[1] = temp_min1[1];
+    		        
+    		        temp_min1[0] = i;
+    		        temp_min1[1] = dp[ind][i];
+    		    }
+    		    else if(dp[ind][i] < temp_min2[1]){
+    		        temp_min2[0] = i;
+    		        temp_min2[1] = dp[ind][i];
+    		    }
+    		    
+		        
+		      //  if(pq.peek()[0] == i){
+		      //      temp = pq.remove();
+		      //      isTaken = true;
+		      //  }
+		        
+		      //  min = pq.peek()[1];
+		        
+		      //  if(isTaken){
+		      //      pq.add(temp);
+		      //      isTaken = false;
+		      //  }
+		        
+		      //  dp[ind][i] = min + costs[ind][i];
+		        
+		      //  al.add(new int[]{i, dp[ind][i]});
+		    }
+		    
+		    min1[0] = temp_min1[0];
+		    min1[1] = temp_min1[1];
+		    
+		    min2[0] = temp_min2[0];
+		    min2[1] = temp_min2[1];
+		    
+		  //  pq.clear();
+		  //  for(int inx[] : al) pq.add(inx);
+		  //  al.clear();
 		}    
 		
 		int mini = Integer.MAX_VALUE;
