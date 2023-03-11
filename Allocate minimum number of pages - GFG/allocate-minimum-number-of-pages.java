09a939f3ef301f7e740e5a366d0cf91b07bfa707
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-03-11 14:11:28 +0530

    Added solution - LeetHub

---
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    int m=sc.nextInt();
+		    Solution ob = new Solution();
+		    System.out.println(ob.findPages(a,n,m));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{
+    //Function to find minimum number of pages.
+    public static int findPages(int[]A,int N,int M)
+    {
+        if(M>N) return -1;
+        
+        int total = 0, max=Integer.MIN_VALUE;
+        for(int ind : A){
+            total += ind;
+            max = Math.max(max, ind);
+        }
+        
+        
+        int l=max, h=total+1, ans=-1, m, cnt;
+        
+        while(l<=h){
+            m = l + (h-l)/2;
+            
+            cnt = check(A, N, m, M);
+            
+            if(cnt <= M){
+                // ans = Math.min(ans, m);
+                ans=m;
+                h=m-1;
+            }
+            if(cnt>M) l=m+1;
+        }
+        
+        return ans;
+    }
+    
+    public static int check(int[] A, int N, int m, int M){
+        int cnt=0;
+        long sum=0;
+        
+        for(int ind=0; ind<N; ind++){
+            
+            if(sum+A[ind] > m){
+                cnt++;
+                sum=0;
+            }
+            
+            sum += A[ind];
+        }
+        cnt++;
+        
+        return cnt;
+    }
+};
