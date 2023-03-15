bcd53a75066c43097cc62dcaca41f829bfc2758b
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-03-15 13:05:52 +0530

    Time: 36 ms (25.52%), Space: 42.7 MB (29.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,21 +2,22 @@ class Solution {
     int max = (int)1e8;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        Map<String, Integer> dp = new HashMap<>();
+        int[][] dp = new int[n][amount+1];
+        
+        for(int ind[] : dp)
+            Arrays.fill(ind, -1);
 
         int temp = solve(n-1, coins, amount, dp);
         return temp>=max ? -1 : temp;
     }
     
-    public int solve(int ind, int[] coins, int amount, Map<String, Integer> dp){
+    public int solve(int ind, int[] coins, int amount, int[][] dp){
         if(ind==0){
             return amount%coins[0] == 0? amount/coins[0] : max;
         }
         
-        String st = "" + ind + "," + amount;
-        
-        if(dp.containsKey(st))
-            return dp.get(st);
+        if(dp[ind][amount] != -1)
+            return dp[ind][amount];
         
         int pick = max, not_pick=max, min;
         
@@ -29,8 +30,6 @@ class Solution {
         
         min = Math.min(pick, not_pick);
         
-        dp.put(st, Math.min(min, max));
-        
-        return min>=max ? max : min;
+        return dp[ind][amount] = min>=max ? max : min;
     }
 }
