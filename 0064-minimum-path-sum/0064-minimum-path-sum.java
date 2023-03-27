e21d15459c9c997e950c5fbd76e894492955ae56
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-03-27 20:52:12 +0530

    Time: 2 ms (76.27%), Space: 45.7 MB (14.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,28 +3,23 @@ class Solution {
         int row = grid.length, col = grid[0].length;
         
         int[][] dp = new int[row][col];
+        dp[row-1][col-1] = grid[row-1][col-1];
         
-        for(int ind[] : dp)
-            Arrays.fill(ind, -1);
         
-        return solve(row-1, col-1, grid, dp);
-    }
-    
-    public int solve(int r, int c, int[][] grid, int[][] dp){
-        if(r==0 && c==0){
-            return grid[0][0];
+        for(int r=row-2; r>=0; r--){
+            dp[r][col-1] = dp[r+1][col-1]+grid[r][col-1];
         }
         
-        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length){
-            return (int)1e8;
+        for(int c=col-2; c>=0; c--){
+            dp[row-1][c] = dp[row-1][c+1]+grid[row-1][c];
         }
         
-        if(dp[r][c] != -1)
-            return dp[r][c];
+        for(int r=row-2; r>=0; r--){
+            for(int c=col-2; c>=0; c--){
+                dp[r][c] = Math.min(dp[r+1][c], dp[r][c+1])+grid[r][c];
+            }
+        }
         
-        int left = solve(r, c-1, grid, dp),
-            up = solve(r-1, c, grid, dp);
-        
-        return dp[r][c] = Math.min(left, up)+grid[r][c];
+        return dp[0][0];
     }
 }
