7a2479d68e2fcfeefc23c2c56449ac86564d4ee6
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-04-22 15:13:09 +0530

    Time: 27 ms (77.27%), Space: 40.5 MB (98.77%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -8,20 +8,24 @@ class Solution {
     public int lcs(String s1, String s2){
         int n = s1.length(), m = s2.length();
         
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[m+1], curr = new int[m+1];
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    curr[j] = 1+dp[j-1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    curr[j] = Math.max(dp[j], curr[j-1]);
                 }
+            }
+            
+            for(int ind=0; ind<=m; ind++){
+                dp[ind] = curr[ind];
             }
         }
         
-        return dp[n][m];
+        return dp[m];
     }
     
     public int helper(int ind1, int ind2, String s1, String s2, int[][] dp){
