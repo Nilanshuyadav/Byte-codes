baefcdc36c7e3131b7a854ac11779fa028f23ff5
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-10 11:57:10 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K;
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.totalCuts(N, K, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalCuts(int N, int K, int[] A) {
+        // code here
+        int[] largest = new int[N], smallest = new int[N];
+        largest[0] = A[0];
+        smallest[N-1] = A[N-1];
+        
+        for(int i=1, j=N-2; i<N; i++, j--){
+            largest[i] = Math.max(largest[i-1], A[i]);
+            smallest[j] = Math.min(smallest[j+1], A[j]);
+        }
+        
+        int count = 0;
+        
+        for(int ind=1; ind<N; ind++){
+            if(largest[ind-1]+smallest[ind] >= K){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+        
