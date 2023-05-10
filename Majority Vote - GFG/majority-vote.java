376ae5559a11a8c5004ff4ba54f0d05a25b20640
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-10 12:15:04 +0530

    Added solution - LeetHub

---
+++ Majority Vote - GFG/majority-vote.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.Solve(n, nums);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> Solve(int n, int[] nums) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int ind : nums){
+            map.put(ind, map.getOrDefault(ind, 0) + 1);
+        }
+        
+        int target = n/3;
+        for(int ind : map.keySet()){
+            if(map.get(ind) > target){
+                al.add(ind);
+            }
+        }
+        
+        return al.size()>0 ? al : new ArrayList<>(Arrays.asList(-1));
+    }
+}
+        
