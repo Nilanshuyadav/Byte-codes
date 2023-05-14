399d2a0c5f0ddc38f0643c6ff284d63a65bdeb5b
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-15 00:11:29 +0530

    Added solution - LeetHub

---
+++ Maximum subset sum - GFG/maximum-subset-sum.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            long res = obj.findMaxSubsetSum(N, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    public static long findMaxSubsetSum(int N, int[] A) {
+        // code here
+        long[] arr = new long[N];
+        
+        arr[0] = A[0];
+        arr[1] = A[1]+Math.max(arr[0], 0);
+        
+        for(int ind=2; ind<N; ind++){
+            arr[ind] = A[ind]+Math.max(arr[ind-1], arr[ind-2]);
+        }
+        
+        return Math.max(arr[N-1], arr[N-2]);
+    }
+}
+        
