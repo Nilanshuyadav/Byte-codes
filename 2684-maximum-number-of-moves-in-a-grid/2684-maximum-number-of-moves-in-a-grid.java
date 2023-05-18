d22cb151895a242743d172fecec4d4fb46b3cd25
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-18 14:49:57 +0530

    Time: 27 ms (17.37%), Space: 51.9 MB (75.90%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -4,12 +4,37 @@ class Solution {
         int max = 0;
         int[][] dp = new int[row][col];
         
-        for(int ind[] : dp){
-            Arrays.fill(ind, -1);
+//         for(int ind[] : dp){
+//             Arrays.fill(ind, -1);
+//         }
+        
+//         for(int ind=0; ind<row; ind++){
+//             max = Math.max(max, solve(ind, 0, grid, row, col, dp));
+//         }
+        
+        int new_r, new_c;
+        
+        for(int c=col-2; c>=0; c--){
+            for(int r=0; r<row; r++){
+                max = 0;
+                
+                for(int ind=0; ind<3; ind++){
+                    new_r = r+r_arr[ind];
+                    new_c = c+1;
+                    
+                    if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || grid[new_r][new_c]<=grid[r][c])
+                        continue;
+                    
+                    max = Math.max(max, dp[new_r][new_c]+1);
+                }
+                
+                dp[r][c] = max;
+            }
         }
         
+        max = 0;
         for(int ind=0; ind<row; ind++){
-            max = Math.max(max, solve(ind, 0, grid, row, col, dp));
+            max = Math.max(max, dp[ind][0]);
         }
         
         return max;
