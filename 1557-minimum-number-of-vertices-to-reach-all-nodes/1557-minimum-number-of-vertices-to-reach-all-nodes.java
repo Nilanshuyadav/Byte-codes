7edee004acb146753385146f77ad422e39086bc3
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-18 14:08:50 +0530

    Time: 11 ms (77.29%), Space: 90.3 MB (24.73%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,72 +1,18 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        Stack<Integer> st = new Stack<>();
-        
-        List<List<Integer>> adj = new ArrayList<>();
-        
-        for(int ind=0; ind<n; ind++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for(List<Integer> al : edges){
-            adj.get(al.get(0)).add(al.get(1));
-        }
-        
-        boolean[] vis = new boolean[n];
-        
-        for(int ind=0; ind<n; ind++){
-            if(!vis[ind]){
-                findTopo(ind, st, adj, vis);
-            }
-        }
-        
-        int cnt=0;
-        for(int ind=0; ind<n; ind++){
-            vis[ind] = false;
-        }
-        
         List<Integer> ans = new ArrayList<>();
-        int temp;
+        boolean[] inDegree = new boolean[n];
         
-        while(!st.isEmpty()){
-            temp = st.pop();
-            
-            if(vis[temp]){
-                continue;
-            }
-            
-            cnt += findRange(temp, adj, vis);
-            ans.add(temp);
-            
-            if(cnt == n){
-                break;
+        for(int ind=0; ind<edges.size(); ind++){
+            inDegree[edges.get(ind).get(1)] = true;
+        }
+        
+        for(int ind=0; ind<n; ind++){
+            if(!inDegree[ind]){
+                ans.add(ind);
             }
         }
         
         return ans;
-    }
-    
-    public int findRange(int ind, List<List<Integer>> adj, boolean[] vis){
-        int sum = 0;
-        
-        for(int inx : adj.get(ind)){
-            if(!vis[inx]){
-                sum += findRange(inx, adj, vis);
-            }    
-        }
-        
-        vis[ind] = true;
-        return sum+1;
-    }
-    
-    public void findTopo(int ind, Stack<Integer> st, List<List<Integer>> adj, boolean[] vis){
-        for(int inx : adj.get(ind)){
-            if(!vis[inx]){
-                findTopo(inx, st, adj, vis);
-            }
-        }
-        
-        vis[ind] = true;
-        st.add(ind);
     }
 }
