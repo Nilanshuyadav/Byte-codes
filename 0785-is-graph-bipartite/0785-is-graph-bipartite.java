ecf189ebffcb1b7b537911a7ca823732146dd982
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-19 11:57:40 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (6.63%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,34 +1,31 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        boolean[] vis = new boolean[n];
+        Boolean[] vis = new Boolean[n];
         boolean ans = true;
-        int[] color = new int[n];
         
         for(int ind=0; ind<n; ind++){
-            if(!vis[ind]){
-                color[ind] = 1;
+            if(vis[ind] == null){
                 vis[ind] = true;
-                ans = ans && checkBipartite(ind, graph, vis, color);
+                ans = ans && checkBipartite(ind, graph, vis);
             }
         }
         
         return ans;
     }
     
-    public boolean checkBipartite(int ind, int[][] graph, boolean[] vis, int[] color){
+    public boolean checkBipartite(int ind, int[][] graph, Boolean[] vis){
         
         boolean ans = true;
         
         for(int inx : graph[ind]){
-            if(vis[inx]){
-                if(color[inx] == color[ind])
+            if(vis[inx] != null){
+                if(vis[inx] == vis[ind])
                     return false;
             }
             else{
-                vis[inx] = true;
-                color[inx] = (color[ind]+1)%2;
-                ans = ans && checkBipartite(inx, graph, vis, color);
+                vis[inx] = !vis[ind];
+                ans = ans && checkBipartite(inx, graph, vis);
             }
         }
         
