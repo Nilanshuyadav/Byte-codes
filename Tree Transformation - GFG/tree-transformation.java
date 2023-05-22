56a31d7a07303b9a361d412f5dcdcfc75c8d9a71
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-22 15:38:51 +0530

    Added solution - LeetHub

---
+++ Tree Transformation - GFG/tree-transformation.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] p = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.solve(N, p);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int solve(int N, int[] p) {
+        int[] count = new int[N];
+        
+        for(int ind=1; ind<N; ind++){
+            count[ind]++;
+            count[p[ind]]++;
+        }
+        
+        int ans = 0;
+        
+        for(int ind : count){
+            if(ind == 1){
+                ans++;
+            }
+        }
+        
+        return Math.max(0, N - ans - 1);
+        
+        // Set<Integer> set = new HashSet<>();
+        
+        // for(int ind : p){
+        //     set.add(ind);
+        // }
+        
+        // return N - (N - (set.size()-1)) - 1;
+    }
+}
+        
