32175832dbd549ce711fdb4cf88d3a8cc0c33457
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-24 14:09:54 +0530

    Time: 93 ms (90.80%), Space: 60.1 MB (50.00%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -7,25 +7,24 @@ class Solution {
             arr[ind] = new Pair(nums1[ind], nums2[ind]);
         }
         
-        Arrays.sort(arr, (a, b) -> b.num2-a.num2);
-        
+        Arrays.sort(arr, (a,b) -> b.nums2 - a.nums2);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        long sum = 0, ans=0;
-        int size=0;
+        long sum = 0, ans = 0;
+        int size = 0;
         
         for(Pair ind : arr){
-            if(size==k){
+            if(size == k){
                 sum -= pq.remove();
                 size--;
-            }
+            }    
             
-            sum += ind.num1;
-            pq.add(ind.num1);
+            sum += ind.nums1;
+            pq.add(ind.nums1);
             size++;
             
-            if(size==k){
-                ans = Math.max(ans, sum*ind.num2);
+            if(size == k){
+                ans = Math.max(ans, sum * ind.nums2);
             }
         }
         
@@ -34,10 +33,10 @@ class Solution {
 }
 
 class Pair{
-    int num1, num2;
+    int nums1, nums2;
     
-    public Pair(int num1, int num2){
-        this.num1 = num1;
-        this.num2 = num2;
+    public Pair(int nums1, int nums2){
+        this.nums1 = nums1;
+        this.nums2 = nums2;
     }
 }
