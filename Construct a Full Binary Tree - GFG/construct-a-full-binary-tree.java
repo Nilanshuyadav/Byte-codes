31ea750702dfd66c8ede72352a9fc64bebb355ff
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-05-24 10:50:21 +0530

    Added solution - LeetHub

--- Construct a Full Binary Tree - GFG/construct-a-full-binary-tree.java
+++ Construct a Full Binary Tree - GFG/construct-a-full-binary-tree.java
@@ -60,32 +60,45 @@ class ConstructTree
 
 //User function Template for Java
 
+
 class Solution
 {
     int lvl;
     int stop;
     int index;
+    
+    int[] preo, preoM;
     public Node constructBTree(int pre[], int preM[], int size)
     {
         // your code here
-        lvl = (int)(Math.log(size) / Math.log(2));
+        lvl = (int)(Math.log(size)/Math.log(2));
         stop = size/2 + 1;
         index = 1;
+        
+        preo = pre;
+        preoM = preM;
+        
         Node root = new Node(pre[0]);
-        helper(root, root, pre, preM,  0);
+        solve(root, root, 0);
+        
         return root;
     }
-    public void helper(Node n1, Node n2, int[] pre, int[] preM,  
-    int level){
-        if(lvl == level) return;
-        if(index == stop) return;
-        n1.left = new Node(pre[index]);
-        n2.right = new Node(preM[index]);
-        index = index+1;
-        helper(n1.left, n2.right, pre, preM, level+1);
-        n1.right = new Node(pre[index]);
-        n2.left = new Node(preM[index]);
-        index = index+1;
-        helper(n1.right, n2.left, pre, preM, level+1);
+    
+    public void solve(Node n1, Node n2, int temp_lvl){
+        if(temp_lvl == lvl || index==stop)
+            return;
+            
+        n1.left = new Node(preo[index]);
+        n2.right = new Node(preoM[index]);
+        
+        index++;
+        solve(n1.left, n2.right, temp_lvl+1);
+        
+        n1.right = new Node(preo[index]);
+        n2.left = new Node(preoM[index]);
+        
+        index++;
+        solve(n1.right, n2.left, temp_lvl+1);
     }
+    
 }
