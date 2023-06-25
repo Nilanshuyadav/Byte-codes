99befcd72579fa882e2d6c25a649b6c87bf8782d
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-06-25 12:28:50 +0530

    Time: 33 ms (68.75%), Space: 43.9 MB (75.00%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.java
+++ 0956-tallest-billboard/0956-tallest-billboard.java
@@ -1,24 +1,27 @@
 class Solution {
     public int tallestBillboard(int[] rods) {
         int n = rods.length;
-        return solve(0, n, 0, rods, new HashMap<>());
-    }
-    
-    public int solve(int ind, int n, int diff, int[] rods, Map<String, Integer> dp){
-        if(ind == n){
-            return diff==0 ? 0 : -(int)1e8;
+        int[][] dp = new int[n][10001];
+        for(int ind[] : dp){
+            Arrays.fill(ind, -1);
         }
         
-        String st = ""+ind+","+diff;
-        if(dp.containsKey(st)){
-            return dp.get(st);
+        return solve(0, n, 5000, rods, dp);
+    }
+    
+    public int solve(int ind, int n, int diff, int[] rods, int[][] dp){
+        if(ind == n){
+            return diff==5000 ? 0 : -(int)1e8;
+        }
+        
+        if(dp[ind][diff] != -1){
+            return dp[ind][diff];
         }
         
         int bucket1 = rods[ind] + solve(ind+1, n, diff-rods[ind], rods, dp);
         int bucket2 = solve(ind+1, n, diff+rods[ind], rods, dp);
         int bucket3 = solve(ind+1, n, diff, rods, dp);
         
-        dp.put(st, Math.max(bucket1, Math.max(bucket2, bucket3)));
-        return dp.get(st);
+        return dp[ind][diff] = Math.max(bucket1, Math.max(bucket2, bucket3));
     }
 }
