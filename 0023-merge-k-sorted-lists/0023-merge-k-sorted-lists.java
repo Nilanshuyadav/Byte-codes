b8051d48b1af870905b08190509785139736333f
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-06-25 19:46:23 +0530

    Time: 103 ms (19.35%), Space: 43.6 MB (71.23%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -12,31 +12,40 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
         
-        if(n==0)    return null;
+        if(n == 0){
+            return null;
+        }
         
         ListNode dummy = new ListNode();
         
-        return solve(0, lists, n);
-    }
-    
-    public ListNode solve(int ind, ListNode[] lists, int n){
-        if(ind == n-1){
-            return lists[ind];
+        for(int ind=0; ind<n; ind++){
+            if(lists[ind] != null){
+                if(dummy.next == null){
+                    dummy.next = lists[ind];
+                }    
+                else{
+                    dummy = merge(dummy, dummy.next, lists[ind]);
+                }
+            }
         }
         
-        ListNode dummy = new ListNode(), curr1 = lists[ind], curr2 = solve(ind+1, lists, n), curr=dummy;
+        return dummy.next;
+    }
+    
+    public ListNode merge(ListNode dummy, ListNode l1, ListNode l2){
+        ListNode curr1 = l1, curr2 = l2, curr = dummy;
         
-        while(curr1!=null && curr2!=null){
+        while(curr1 != null && curr2 != null){
             if(curr1.val < curr2.val){
                 curr.next = curr1;
-                
                 curr = curr1;
+                
                 curr1 = curr1.next;
             }
             else{
                 curr.next = curr2;
-                
                 curr = curr2;
+                
                 curr2 = curr2.next;
             }
         }
@@ -44,10 +53,11 @@ class Solution {
         if(curr1 != null){
             curr.next = curr1;
         }
-        else{
+        
+        if(curr2 != null){
             curr.next = curr2;
         }
         
-        return dummy.next;
+        return dummy;
     }
 }
