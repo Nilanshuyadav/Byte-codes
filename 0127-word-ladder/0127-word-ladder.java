546ad7f56f97dec28563d3e47622fcb617e1d071
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-06-27 13:44:14 +0530

    Time: 74 ms (81.19%), Space: 45.6 MB (47.79%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,6 +1,6 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> q = new LinkedList<>();
+        Queue<String> q = new LinkedList<>();
         Set<String> set = new HashSet<>();
         int size=0, n=beginWord.length();
         
@@ -9,54 +9,45 @@ class Solution {
         }
         
         if(!set.contains(endWord))   return 0;
-        String curr_st, str;
-        int curr_steps;
+        String temp, str;
+        int curr_steps=0;
         
-        q.add(new Pair(beginWord, 1));
-        Pair temp;
+        q.add(beginWord);
         char pre;
         
         while(!q.isEmpty()){
-            temp = q.remove();
-
-            curr_st = temp.st;
-            curr_steps = temp.steps;
-
-            if(curr_st.equals(endWord)){
-                return curr_steps;
-            }
-
-            StringBuilder sb = new StringBuilder(curr_st);
-
-            for(int ind=0; ind<n; ind++){
-                pre = sb.charAt(ind);
-
-                for(char ch='a'; ch<='z'; ch++){
-                    sb.setCharAt(ind, ch);
-                    str = sb.toString();
-
-                    if(set.contains(str)){
-                        set.remove(str);
-
-                        q.add(new Pair(str, curr_steps+1));
-                    }
+            size = q.size();
+            curr_steps++;
+            
+            while(size-->0){
+                temp = q.remove();
+                
+                if(temp.equals(endWord)){
+                    return curr_steps;
                 }
 
-                sb.setCharAt(ind, pre);
+                StringBuilder sb = new StringBuilder(temp);
+
+                for(int ind=0; ind<n; ind++){
+                    pre = sb.charAt(ind);
+
+                    for(char ch='a'; ch<='z'; ch++){
+                        sb.setCharAt(ind, ch);
+                        str = sb.toString();
+
+                        if(set.contains(str)){
+                            set.remove(str);
+
+                            q.add(str);
+                        }
+                    }
+
+                    sb.setCharAt(ind, pre);
+                }    
             }
         
         }
         
         return 0;
-    }
-}
-
-class Pair{
-    String st;
-    int steps;
-    
-    public Pair(String st, int steps){
-        this.st = st;
-        this.steps = steps;
     }
 }
