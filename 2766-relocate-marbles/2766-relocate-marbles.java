0e6c8208c5cbc388cb119a28cf3867f43cb61df1
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-12 10:13:27 +0530

    Time: 67 ms (86.42%), Space: 61.4 MB (17.09%) - LeetHub

---
+++ 2766-relocate-marbles/2766-relocate-marbles.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
+        Set<Integer> set = new HashSet<>();
+        
+        for(int ind : nums){
+            set.add(ind);
+        }
+        
+        for(int ind : moveFrom){
+            set.remove(ind);
+        }
+        
+        for(int ind=0; ind<moveFrom.length; ind++){
+            set.remove(moveFrom[ind]);
+            set.add(moveTo[ind]);
+        }
+        
+        List<Integer> al = new ArrayList<>(set);
+        
+        Collections.sort(al);
+        
+        return al;
+    }
+}
