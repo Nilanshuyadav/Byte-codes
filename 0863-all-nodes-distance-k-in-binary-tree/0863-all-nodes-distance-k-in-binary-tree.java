16791f7bac7bfe3f693e8d11f402012d0a58c011
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-12 19:45:40 +0530

    Time: 11 ms (91.29%), Space: 41.8 MB (71.79%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -11,55 +11,48 @@ class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         Map<TreeNode, TreeNode> map = new HashMap<>();
         
-        get_parent(root, null, map);
+        addParent(root, map);
         
-        Queue<TreeNode> q = new LinkedList<>();
-        Set<Integer> set = new HashSet<>();
+        TreeNode tar = findTar(root, target);
         
-        q.add(target);
-        set.add(target.val);
+        List<Integer> al = new ArrayList<>();
         
-        while(!q.isEmpty()){
-            if(k == 0)
-                break;
-            
-            int size = q.size();
-            
-            while(size-->0){
-                TreeNode temp = q.remove();
-                
-                if(temp.left != null && !set.contains(temp.left.val)){
-                    set.add(temp.left.val);
-                    q.add(temp.left);
-                }
-                
-                if(temp.right != null && !set.contains(temp.right.val)){
-                    set.add(temp.right.val);
-                    q.add(temp.right);
-                }
-                
-                if(temp!=root && !set.contains(map.get(temp).val)){
-                    set.add(map.get(temp).val);
-                    q.add(map.get(temp));
-                }
-            }
-            
-            k--;
-        }
+        solve(tar, map, al, k, new TreeNode(-1));
         
-        List<Integer> ans = new ArrayList<>();
-        
-        while(!q.isEmpty())
-            ans.add(q.remove().val);
-        
-        return ans;
+        return al;
     }
     
-    public void get_parent(TreeNode child, TreeNode parent, Map<TreeNode, TreeNode> map){
-        if(child != null){
-            map.put(child, parent);
-            get_parent(child.left, child, map);
-            get_parent(child.right, child, map);
+    public void solve(TreeNode tar, Map<TreeNode, TreeNode> map, List<Integer> al, int k, TreeNode pre){
+        if(k==0){
+            al.add(tar.val);
+            return;
         }
+        
+        if(tar.left!=pre && tar.left!=null) solve(tar.left, map, al, k-1, tar);
+        if(tar.right!=pre && tar.right!=null) solve(tar.right, map, al, k-1, tar);
+        if(map.containsKey(tar) && map.get(tar)!=pre) solve(map.get(tar), map, al, k-1, tar);
+    }
+    
+    public TreeNode findTar(TreeNode root, TreeNode target){
+        if(root==null || root==target){
+            return root;
+        }
+        
+        TreeNode left = findTar(root.left, target),
+            right = findTar(root.right, target);
+        
+        if(left!=null)  return left;
+        
+        return right;
+    }
+    
+    public void addParent(TreeNode root, Map<TreeNode, TreeNode> map){
+        if(root == null)    return;
+        
+        if(root.left != null)   map.put(root.left, root);
+        if(root.right != null)  map.put(root.right, root);
+        
+        addParent(root.left, map);
+        addParent(root.right, map);
     }
 }
