07c59b50ee79deee5656976cad34ea2439f004f1
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-12 16:53:46 +0530

    Time: 10 ms (11.69%), Space: 44.3 MB (6.10%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.java
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.java
@@ -1,12 +1,37 @@
 class Solution {
     public int minimumBeautifulSubstrings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, (int)1e8);
+        StringBuilder sb;
+        int min;
         
-        int temp = s.charAt(0)!='0' ? solve(0, s, new StringBuilder(""), n, dp) : -1;
+        dp[n] = 0;
         
-        return temp!=(int)1e8 ? temp : -1;
+        for(int ind=n-1; ind>=0; ind--){
+            min = (int)1e8;
+            if(s.charAt(ind) == '0')    continue;
+            
+            sb = new StringBuilder("");
+            
+            for(int i=ind; i<n; i++){
+                sb.append(s.charAt(i));
+            
+                if((i==n-1 || s.charAt(i+1)!='0') && canWe(sb)){
+                    min = Math.min(min, dp[i+1] + 1);
+                }
+            }
+            
+            dp[ind] = min;
+        }
+        
+        return dp[0]==(int)1e8 ? -1 : dp[0];
+        
+//         Arrays.fill(dp, -1);
+        
+//         int temp = s.charAt(0)!='0' ? solve(0, s, new StringBuilder(""), n, dp) : -1;
+        
+//         return temp!=(int)1e8 ? temp : -1;
         
     }
     
