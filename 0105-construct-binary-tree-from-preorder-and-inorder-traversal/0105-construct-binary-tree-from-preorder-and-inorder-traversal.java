4743c2ceac095d2436c90100b8949b376615eee9
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-24 20:18:16 +0530

    Time: 2 ms (95.99%), Space: 43.5 MB (41.30%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,26 +16,31 @@
 class Solution {
     int ind=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return solve(0, preorder.length-1, preorder, inorder);
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = inorder.length;
+        
+        for(int ind=0; ind<n; ind++) map.put(inorder[ind], ind);
+        
+        return solve(0, n-1, preorder, inorder, map);
     }
     
-    public TreeNode solve(int l, int r, int[] preorder, int[] inorder){
+    public TreeNode solve(int l, int r, int[] preorder, int[] inorder, Map<Integer, Integer> map){
         
         if(l==r){
             return new TreeNode(preorder[ind]);
         }
         
         TreeNode node = new TreeNode(preorder[ind]);
-        int index = findIndex(preorder[ind], inorder);
+        int index = map.get(preorder[ind]);
         
         if(l<=index-1){
             ind++;
-            node.left = solve(l, index-1, preorder, inorder);    
+            node.left = solve(l, index-1, preorder, inorder, map);    
         }
         
         if(index+1<=r){
             ind++;
-            node.right = solve(index+1, r, preorder, inorder);    
+            node.right = solve(index+1, r, preorder, inorder, map);    
         }
         
         
