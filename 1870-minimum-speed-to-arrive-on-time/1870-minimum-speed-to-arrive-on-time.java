f5bb8e1a560b3ab871f1715b6794e7968937577d
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-26 12:53:31 +0530

    Time: 240 ms (29.07%), Space: 59.3 MB (17.65%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int l=1, h=(int)1e8, m, n=dist.length;
+        
+        while(l<=h){
+            m = l + (h-l)/2;
+            
+            double temp = canWe(dist, m, n);
+            
+            if(temp<=hour){
+                h = m-1;
+            }
+            else{
+                l = m+1;
+            }
+        }
+        
+        return h==(int)1e8 ? -1 : h+1;
+    }
+    
+    public double canWe(int[] dist, int m, int n){
+        double sum=0;
+        
+        for(int ind=0; ind<n; ind++){
+            if(ind==n-1){
+                sum += ((double)dist[ind])/m;
+                break;
+            }
+            
+            sum += Math.ceil((double)dist[ind]/m);
+        }
+        
+        return sum;
+    }
+}
