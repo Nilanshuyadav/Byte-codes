e8f69c8d6154014a03582dec96b287d6b6cf0a70
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-28 13:44:31 +0530

    Time: 1220 ms (5.20%), Space: 122.8 MB (5.31%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int n=nums.length, mask=0;
+        
+        for(int ind=0; ind<n; ind++){
+            mask |= (1<<ind);
+        }
+        
+        int[][] dp = new int[mask][2];
+        
+        mask = 0;
+        mask |= 1;
+        mask |= (1<<n-1);
+        
+        return solve(0, nums, mask, dp)<0 ? false : true;
+    }
+    
+    public int solve(int player, int[] nums, int mask, int[][] dp){
+        int[] pos = new int[2];
+        Arrays.fill(pos, -1);
+        
+        int k=0;
+        for(int ind=0; ind<21; ind++){
+            if((mask&(1<<ind)) != 0){
+                pos[k++] = ind;
+            }
+        }
+        
+        if(player == 1){
+            k=-1;
+        }
+        else{
+            k=1;
+        }
+        
+        if(pos[1]==-1){
+            return nums[pos[0]]*k;    
+        }
+        
+        int left_mask = 0;
+        left_mask |= (1<<pos[1]);
+        left_mask |= (1<<(pos[0]+1));
+        int left = (k*nums[pos[0]]) + solve((player+1)%2 , nums, left_mask, dp);
+        
+        int right_mask = 0;
+        right_mask |= (1<<pos[0]);
+        right_mask |= (1<<(pos[1]-1));
+        int right = (k*nums[pos[1]]) + solve((player+1)%2, nums, right_mask, dp);
+        
+        return player==1? Math.min(left, right) : Math.max(left, right);
+    }
+}
