bcaa7e77cc227b4abb07c1cff290d634aa14ac00
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-07-28 14:14:06 +0530

    Time: 2 ms (44.91%), Space: 40.7 MB (5.31%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -2,33 +2,37 @@ class Solution {
     public boolean PredictTheWinner(int[] nums) {
         int n=nums.length, mask=0;
         
-        for(int ind=0; ind<n; ind++){
-            mask |= (1<<ind);
-        }
+        // for(int ind=0; ind<n; ind++){
+        //     mask |= (1<<ind);
+        // }
         
-        int[][] dp = new int[mask+1][2];
-        for(int ind[] : dp){
-            Arrays.fill(ind, -1);
-        }
-        
-        mask = 0;
-        mask |= 1;
-        mask |= (1<<n-1);
-        
-        return solve(0, nums, mask, dp)<0 ? false : true;
-    }
-    
-    public int solve(int player, int[] nums, int mask, int[][] dp){
-        int[] pos = new int[2];
-        Arrays.fill(pos, -1);
-        
-        int k=0;
-        for(int ind=0; ind<21; ind++){
-            if((mask&(1<<ind)) != 0){
-                pos[k++] = ind;
+        int[][][] dp = new int[21][21][2];
+        for(int i[][] : dp){
+            for(int j[] : i){
+                Arrays.fill(j, -1);    
             }
         }
         
+        // mask = 0;
+        // mask |= 1;
+        // mask |= (1<<n-1);
+        
+        return solve(0, nums, 0, n-1, dp)<0 ? false : true;
+    }
+    
+    public int solve(int player, int[] nums, int pos1, int pos2, int[][][] dp){
+//         int[] pos = new int[2];
+//         Arrays.fill(pos, -1);
+        
+//         int k=0;
+//         for(int ind=0; ind<21; ind++){
+//             if((mask&(1<<ind)) != 0){
+//                 pos[k++] = ind;
+//             }
+//         }
+        
+        
+        int k;
         if(player == 1){
             k=-1;
         }
@@ -36,24 +40,20 @@ class Solution {
             k=1;
         }
         
-        if(pos[1]==-1){
-            return nums[pos[0]]*k;    
+        if(pos1==pos2){
+            return nums[pos1]*k;    
         }
         
-        if(dp[mask][player] != -1){
-            return dp[mask][player];
+        if(dp[pos1][pos2][player] != -1){
+            return dp[pos1][pos2][player];
         }
         
-        int left_mask = 0;
-        left_mask |= (1<<pos[1]);
-        left_mask |= (1<<(pos[0]+1));
-        int left = (k*nums[pos[0]]) + solve((player+1)%2 , nums, left_mask, dp);
+        // int left_mask = 0;
+        int left = (k*nums[pos1]) + solve((player+1)%2 , nums, pos1+1, pos2, dp);
         
-        int right_mask = 0;
-        right_mask |= (1<<pos[0]);
-        right_mask |= (1<<(pos[1]-1));
-        int right = (k*nums[pos[1]]) + solve((player+1)%2, nums, right_mask, dp);
+        // int right_mask = 0;
+        int right = (k*nums[pos2]) + solve((player+1)%2, nums, pos1, pos2-1, dp);
         
-        return dp[mask][player] = player==1? Math.min(left, right) : Math.max(left, right);
+        return dp[pos1][pos2][player] = player==1? Math.min(left, right) : Math.max(left, right);
     }
 }
