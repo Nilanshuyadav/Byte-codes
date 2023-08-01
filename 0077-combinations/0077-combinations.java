5bb9fdacf5c16955f98bf5be83c0cfdb3bfd572e
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-01 12:37:01 +0530

    Time: 1 ms (100.00%), Space: 45.5 MB (35.98%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,24 @@
 class Solution {
+    List<List<Integer>> al;
+    
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans = new ArrayList<>();
-        solve(1, n+1, k, new ArrayList<>(), ans);
-        return ans;
+        al = new ArrayList<>();
+        
+        solve(n, k, new ArrayList<>());
+        
+        return al;
     }
     
-    public void solve(int ind, int n, int k, List<Integer> temp, List<List<Integer>> ans){
-        if(ind==n){
-            if(k==0)
-                ans.add(new ArrayList<>(temp));
+    public void solve(int ind, int k, List<Integer> temp){
+        if(ind==0 || k==0 || ind<k){
+            if(k==0) al.add(new ArrayList<>(temp));
             return;
-        } 
-        
-        if(k>0){
-            temp.add(ind);
-            solve(ind+1, n, k-1, temp, ans);
-            temp.remove(temp.size()-1);
         }
         
-        solve(ind+1, n, k, temp, ans);
-    }
+        temp.add(ind);
+        if(k>0) solve(ind-1, k-1, temp);
+        
+        temp.remove(temp.size()-1);
+        solve(ind-1, k, temp);
+    } 
 }
