01abd69ceb6a89e17db712f64d23992802c70f88
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-03 15:16:11 +0530

    Time: 1 ms (78.73%), Space: 41.3 MB (49.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,64 +1,72 @@
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        int n = digits.length();
+    public List<String> letterCombinations(String s) {
+        int n = s.length();
         
-        if(n==0) return new ArrayList<>();
-        
-        Map<Character, List<String>> map = new HashMap<>();
-        
-        for(char ch='2'; ch<='9'; ch++){
-            List<String> al;
-            
-            switch(ch){
-                case '2':
-                    al = new ArrayList(Arrays.asList("a", "b", "c"));
-                    break;
-                case '3':
-                    al = new ArrayList(Arrays.asList("d", "e", "f"));
-                    break;
-                case '4':
-                    al = new ArrayList(Arrays.asList("g", "h", "i"));
-                    break;
-                case '5':
-                    al = new ArrayList(Arrays.asList("j", "k", "l"));
-                    break;
-                case '6':
-                    al = new ArrayList(Arrays.asList("m", "n", "o"));
-                    break;
-                case '7':
-                    al = new ArrayList(Arrays.asList("p", "q", "r", "s"));
-                    break;
-                case '8':
-                    al = new ArrayList(Arrays.asList("t", "u", "v"));
-                    break;
-                case '9':
-                    al = new ArrayList(Arrays.asList("w", "x", "y", "z"));
-                    break;
-                default:
-                    al = new ArrayList<>();
-            }
-            
-            map.put(ch, al);
+        if(n==0){
+            return new ArrayList<>();
         }
         
-        if(n==1)   return map.get(digits.charAt(0));
+        Map<Character, List<String>> map = new HashMap<>();
+    
+        char ch;
         
-        List<String> ans=new ArrayList<>();
-        List<String> al1=map.get(digits.charAt(0)) , al2;
+        for(int ind=0; ind<n; ind++){
+            ch = s.charAt(ind);
+            
+            if(!map.containsKey(ch)){
+                List<String> temp;
+                
+                switch(ch){
+                    case '2' :
+                        temp = new ArrayList<>(Arrays.asList("a","b","c"));
+                        break;
+                    case '3' :
+                        temp = new ArrayList<>(Arrays.asList("d","e","f"));
+                        break;
+                    case '4' :
+                        temp = new ArrayList<>(Arrays.asList("g","h","i"));
+                        break;
+                    case '5' :
+                        temp = new ArrayList<>(Arrays.asList("j","k","l"));
+                        break;
+                    case '6' :
+                        temp = new ArrayList<>(Arrays.asList("m","n","o"));
+                        break;
+                    case '7' :
+                        temp = new ArrayList<>(Arrays.asList("p","q","r","s"));
+                        break;
+                    case '8' :
+                        temp = new ArrayList<>(Arrays.asList("t","u","v"));
+                        break;
+                    default :    
+                        temp = new ArrayList<>(Arrays.asList("w","x","y","z"));
+                }
+                
+                map.put(ch, temp);
+            }
+        }
+        
+        List<String> temp2, temp1 = map.get(s.charAt(0)), ans = new ArrayList<>();
+        String st;
         
         for(int ind=1; ind<n; ind++){
-            al2 = map.get(digits.charAt(ind));
-            ans = new ArrayList<>();
+            ch = s.charAt(ind);
             
-            for(String st1 : al1){
-                for(String st2 : al2){
-                    ans.add(st1+st2);
+            temp2 = map.get(ch);
+            
+            for(String s1 : temp1){
+                for(String s2 : temp2){
+                    st = s1+s2;
+                    
+                    ans.add(st);
                 }
             }
             
-            al1 = ans;
+            temp1 = ans;
+            ans = new ArrayList<>();            
         }
         
-        return ans;
+        return temp1;
+            
     }
 }
