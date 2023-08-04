1c76c90f6e9023bc307bed38b0afbd56fe7ca0a0
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-04 12:00:07 +0530

    Time: 7 ms (60.52%), Space: 44 MB (14.85%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,33 +1,28 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        Boolean[] dp = new Boolean[n];
-        return solve(0, s, n, new HashSet<String>(wordDict), dp);
+        Boolean[] vis = new Boolean[n];
+        
+        return solve(0, n, s, new StringBuilder(""), new HashSet<>(wordDict), vis);    
     }
     
-    public boolean solve(int ind, String s, int n, Set<String> dict, Boolean[] dp){
+    public boolean solve(int ind, int n, String s, StringBuilder sb, Set<String> set, Boolean[] dp){
         if(ind == n){
             return true;
         }
         
-        if(dp[ind] != null) return dp[ind];
+        if(dp[ind] != null)  return dp[ind];
         
-        StringBuilder sb = new StringBuilder("");
         char ch;
         
-        boolean temp = false;
         for(int i=ind; i<n; i++){
             ch = s.charAt(i);
             
             sb.append(ch);
-            
-            if(dict.contains(sb.toString())){
-                temp |= solve(i+1, s, n, dict, dp);
-            }
-            
-            if(temp)  return dp[ind] = temp;
+            if(set.contains(sb.toString()) && solve(i+1, n, s, new StringBuilder(""), set, dp))
+                return true;
         }
         
-        return dp[ind] = temp;
+        return dp[ind] = false;
     }
 }
