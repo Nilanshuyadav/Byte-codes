6b4fae6b7865ef24b3eac35d664709958cbea11c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-04 23:26:23 +0530

    Time: 5 ms (27.85%), Space: 41.5 MB (6.93%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -2,20 +2,31 @@ class Solution {
     List<String> ans;
     public List<String> wordBreak(String s, List<String> wordDict) {
         ans = new ArrayList<>();
+        int n = s.length();
+        List<String>[] dp = new ArrayList[n];
         
-        solve(0, s.length(), s, "", new HashSet<>(wordDict));
+        List<String> ans = solve(0, s.length(), s, "", new HashSet<>(wordDict), dp);
+        
+        for(int ind=0; ind<ans.size(); ind++){
+            ans.set(ind, ans.get(ind).substring(1));
+        }
         
         return ans;
     }
     
-    public void solve(int ind, int n, String s, String temp, Set<String> set){
+    public List<String> solve(int ind, int n, String s, String temp, Set<String> set, List<String>[] dp){
         if(ind == n){
-            ans.add(temp.substring(1));
-            return;
+            return new ArrayList<>(Arrays.asList(""));
+        }
+        
+        if(dp[ind] != null){
+            return dp[ind];
         }
         
         StringBuilder sb = new StringBuilder("");
         char ch;
+        
+        List<String> toReturn = new ArrayList<>();
         
         for(int i=ind; i<n; i++){
             ch = s.charAt(i);
@@ -23,8 +34,16 @@ class Solution {
             sb.append(ch);
             
             if(set.contains(sb.toString())){
-                solve(i+1, n, s, temp+" "+sb, set);
+                List<String> returned = solve(i+1, n, s, temp+" "+sb, set, dp);
+                
+                if(returned.size()!=0){
+                    for(String st : returned){
+                        toReturn.add(" "+sb+st);
+                    }
+                }
             }
         }
+        
+        return dp[ind] = toReturn;
     }
 }
