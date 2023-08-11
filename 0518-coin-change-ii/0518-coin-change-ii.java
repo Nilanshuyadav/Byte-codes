f50290791d3dfdf4bf5ce3ff4a79587ace9afe34
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-11 23:05:47 +0530

    Time: 12 ms (22.08%), Space: 44.9 MB (57.67%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[amount+1][n];
+        
+        for(int ind[] : dp){
+            Arrays.fill(ind, -1);
+        }
+        
+        return solve(0, coins, amount, dp);
+    }
+    
+    public int solve(int ind, int[] coins, int amount, int[][] dp){
+        if(ind==coins.length){
+            return amount==0?1 : 0;
+        }
+        
+        if(amount == 0){
+            return 1;
+        }
+        
+        if(dp[amount][ind] != -1){
+            return dp[amount][ind];
+        }
+        
+        int pick=0, not_pick=0;
+        
+        if(coins[ind]<=amount){
+            pick = solve(ind, coins, amount-coins[ind], dp);
+        }
+        
+        not_pick = solve(ind+1, coins, amount, dp);
+        
+        return dp[amount][ind] = pick + not_pick;
+    }
+}
