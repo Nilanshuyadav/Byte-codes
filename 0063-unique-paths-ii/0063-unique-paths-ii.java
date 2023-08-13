fe5e488bae40882f3f762c9db3ff098315f3af2c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-13 21:54:24 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (6.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,26 +1,39 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid[0][0] == 1) {
-            return 0;
+    int[] r_arr={0, -1}, c_arr={-1, 0};
+    
+    public int uniquePathsWithObstacles(int[][] m) {
+        int row = m.length, col = m[0].length;
+        
+        int[][] dp = new int[row][col];
+        
+        for(int ind[] : dp){
+            Arrays.fill(ind, -1);
         }
-
-        int rows = obstacleGrid.length;
-        int cols = obstacleGrid[0].length;
-        int[] dp = new int[cols];
-        dp[0] = 1;
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (obstacleGrid[r][c] == 1) {
-                    dp[c] = 0;
-                } else {
-                    if (c > 0) {
-                        dp[c] += dp[c - 1];
-                    }
-                }
-            }
+        
+        return m[row-1][col-1]==1 ? 0 : solve(row-1, col-1, row, col, m, dp);
+    }
+    
+    public int solve(int r, int c, int row, int col, int[][] m, int[][] dp){
+        if(r==0 && c==0){
+            return 1;
         }
-
-        return dp[cols - 1];        
+        
+        if(dp[r][c] != -1){
+            return dp[r][c];
+        }
+        
+        int new_r, new_c, ans=0;
+        
+        for(int ind=0; ind<2; ind++){
+            new_r = r+r_arr[ind];
+            new_c = c+c_arr[ind];
+            
+            if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || m[new_r][new_c]==1)
+                continue;
+            
+            ans += solve(new_r, new_c, row, col, m, dp);
+        }
+        
+        return dp[r][c] = ans;
     }
 }
