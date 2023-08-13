df069d71f3d4d82a2c473d263c373e70342d524d
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-13 21:41:04 +0530

    Time: 11 ms (48.54%), Space: 56.3 MB (89.32%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,9 +1,34 @@
 class Solution {
     public boolean validPartition(int[] nums) {
         int n = nums.length;
-        Boolean[] vis = new Boolean[n];
+        Boolean[] vis = new Boolean[n+1];
         
-        return solve(0, nums, n, vis);
+        vis[n] = true;
+        boolean ans;
+        
+        for(int ind=n-1; ind>=0; ind--){
+            ans = false;
+            
+            if(ind<n-1){
+                if(nums[ind]==nums[ind+1]){
+                    ans |= vis[ind+2];
+                }
+                
+                if(ind<n-2){
+                    if(nums[ind]==nums[ind+1] && nums[ind+1]==nums[ind+2]){
+                        ans |= vis[ind+3];
+                    }
+                    
+                    if(nums[ind]==nums[ind+1]-1 && nums[ind]==nums[ind+2]-2){
+                        ans |= vis[ind+3];
+                    }
+                }
+            }
+            
+            vis[ind] = ans;
+        }
+        
+        return vis[0];
     }
     
     public boolean solve(int ind, int[] nums, int n, Boolean[] vis){
