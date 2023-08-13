bc6a208b2177a5303130bd06025b0efbb3a1b6d6
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-13 22:34:19 +0530

    Time: 9 ms (32.48%), Space: 45.7 MB (40.21%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,36 +1,61 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[amount+1][n];
+        int[][] dp = new int[n][amount+1];
         
-        for(int ind[] : dp){
-            Arrays.fill(ind, -1);
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
         }
         
-        return solve(0, coins, amount, dp);
+        int pick, not_pick;
+        
+        for(int r=0; r<n; r++){
+            for(int c=1; c<=amount; c++){
+                pick=0;
+                not_pick=0;
+                
+                if(coins[r]<=c){
+                    pick = dp[r][c-coins[r]];
+                }
+                
+                if(r>0){
+                    not_pick = dp[r-1][c];
+                }    
+                
+                dp[r][c] = pick+not_pick;
+            }
+        }
+        
+        return dp[n-1][amount];
+        
+//         for(int ind[] : dp){
+//             Arrays.fill(ind, -1);
+//         }
+        
+//         return solve(0, coins, amount, dp);
     }
     
-    public int solve(int ind, int[] coins, int amount, int[][] dp){
-        if(ind==coins.length){
-            return 0;
-        }
+//     public int solve(int ind, int[] coins, int amount, int[][] dp){
+//         if(ind==coins.length){
+//             return 0;
+//         }
         
-        if(amount == 0){
-            return 1;
-        }
+//         if(amount == 0){
+//             return 1;
+//         }
         
-        if(dp[amount][ind] != -1){
-            return dp[amount][ind];
-        }
+//         if(dp[amount][ind] != -1){
+//             return dp[amount][ind];
+//         }
         
-        int pick=0, not_pick=0;
+//         int pick=0, not_pick=0;
         
-        if(coins[ind]<=amount){
-            pick = solve(ind, coins, amount-coins[ind], dp);
-        }
+//         if(coins[ind]<=amount){
+//             pick = solve(ind, coins, amount-coins[ind], dp);
+//         }
         
-        not_pick = solve(ind+1, coins, amount, dp);
+//         not_pick = solve(ind+1, coins, amount, dp);
         
-        return dp[amount][ind] = pick + not_pick;
-    }
+//         return dp[amount][ind] = pick + not_pick;
+//     }
 }
