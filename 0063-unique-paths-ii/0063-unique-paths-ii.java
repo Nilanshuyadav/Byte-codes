6280bab496db2fae0cdaf5e6aae4cec521c398d8
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-13 22:02:54 +0530

    Time: 1 ms (17.22%), Space: 40.7 MB (60.42%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,36 +4,57 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] m) {
         int row = m.length, col = m[0].length;
         
-        int[][] dp = new int[row][col];
-        
-        for(int ind[] : dp){
-            Arrays.fill(ind, -1);
+        if(m[row-1][col-1]==1 || m[0][0]==1){
+            return 0;
         }
         
-        return m[row-1][col-1]==1 ? 0 : solve(row-1, col-1, row, col, m, dp);
+        int[][] dp = new int[row][col];
+        int ans=1;
+        
+        for(int r=0; r<row; r++){
+            for(int c=0; c<col; c++){
+                if(m[r][c]==1){
+                    dp[r][c] = 0;
+                    continue;
+                }
+                
+                if(c!=0){
+                    ans += dp[r][c-1];
+                }
+                
+                if(r!=0){
+                    ans += dp[r-1][c];
+                }
+                
+                dp[r][c] = ans;
+                ans=0;
+            }
+        }
+        
+        return dp[row-1][col-1];
     }
     
-    public int solve(int r, int c, int row, int col, int[][] m, int[][] dp){
-        if(r==0 && c==0){
-            return 1;
-        }
+//     public int solve(int r, int c, int row, int col, int[][] m, int[][] dp){
+//         if(r==0 && c==0){
+//             return 1;
+//         }
         
-        if(dp[r][c] != -1){
-            return dp[r][c];
-        }
+//         if(dp[r][c] != -1){
+//             return dp[r][c];
+//         }
         
-        int new_r, new_c, ans=0;
+//         int new_r, new_c, ans=0;
         
-        for(int ind=0; ind<2; ind++){
-            new_r = r+r_arr[ind];
-            new_c = c+c_arr[ind];
+//         for(int ind=0; ind<2; ind++){
+//             new_r = r+r_arr[ind];
+//             new_c = c+c_arr[ind];
             
-            if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || m[new_r][new_c]==1)
-                continue;
+//             if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || m[new_r][new_c]==1)
+//                 continue;
             
-            ans += solve(new_r, new_c, row, col, m, dp);
-        }
+//             ans += solve(new_r, new_c, row, col, m, dp);
+//         }
         
-        return dp[r][c] = ans;
-    }
+//         return dp[r][c] = ans;
+//     }
 }
