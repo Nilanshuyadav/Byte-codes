99c45cdd66f7f716cd41f6c3d49494038e4bf6d6
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-20 02:35:09 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (18.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,33 @@
 class Solution {
     List<List<Integer>> ans;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         ans = new ArrayList<>();
-        
         int n = candidates.length;
         
-        dfs(0, new ArrayList<>(), candidates, n, target);
+        solve(0, candidates, n, target, new ArrayList<>());
         
         return ans;
     }
     
-    public void dfs(int ind, List<Integer> al, int[] candidates, int n, int target){
-        if(n <= ind){
-            if(target == 0)
-                ans.add(new ArrayList<>(al));
-            
+    public void solve(int ind, int[] candidates, int n, int target, List<Integer> temp){
+        if(target == 0){
+            ans. add(new ArrayList<>(temp));
+            return;
+        }
+        
+        if(ind == n){
             return;
         }
         
         if(candidates[ind] <= target){
-            al.add(candidates[ind]);
-            dfs(ind, al, candidates, n, target-candidates[ind]);
-            al.remove(al.size()-1);
+            temp.add(candidates[ind]);
+            solve(ind, candidates, n, target-candidates[ind], temp);
+            temp.remove(temp.size()-1);
         }
         
-        dfs(ind+1, al, candidates, n, target);
+        solve(ind+1, candidates, n, target, temp);
+        
+        return;
     }
 }
