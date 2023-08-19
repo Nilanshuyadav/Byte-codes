60d60f89e85d63cf2a189778b6fd383c045f23ce
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-19 18:35:43 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum=0;
+        
+        for(int ind=0; ind<n; ind++){
+            sum += arr[ind];
+            
+            if(sum == s){
+                return new ArrayList<>(Arrays.asList(1, ind+1));
+            }
+            else if(map.containsKey(sum-s)){
+                return new ArrayList<>(Arrays.asList(map.get(sum-s)+2, ind+1));
+            }
+            
+            map.put(sum, ind);
+        }
+        
+        return new ArrayList<>(Arrays.asList(-1));
+    }
+}
