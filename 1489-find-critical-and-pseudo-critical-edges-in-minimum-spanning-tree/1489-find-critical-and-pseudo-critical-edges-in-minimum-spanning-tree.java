edc1eb22437ffcdd093a50e1d72d9be4bb6270b6
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-27 21:18:20 +0530

    Time: 23 ms (86.19%), Space: 44.7 MB (17.35%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
@@ -1,93 +1,94 @@
-class UnionFind{
-    int[] rank, parent;
+class DSU{
+    int rank[], parent[];
+    
     public void union(int par_a, int par_b){
-        
-        if(rank[par_a] > rank[par_b]){
-            parent[par_b] = par_a;
-        }
-        else if(rank[par_a] < rank[par_b]){
+        if(rank[par_a] < rank[par_b]){
             parent[par_a] = par_b;
         }
-        else{
+        else if(rank[par_a] > rank[par_b]){
             parent[par_b] = par_a;
-            rank[par_a]++;
+        }
+        else{
+            parent[par_a] = par_b;
+            rank[par_b]++;
         }
     }
     
-    public int findPar(int i){
-        if(parent[i] == i)
-            return i;
-        
-        return parent[i] = findPar(parent[i]);
+    public int findPar(int a){
+        if(parent[a] == a){
+            return a;
+        }
+        return parent[a] = findPar(parent[a]);
     }
     
-    public UnionFind(int n){
+    public DSU(int n){
         rank = new int[n];
         parent = new int[n];
         
-        for(int ind=0; ind<n; ind++){
+        for(int ind=0; ind<n; ind++)
             parent[ind] = ind;
-        }
     }
 }
 class Solution {
     public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
-        int m = edges.length, edge[];
-        List<Integer> critical = new ArrayList<>(), pseudo_critical = new ArrayList<>();
+        int m = edges.length, temp[];
         
         for(int ind=0; ind<m; ind++){
-            edge = edges[ind];
-            edge = Arrays.copyOf(edge, 4);
-            edge[3] = ind;
-            edges[ind] = edge;
+            temp = edges[ind];
+            
+            temp = Arrays.copyOf(temp, 4);
+            temp[3] = ind;
+            
+            edges[ind] = temp;
         }
         
         Arrays.sort(edges, (a,b) -> a[2]-b[2]);
+        List<Integer> critical = new ArrayList<>(), pseudo_critical = new ArrayList<>();
         
-        int mstwt = findMST(n, edges, -1, -1);
+        int mstwt = findMST(edges, n, m, -1, -1);
         
         for(int ind=0; ind<m; ind++){
-            if(mstwt < findMST(n, edges, ind, -1))
+            if(findMST(edges, n, m, -1, ind) > mstwt){
                 critical.add(edges[ind][3]);
-            else if(mstwt == findMST(n, edges, -1, ind))
+            }
+            else if(findMST(edges, n, m, ind, -1) == mstwt){
                 pseudo_critical.add(edges[ind][3]);
+            }
         }
         
         List<List<Integer>> ans = new ArrayList<>();
+        
         ans.add(critical);
         ans.add(pseudo_critical);
         
         return ans;
     }
     
-    public int findMST(int n, int[][] edges, int block, int include){
-        UnionFind uf = new UnionFind(n);
+    public int findMST(int[][] edges, int n, int m, int i, int e){
+        DSU uf = new DSU(n);
         
-        int weight = 0;
-        if(include != -1){
-            weight += edges[include][2];
-            uf.union(edges[include][0], edges[include][1]);
+        int weight = 0, par_a, par_b;
+        
+        if(i != -1){
+            uf.union(edges[i][0], edges[i][1]);
+            weight += edges[i][2];
         }
         
-        int par_a, par_b, ind[];
-        
-        for(int i=0; i<edges.length; i++){
-            if(i==block) continue;
+        for(int ind=0; ind<m; ind++){
+            if(ind == e)
+                continue;
             
-            ind = edges[i];
-            
-            par_a = uf.findPar(ind[0]);
-            par_b = uf.findPar(ind[1]);
+            par_a = uf.findPar(edges[ind][0]);
+            par_b = uf.findPar(edges[ind][1]);
             
             if(par_a != par_b){
-                weight += ind[2];
-                
                 uf.union(par_a, par_b);
+                weight += edges[ind][2];
             }
         }
         
-        for(int i=0; i<n; i++){
-            if(uf.findPar(i) != uf.findPar(0)){
+        for(int ind=0; ind<n; ind++){
+            if(uf.findPar(ind) != uf.findPar(0)){
                 return Integer.MAX_VALUE;
             }
         }
