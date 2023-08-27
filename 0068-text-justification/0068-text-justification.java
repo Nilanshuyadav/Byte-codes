7fee91dfe8ce96ec771bdd232e68d45b1769e4f2
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-27 18:43:47 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (13.24%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,51 +1,66 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution {
+class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> res = new ArrayList<>();
-
-        int i = 0;
-        int width = 0;
-        List<String> curLine = new ArrayList<>();
-
-        while (i < words.length) {
-            String curWord = words[i];
-
-            if (width + curWord.length() <= maxWidth) {
-                curLine.add(curWord);
-                width += curWord.length() + 1;
-                i++;
-            } else {
-                int spaces = maxWidth - width + curLine.size();
-
-                int added = 0;
-                int j = 0;
-
-                while (added < spaces) {
-                    if (j >= curLine.size() - 1) {
-                        j = 0;
-                    }
-
-                    curLine.set(j, curLine.get(j) + " ");
-                    added++;
-                    j++;
-                }
-
-                res.add(String.join("", curLine));
-
-                curLine.clear();
-                width = 0;
+        int n = words.length;
+        
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb;
+        
+        int i=0, j=0, cnt, space, remaining, rem, temp, wrd_cnt;
+        while(j<n){
+            cnt = 0;
+            space = 0;
+            remaining = maxWidth;
+            sb = new StringBuilder("");
+            
+            while(j<n && remaining >= words[j].length()+space){
+                remaining -= words[j].length();
+                space += 1;
+                j++;
             }
+            
+            wrd_cnt = j-i;
+            rem = wrd_cnt>1 ? remaining%(wrd_cnt-1) : remaining;
+            
+            // System.out.println(wrd_cnt + " " + rem + " "+remaining);
+            
+            for(int ind=i; ind<j; ind++){
+                sb.append(words[ind]);
+                
+                temp = wrd_cnt>1 ? remaining/(wrd_cnt-1) : remaining;
+                if(j==n){
+                    temp = remaining;
+                }
+                
+                while(ind!=j-1 && wrd_cnt>1 && temp-->0){
+                    sb.append(" ");
+                    
+                    if(j==n){
+                        remaining--;
+                        break;
+                    }
+                }
+                
+                if(j!=n && rem > 0){
+                    sb.append(" ");
+                    rem--;
+                }
+            }
+            
+            while(j!=n && rem > 0){
+                sb.append(" ");
+                rem--;
+            }
+            
+            if(j==n){
+                while(remaining-->0){
+                    sb.append(" ");
+                }
+            }
+            
+            ans.add(sb.toString());
+            i = j;
         }
-
-        for (int word = 0; word < curLine.size() - 1; word++) {
-            curLine.set(word, curLine.get(word) + " ");
-        }
-        curLine.set(curLine.size() - 1, curLine.get(curLine.size() - 1) + " ".repeat(maxWidth - width + 1));
-
-        res.add(String.join("", curLine));
-
-        return res;
+        
+        return ans;
     }
 }
