7c1dc5e43603a0f8086cb8c1e49bdb4c34bada60
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-27 16:12:25 +0530

    Time: 70 ms (33.18%), Space: 62.7 MB (15.95%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,31 +1,65 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        TreeMap<Integer, Set<Integer>> map = new TreeMap<>();
         int n = stones.length;
         
-        for(int ind=0; ind<n; ind++)
-            map.put(stones[ind], new HashSet<>());
+        Boolean[][] dp = new Boolean[n][n];
         
-        Set<Integer> temp = map.get(0);
-        temp.add(1);
-        
-        map.put(0, temp);
-        
-        for(Map.Entry<Integer, Set<Integer>> entry : map.entrySet()){
-            int to, from = entry.getKey();
-            
-            for(int ind : entry.getValue()){
-                to = from+ind;
-                
-                if(map.containsKey(to)){
-                    if(ind!=1)
-                        map.get(to).add(ind-1);
-                    map.get(to).add(ind);
-                    map.get(to).add(ind+1);
-                }
-            }    
+        return solve(0, stones, 0, n, dp);
+    }
+    
+    
+    public boolean solve(int ind, int[] stones, int k, int n, Boolean[][] dp){
+        if(ind == n-1){
+            return true;
         }
         
-        return !map.get(stones[n-1]).isEmpty();
+        if(dp[ind][k] != null){
+            return dp[ind][k];
+        }
+        
+        boolean bool = false;
+        
+        if(k-1>0){
+            int temp = contains(stones, n, stones[ind]+k-1);
+            
+            if(temp!=-1)
+                bool |= solve(temp, stones, k-1, n, dp);
+        }
+        
+        if(k>0){
+            int temp = contains(stones, n, stones[ind]+k);
+            
+            if(temp!=-1)
+                bool |= solve(temp, stones, k, n, dp);
+        }
+        
+        if(k+1>0){
+            int temp = contains(stones, n, stones[ind]+k+1);
+            
+            if(temp!=-1)
+                bool |= solve(temp, stones, k+1, n, dp);
+        }
+        
+        return dp[ind][k] = bool;
+    }
+    
+    public int contains(int[] stones, int n, int tar){
+        int l=0, h=n-1, m;
+        
+        while(l<=h){
+            m = l + (h-l)/2;
+            
+            if(stones[m] == tar){
+                return m;
+            }
+            else if(stones[m] < tar){
+                l = m+1;
+            }
+            else{
+                h = m-1;
+            }
+        }
+        
+        return -1;
     }
 }
