b8a4a43835edec71f8659af9772263837c607d23
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2023-08-29 22:49:13 +0530

    Time: 8 ms (34.38%), Space: 44 MB (98.19%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
@@ -4,21 +4,35 @@ class Solution {
     }
     
     public boolean solve(int ind, int ind2, String s1, String s2, int l1, int l2){
-        if(ind2 == l2){
-            return true;
-        }
+//         if(ind2 == l2){
+//             return true;
+//         }
         
-        boolean bool = false;
+//         boolean bool = false;
         
-        for(int i=ind; i<l1; i++){
-            if(s1.charAt(i)==s2.charAt(ind2) || s1.charAt(i)==pre(s2.charAt(ind2))){
-                bool |= solve(i+1, ind2+1, s1, s2, l1, l2);
+//         for(int i=ind; i<l1; i++){
+//             if(s1.charAt(i)==s2.charAt(ind2) || s1.charAt(i)==pre(s2.charAt(ind2))){
+//                 bool |= solve(i+1, ind2+1, s1, s2, l1, l2);
                 
-                break;
-            }
-        }
+//                 break;
+//             }
+//         }
         
-        return bool;
+//         return bool;
+        
+        while(true){
+            while(s1.charAt(ind)!=s2.charAt(ind2) && s1.charAt(ind)!=pre(s2.charAt(ind2))){
+                ind++;
+                
+                if(ind==l1) return false;
+            }
+            
+            ind++;
+            ind2++;
+            
+            if(ind2==l2) return true;
+            if(ind==l1) return false;
+        }
     }
     
     public char pre(char ch){
