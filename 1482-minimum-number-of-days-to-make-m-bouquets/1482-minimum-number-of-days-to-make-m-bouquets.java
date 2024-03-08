ea2ae3123970e1f002cfd0c99ae7de73bd76b29c
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-03-09 00:19:30 +0530

    Time: 22 ms (39.63%), Space: 58.6 MB (12.94%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int max = 0;
+        for(int i : bloomDay){
+            max = Math.max(max, i);
+        }
+        
+        int l=1, h=max, mid;
+        
+        while(l<=h){
+            mid = l + (h-l)/2;
+            
+            if(canWe(bloomDay, mid, m, k)) h = mid-1;
+            else l = mid+1;
+        }
+        
+        return h==max? -1 : h+1;
+    }
+    
+    public boolean canWe(int[] bloomDay, int mid, int m, int k){
+        int cnt=0, temp_k=k;
+        
+        for(int i : bloomDay){
+            if(i <= mid){
+                temp_k--;
+            }
+            else{
+                temp_k = k;
+            }
+            
+            if(temp_k == 0){
+                cnt++;
+                temp_k = k;
+            }
+        }
+        
+        return cnt>=m;
+    }
+}
