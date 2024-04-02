a5f592d7cf0d0250336736260dd53ddbf79cfd31
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-04-02 14:03:46 +0530

    Time: 24 ms (11.41%), Space: 42.4 MB (51.51%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,17 +4,22 @@ class Solution {
         
         if(n!=m) return false;
         
-        Map<Character, Character> map1 = new HashMap<>(), map2 = new HashMap<>();
-        char c1, c2;
+        Map<Character, Character> map1 = new HashMap<>(), map2 = new HashMap<Character, Character>();        
+        int i=0, j=0;
+        char ch1, ch2;
         
-        for(int ind=0; ind<n ; ind++){
-            c1 = s.charAt(ind);
-            c2 = t.charAt(ind);
+        while(i<n && j<m){
+            ch1 = s.charAt(i);
+            ch2 = t.charAt(j);
             
-            if((map1.containsKey(c1) && map1.get(c1)!=c2) || (map2.containsKey(c2) && map2.get(c2)!=c1)) return false;
+            if(map1.containsKey(ch1) && map1.get(ch1)!=ch2) return false;
+            else if(map2.containsKey(ch2) && map2.get(ch2)!=ch1) return false;
             
-            map1.put(c1, c2);
-            map2.put(c2, c1);
+            map1.put(ch1, ch2);
+            map2.put(ch2, ch1);
+            
+            i++;
+            j++;
         }
         
         return true;
