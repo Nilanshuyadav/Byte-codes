b865e91a6cf2f092d37fa720a04fe593ea558603
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-05-26 21:17:40 +0530

    Time: 228 ms (69.31%), Space: 59.4 MB (61.01%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -2,32 +2,33 @@ class Solution {
     public int checkRecord(int n) {
         int[][][] dp = new int[n+1][2][3];
         
-        for(int i[][] : dp){
-            for(int j[] : i){
-                Arrays.fill(j, -1);
+        for(int A=0; A<2; A++){
+            for(int L=0; L<3; L++){
+                dp[0][A][L] = 1;
             }
         }
         
-        return (int)helper(n, 1, 2, dp);
-    }
-    
-    public long helper(int ind, int A, int L, int[][][] dp){
-        if(ind==0){
-            return 1;
-        }
-        
-        if(dp[ind][A][L] != -1)
-            return dp[ind][A][L];
-        
         long sum = 0;
         
-        if(A>0)
-            sum += helper(ind-1, A-1, 2, dp);
-        if(L>0)
-            sum += helper(ind-1, A, L-1, dp);
+        for(int ind=1; ind<=n; ind++){
+            for(int A=0; A<2; A++){
+                for(int L=0; L<3; L++){
+                    sum = 0;
+                    
+                    if(A>0){
+                        sum += dp[ind-1][A-1][2];
+                    }   
+                    if(L>0){
+                        sum += dp[ind-1][A][L-1];
+                    }
+                    
+                    sum += dp[ind-1][A][2];
+                    
+                    dp[ind][A][L] = (int)(sum%1000000007);
+                }
+            }
+        }
         
-        sum += helper(ind-1, A, 2, dp);
-        
-        return dp[ind][A][L] = (int)(sum%1000000007);
+        return dp[n][1][2];
     }
 }
