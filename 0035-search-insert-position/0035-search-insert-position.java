8089fbd6489b1092e1670e4e78dca1fb6675fc8a
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-05-27 19:58:20 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (71.79%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,9 +5,15 @@ class Solution {
         while(l<=h){
             m = l + (h-l)/2;
             
-            if(nums[m] == target) return m;
-            else if(nums[m] < target) l = m+1;
-            else h = m-1;
+            if(nums[m] == target){
+                return m;
+            }
+            else if(nums[m] > target){
+                h = m-1;
+            }
+            else{
+                l = m+1;
+            }
         }
         
         return l;
