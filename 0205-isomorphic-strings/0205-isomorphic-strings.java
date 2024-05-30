d06917845e763cb9ac46b5ebb2f065f58e6d30af
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-05-30 14:28:36 +0530

    Time: 5 ms (95.73%), Space: 42.3 MB (45.42%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int n=s.length(), m=t.length();
+        int n = s.length(), m = t.length();
         
         if(n!=m) return false;
         
-        Map<Character, Character> map1 = new HashMap<>(), map2 = new HashMap<Character, Character>();        
-        int i=0, j=0;
+        int[] arr1= new int[256];
+        int[] arr2 = new int[256];
+        
+        Arrays.fill(arr1, -1);
+        Arrays.fill(arr2, -1);
+        
         char ch1, ch2;
         
-        while(i<n && j<m){
+        for(int i=0; i<n; i++){
             ch1 = s.charAt(i);
-            ch2 = t.charAt(j);
+            ch2 = t.charAt(i);
             
-            if(map1.containsKey(ch1) && map1.get(ch1)!=ch2) return false;
-            else if(map2.containsKey(ch2) && map2.get(ch2)!=ch1) return false;
+            if(arr1[ch1]==-1){
+                arr1[ch1] = ch2;
+            }
+            if(arr2[ch2]==-1){
+                arr2[ch2] = ch1;
+            }
             
-            map1.put(ch1, ch2);
-            map2.put(ch2, ch1);
+            if(arr1[ch1]!=ch2 || arr2[ch2]!=ch1) return false;
             
-            i++;
-            j++;
         }
         
         return true;
