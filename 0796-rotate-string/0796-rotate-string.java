39e2dc24e8365bacd393706ec00a613053607825
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-05-30 15:35:32 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (72.34%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
-        return s.repeat(2).indexOf(goal)!=-1;
+        return s.length()==goal.length() && s.repeat(2).indexOf(goal)!=-1;
     }
 }
