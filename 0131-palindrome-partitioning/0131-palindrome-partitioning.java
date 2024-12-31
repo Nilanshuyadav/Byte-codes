a5176e3dc832b05594e22bc1136f8cae9bcb6d6d
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-12-31 12:41:56 +0530

    Time: 10 ms (21.22%), Space: 56 MB (95.73%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,41 +1,45 @@
 class Solution {
+    List<List<String>> ans;
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        
-        solve(0, s, s.length(), "", new ArrayList<>(), ans);
-        
+        int n = s.length();
+
+        ans = new ArrayList<>();
+
+        helper(0, s, n, new ArrayList<>());
+
         return ans;
     }
-    
-    public void solve(int ind, String s, int n, String temp_string, List<String> temp, List<List<String>> ans){
+
+    public void helper(int ind, String s, int n, List<String> al){
         if(ind == n){
-            if(temp_string.equals(""))
-                ans.add(new ArrayList<>(temp));
-            
-            return;    
+            ans.add(new ArrayList<>(al));
+            return;
         }
-        
-        StringBuilder sb = new StringBuilder(temp_string);
-        
-        sb.append(s.charAt(ind));
-        
-        if(isPalindrome(sb.toString())){
-            temp.add(sb.toString());
-            solve(ind+1, s, n, "", temp, ans);
-            temp.remove(temp.size()-1);
+
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i=ind; i<n; i++){
+            sb.append(s.charAt(i));
+
+            if(isPallindrome(sb)){
+                al.add(sb.toString());
+                helper(i+1, s, n, al);
+                al.remove(al.size()-1);
+            }
         }
-        
-        solve(ind+1, s, n, sb.toString(), temp, ans);
     }
-    
-    public boolean isPalindrome(String sb){
-        int low = 0, high = sb.length()-1;
-        
-        while(low<high){
-            if(sb.charAt(low++) != sb.charAt(high--))
-                return false;
+
+    public boolean isPallindrome(StringBuilder s){
+        int n = s.length();
+
+        int i=0, j=n-1;
+
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
         }
-        
+
         return true;
     }
 }
