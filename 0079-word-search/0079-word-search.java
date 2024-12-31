5f2c269b3f134cd13007694b34c4034d445a36c9
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2024-12-31 12:18:10 +0530

    Time: 166 ms (35.12%), Space: 41.3 MB (86.69%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,47 +1,46 @@
 class Solution {
-    int[] r_arr={-1,0,1,0}, c_arr={0,1,0,-1};
-    
+    int[] r_arr = {-1, 0, 1, 0}, c_arr = {0, 1, 0, -1};
     public boolean exist(char[][] board, String word) {
-        int row=board.length, col=board[0].length;
+        int row = board.length;
+        int col = board[0].length;
+
+        boolean ans = false;
         
         for(int r=0; r<row; r++){
             for(int c=0; c<col; c++){
                 if(board[r][c] == word.charAt(0)){
                     board[r][c] = '*';
-                    
-                    if(solve(r,c,1,board,word)){
-                        return true;
-                    }
-                    
+                    ans = ans || helper(r, c, row, col, board, word, 1, word.length());
                     board[r][c] = word.charAt(0);
                 }
             }
         }
-        
-        return false;
+
+        return ans;
     }
-    
-    public boolean solve(int r, int c, int i, char[][] board, String word){
-        if(i==word.length()){
+
+    public boolean helper(int r, int c, int row, int col, char[][] board,  String word, int ind, int n){
+        if(ind == n){
             return true;
         }
-        
-        int new_r, new_c, row=board.length, col=board[0].length;
-        boolean bool=false;
-        
-        for(int ind=0; ind<4; ind++){
-            new_r = r+r_arr[ind];
-            new_c = c+c_arr[ind];
-            
-            if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || board[new_r][new_c]!=word.charAt(i)){
-                continue;
-            }
-            
+
+        int new_r, new_c;
+        char curr = board[r][c];
+        boolean ans = false;
+
+        for(int i=0; i<4; i++){
+            new_r = r+r_arr[i];
+            new_c = c+c_arr[i];
+
+            if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || board[new_r][new_c]=='*' || board[new_r][new_c]!=word.charAt(ind)) continue;
+            curr = board[new_r][new_c];
             board[new_r][new_c] = '*';
-            bool = bool || solve(new_r, new_c, i+1, board, word);
-            board[new_r][new_c] = word.charAt(i);
+
+            ans = ans || helper(new_r, new_c, row, col, board, word, ind+1, n);
+
+            board[new_r][new_c] = curr;
         }
-        
-        return bool;
+
+        return ans;
     }
 }
