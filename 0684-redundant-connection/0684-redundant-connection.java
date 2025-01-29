6baf63fddcc902c1675a3143511937812bb04f4a
Nilanshu Yadav <91049897+Nilanshuyadav@users.noreply.github.com>
2025-01-29 10:15:23 +0530

    Time: 0 ms (100%), Space: 43.2 MB (75.11%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -2,42 +2,46 @@ class Solution {
     int[] parent, rank;
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        
+
         parent = new int[n+1];
         rank = new int[n+1];
-        
-        for(int ind=0; ind<=n; ind++)
-            parent[ind] = ind;
-        
-        
-        for(int ind[] : edges){
-            int a_par = findPar(ind[0]);
-            int b_par = findPar(ind[1]);
-            
-            if(a_par == b_par)
-                return ind;
-            
-            union(a_par, b_par);
+
+        for(int i=0; i<=n; i++){
+            parent[i] = i;
         }
-        
-        return new int[]{0,0};
+
+        for(int i=0; i<n; i++){
+            if(!canWe(edges[i])){
+                return edges[i];
+            }
+        }
+
+        return null;
     }
-    
-    public void union(int a_par, int b_par){
-        if(rank[a_par]<rank[b_par])
-            parent[a_par] = b_par;
-        else if(rank[b_par]<rank[a_par])
-            parent[b_par] = a_par;
+
+    public int findPar(int u){
+        if(parent[u] == u) return u;
+        return parent[u] = findPar(parent[u]);
+    }
+
+    public boolean canWe(int[] arr){
+        int u = arr[0], v = arr[1];
+
+        int par_u = findPar(u), par_v = findPar(v);
+
+        if(par_u == par_v) return false;
+
+        if(rank[par_u]<rank[par_v]){
+            parent[par_u] = par_v;
+        }
+        else if(rank[par_v]<rank[par_u]){
+            parent[par_v] = par_u;
+        }
         else{
-            parent[a_par] = b_par;
-            rank[b_par]++;
+            parent[par_u] = par_v;
+            rank[par_v]++;
         }
-    }
-    
-    public int findPar(int node){
-        if(parent[node] == node)
-            return node;
-        
-        return parent[node] = findPar(parent[node]);
+
+        return true;
     }
 }
